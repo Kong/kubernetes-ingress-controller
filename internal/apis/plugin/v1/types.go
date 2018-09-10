Kong/kubernetes-ingress-controller

commit 32ca70ff371808bd6a335ee084d57928aa0002cc

feat: introduce a Name parameter fo KongPlugin CRD
This is part of a patchset introducing support for global plugins in
Kong via the ingress controller.
This is a backwards compatible change.

This commit will have long-term effects in how plugins are configured
via Ingress and custom resources as explained below:

Currently, to apply a plugin to one has to add an annotation of the
form:
```
rate-limiting.plugin.konghq.com: plugin-conf
```
where `rate-limiting` is the name of the plugin that needs to be
created while `plugin-conf` is the plugin conf that should be used for
that plugin.

This is not as user-friendly as it should be, especially considering
that different kinds of plugins in Kong have different configuration
objects.

This allows us to keep configuration and plugin names together and in
future the annotations could be much more compact and easier to pass
around:
```
plugin.konghq.com: plugin-conf1,plugin-conf2
```

internal/apis/plugin/v1/types.go
@@ -27,6 +27,9 @@ type KongPlugin struct {
 
 	// Config contains the plugin configuration.
 	Config Configuration `json:"config,omitempty"`
+
+	// Name is the name of the plugin to which to apply the config
+	Name string `json:"name,omitempty"`
 }
 
 // KongPluginList is a top-level list type. The client methods for lists are automatically created.

commit d5c8040e6685109ea1c5a66133ecb59095759b4e

fix: rename name to plugin in KongPlugin resource
This is a fix for the issue introduced in 32ca70f.
`name` is already a property defined for every resource in k8s, which is
unique across all objects of the same kind.
The `name` from metav1.objectMeta needs to be accessible in other parts
of code to correctly read the object.

This commit renames the `name` to `plugin` field.

internal/apis/plugin/v1/types.go
@@ -28,8 +28,8 @@ type KongPlugin struct {
 	// Config contains the plugin configuration.
 	Config Configuration `json:"config,omitempty"`
 
-	// Name is the name of the plugin to which to apply the config
-	Name string `json:"name,omitempty"`
+	// PluginName is the name of the plugin to which to apply the config
+	PluginName string `json:"plugin,omitempty"`
 }
 
 // KongPluginList is a top-level list type. The client methods for lists are automatically created.

internal/ingress/controller/kong.go
@@ -131,7 +131,7 @@ func (n *NGINXController) syncGlobalPlugins() error {
 	var duplicates []string // keep track of duplicate
 
 	for i := 0; i < len(targetGlobalPlugins); i++ {
-		name := targetGlobalPlugins[i].Name
+		name := targetGlobalPlugins[i].PluginName
 		// empty name skip it
 		if name == "" {
 			continue
@@ -482,7 +482,7 @@ func (n *NGINXController) syncServices(ingressCfg *ingress.Configuration) (bool,
 					// check the kong plugin configuration is up to date
 					if !pluginDeepEqual(k8sPlugin.Config, configuredPlugin) ||
 						(!k8sPlugin.Disabled != configuredPlugin.Enabled) {
-						glog.Infof("plugin %v configuration in kong is outdated. Updating...", k8sPlugin.Name)
+						glog.Infof("plugin %v configuration in kong is outdated. Updating...", k8sPlugin.PluginName)
 						p := &kongadminv1.Plugin{
 							Name:    configuredPlugin.Name,
 							Config:  kongadminv1.Configuration(k8sPlugin.Config),
@@ -509,7 +509,7 @@ func (n *NGINXController) syncServices(ingressCfg *ingress.Configuration) (bool,
 						continue
 					}
 
-					glog.Infof("plugin %v configuration in kong is up to date.", k8sPlugin.Name)
+					glog.Infof("plugin %v configuration in kong is up to date.", k8sPlugin.PluginName)
 				}
 			}
 		}
@@ -924,7 +924,7 @@ func (n *NGINXController) syncRoutes(ingressCfg *ingress.Configuration) (bool, e
 					// check the kong plugin configuration is up to date
 					if !pluginDeepEqual(k8sPlugin.Config, configuredPlugin) ||
 						(!k8sPlugin.Disabled != configuredPlugin.Enabled) {
-						glog.Infof("plugin %v configuration in kong is outdated. Updating...", k8sPlugin.Name)
+						glog.Infof("plugin %v configuration in kong is outdated. Updating...", k8sPlugin.PluginName)
 						p := &kongadminv1.Plugin{
 							Name:    configuredPlugin.Name,
 							Config:  kongadminv1.Configuration(k8sPlugin.Config),
@@ -941,7 +941,7 @@ func (n *NGINXController) syncRoutes(ingressCfg *ingress.Configuration) (bool, e
 						continue
 					}
 
-					glog.Infof("plugin %v configuration in kong is up to date.", k8sPlugin.Name)
+					glog.Infof("plugin %v configuration in kong is up to date.", k8sPlugin.PluginName)
 				}
 			}
 
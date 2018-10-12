6282336ca438a7a5067f5b7e750f407cc78d3aaf
Harry Bagdi <harrybagdi@gmail.com>
2018-10-12 14:15:23 -0700

    feat(deploy) use Job to run Kong version
    Using InitContainer can result in two Kong nodes running migrations
    concurrently.
    Job is the right way to run migrations.

--- hack/build-single-manifests.sh
+++ hack/build-single-manifests.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
-WITH_POSTGRESQL="manifests/namespace.yaml manifests/custom-types.yaml manifests/postgres.yaml manifests/rbac.yaml manifests/ingress-controller.yaml provider/baremetal/kong-proxy-nodeport.yaml manifests/kong.yaml"
+WITH_POSTGRESQL="manifests/namespace.yaml manifests/custom-types.yaml manifests/postgres.yaml manifests/rbac.yaml manifests/ingress-controller.yaml provider/baremetal/kong-proxy-nodeport.yaml manifests/kong.yaml manifests/migration.yaml"
 MANIFEST=$(cd ${SCRIPT_ROOT}/deploy; cat ${WITH_POSTGRESQL})
 
 echo "${MANIFEST}" > ${SCRIPT_ROOT}/deploy/single/all-in-one-postgres.yaml

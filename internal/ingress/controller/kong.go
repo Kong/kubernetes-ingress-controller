Kong/kubernetes-ingress-controller

commit dc3a19b40c2d229ba47bbb0558bc8f9381bc8b54

feat: add support for path only based routing
Host is not a required field in Kubernetes Ingress Spec.
This is a small change which instantiates routes in Kong based on path
only.

Fix #84
From #142

internal/ingress/controller/kong.go
@@ -706,10 +706,6 @@ func (n *NGINXController) syncRoutes(ingressCfg *ingress.Configuration) (bool, e
 
 	// Routes
 	for _, server := range ingressCfg.Servers {
-		if server.Hostname == "_" {
-			// there is no catch all server in kong
-			continue
-		}
 
 		protos := []string{"http"}
 		if server.SSLCert != nil {
@@ -749,8 +745,10 @@ func (n *NGINXController) syncRoutes(ingressCfg *ingress.Configuration) (bool, e
 			r := &kongadminv1.Route{
 				Paths:     []string{location.Path},
 				Protocols: []string{"http", "https"}, // default
-				Hosts:     []string{server.Hostname},
 				Service:   kongadminv1.InlineService{ID: svc.ID},
+			}
+			if server.Hostname != "_" {
+				r.Hosts = []string{server.Hostname}
 			}
 
 			if kongIngress != nil && kongIngress.Route != nil {
@@ -1194,9 +1192,15 @@ func buildName(backend string, location *ingress.Location) string {
 // getKongService returns a Route from a list using the path and hosts as filters
 func getKongRoute(hostname, path string, routes []kongadminv1.Route) *kongadminv1.Route {
 	for _, r := range routes {
-		if sets.NewString(r.Paths...).Has(path) &&
-			sets.NewString(r.Hosts...).Has(hostname) {
-			return &r
+		if hostname != "_" {
+			if sets.NewString(r.Paths...).Has(path) &&
+				sets.NewString(r.Hosts...).Has(hostname) {
+				return &r
+			}
+		} else {
+			if sets.NewString(r.Paths...).Has(path) {
+				return &r
+			}
 		}
 	}
 

commit 08f679f964d9bbd97b3de85c524fc385fbd23eb4

fix: path only based routing
dc3a19b4 introduced path only based routing but introduced a bug in the
code.
If there exist only path based routing rules for a service, then
the ingress will never be satisfied. There was at least one host based
ingress rule necessary to satisfied other host-less ingress rules for
any service.
This commit fixes that and properly creates routing rules in Kong to
satisfy path-only ingress rules.

internal/ingress/controller/kong.go
@@ -65,10 +65,6 @@ func (n *NGINXController) OnUpdate(ingressCfg *ingress.Configuration) error {
 	}
 
 	for _, server := range ingressCfg.Servers {
-		if server.Hostname == "_" {
-			// there is no catch all server in kong
-			continue
-		}
 
 		err := n.syncUpstreams(server.Locations, ingressCfg.Backends)
 		if err != nil {
@@ -315,10 +311,6 @@ func (n *NGINXController) syncServices(ingressCfg *ingress.Configuration) (bool,
 
 	// Check if the endpoints exists as a service in kong
 	for _, server := range ingressCfg.Servers {
-		if server.Hostname == "_" {
-			// there is no catch all server in kong
-			continue
-		}
 
 		for _, location := range server.Locations {
 			backend := location.Backend
@@ -942,10 +934,6 @@ func (n *NGINXController) syncUpstreams(locations []*ingress.Location, backends 
 
 	for _, location := range locations {
 		backend := location.Backend
-		if backend == "default-backend" {
-			// there is no default backend in Kong
-			continue
-		}
 
 		ingress := location.Ingress
 		if ingress == nil {
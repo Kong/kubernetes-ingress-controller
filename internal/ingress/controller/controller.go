d86c65d6b956f803ee5d392407fe7ee264b582c6
Harry Bagdi <harrybagdi@gmail.com>
2019-05-02 22:30:42 -0700

    refactor: consolidate controller in a single file

--- internal/ingress/controller/controller.go
+++ internal/ingress/controller/controller.go
@@ -17,17 +17,27 @@ limitations under the License.
 package controller
 
 import (
+	"fmt"
 	"sort"
+	"sync"
 	"time"
 
 	"github.com/blang/semver"
+	"github.com/eapache/channels"
 	"github.com/golang/glog"
-	"github.com/pkg/errors"
-
-	clientset "k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-
 	"github.com/hbagdi/go-kong/kong"
+	"github.com/kong/kubernetes-ingress-controller/internal/ingress/controller/parser"
+	"github.com/kong/kubernetes-ingress-controller/internal/ingress/controller/store"
+	"github.com/kong/kubernetes-ingress-controller/internal/ingress/election"
+	"github.com/kong/kubernetes-ingress-controller/internal/ingress/status"
+	"github.com/kong/kubernetes-ingress-controller/internal/ingress/task"
+	"github.com/pkg/errors"
+	extensions "k8s.io/api/extensions/v1beta1"
+	clientset "k8s.io/client-go/kubernetes"
+	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/flowcontrol"
 )
 
 // Kong Represents a Kong client and connection information
@@ -112,6 +122,156 @@ func (n *KongController) syncIngress(interface{}) error {
 	glog.Info("successfully synced configuration to Kong")
 
 	n.runningConfig = state
+
+	return nil
+}
+
+// NewKongController creates a new NGINX Ingress controller.
+// If the environment variable NGINX_BINARY exists it will be used
+// as source for nginx commands
+func NewKongController(config *Configuration) (*KongController, error) {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{
+		Interface: config.KubeClient.CoreV1().Events(config.Namespace),
+	})
+
+	n := &KongController{
+		cfg:             config,
+		syncRateLimiter: flowcontrol.NewTokenBucketRateLimiter(config.SyncRateLimit, 1),
+
+		stopCh:   make(chan struct{}),
+		updateCh: channels.NewRingChannel(1024),
+
+		stopLock: &sync.Mutex{},
+		PluginSchemaStore: *NewPluginSchemaStore(config.Kong.Client,
+			config.Kong.URL),
+	}
+
+	n.store = store.New(
+		config.Namespace,
+		config.ResyncPeriod,
+		config.KubeClient,
+		config.KubeConf,
+		n.updateCh,
+		config.IngressClass)
+
+	n.parser = parser.New(n.store)
+	n.syncQueue = task.NewTaskQueue(n.syncIngress)
+
+	electionID := config.ElectionID + "-" + config.IngressClass
+
+	electionConfig := election.Config{
+		Client:     config.KubeClient,
+		ElectionID: electionID,
+	}
+
+	if config.UpdateStatus {
+		n.syncStatus = status.NewStatusSyncer(status.Config{
+			Client:                 config.KubeClient,
+			PublishService:         config.PublishService,
+			PublishStatusAddress:   config.PublishStatusAddress,
+			IngressLister:          n.store,
+			ElectionID:             electionID,
+			UpdateStatusOnShutdown: config.UpdateStatusOnShutdown,
+			OnStartedLeading: func() {
+				// force a sync
+				n.syncQueue.Enqueue(&extensions.Ingress{})
+			},
+		})
+		electionConfig.Callbacks = n.syncStatus.Callbacks()
+	} else {
+		glog.Warning("Update of ingress status is disabled (flag --update-status=false was specified)")
+	}
+
+	n.elector = election.NewElector(electionConfig)
+
+	return n, nil
+}
+
+// KongController ...
+type KongController struct {
+	cfg *Configuration
+
+	syncQueue *task.Queue
+
+	syncStatus status.Sync
+
+	elector election.Elector
+
+	syncRateLimiter flowcontrol.RateLimiter
+
+	// stopLock is used to enforce only a single call to Stop is active.
+	// Needed because we allow stopping through an http endpoint and
+	// allowing concurrent stoppers leads to stack traces.
+	stopLock *sync.Mutex
+
+	stopCh   chan struct{}
+	updateCh *channels.RingChannel
+
+	// runningConfig contains the running configuration in the Backend
+	runningConfig *parser.KongState
+
+	isShuttingDown bool
+
+	store store.Storer
+
+	parser parser.Parser
+
+	PluginSchemaStore PluginSchemaStore
+}
+
+// Start start a new NGINX master process running in foreground.
+func (n *KongController) Start() {
+	glog.Infof("starting Ingress controller")
+
+	n.store.Run(n.stopCh)
+
+	go n.elector.Run()
+
+	if n.syncStatus != nil {
+		go n.syncStatus.Run()
+	}
+
+	go n.syncQueue.Run(time.Second, n.stopCh)
+	// force initial sync
+	n.syncQueue.Enqueue(&extensions.Ingress{})
+
+	for {
+		select {
+		case event := <-n.updateCh.Out():
+			if n.isShuttingDown {
+				break
+			}
+			if evt, ok := event.(store.Event); ok {
+				glog.V(3).Infof("Event %v received - object %v", evt.Type, evt.Obj)
+				n.syncQueue.Enqueue(evt.Obj)
+			} else {
+				glog.Warningf("unexpected event type received %T", event)
+			}
+		case <-n.stopCh:
+			break
+		}
+	}
+}
+
+// Stop gracefully stops the NGINX master process.
+func (n *KongController) Stop() error {
+	n.isShuttingDown = true
+
+	n.stopLock.Lock()
+	defer n.stopLock.Unlock()
+
+	// Only try draining the workqueue if we haven't already.
+	if n.syncQueue.IsShuttingDown() {
+		return fmt.Errorf("shutdown already in progress")
+	}
+
+	glog.Infof("shutting down controller queues")
+	close(n.stopCh)
+	go n.syncQueue.Shutdown()
+	if n.syncStatus != nil {
+		n.syncStatus.Shutdown(n.elector.IsLeader())
+	}
 
 	return nil
 }

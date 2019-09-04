a5c611425b63bb61ab28a235a11e0b243cc87f6b
Harry Bagdi <harrybagdi@gmail.com>
2019-09-04 16:07:04 -0700

    feat(deploy) use kustomize to generate single YAML files
    This commit will also allow an end-user to use kustomize to deploy
    Kong Ingress Controller using remote build.

--- hack/build-single-manifests.sh
+++ hack/build-single-manifests.sh
@@ -6,14 +6,6 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
-COMMON="namespace.yaml custom-types.yaml rbac.yaml service.yaml"
-COMMON+=" custom-server-block.yaml validation-service.yaml"
-DB="postgres.yaml migration.yaml kong-ingress-postgres.yaml"
-DBLESS="kong-ingress-dbless.yaml"
-
-MANIFEST=$(cd ${SCRIPT_ROOT}/deploy/manifests; cat ${COMMON} ${DB})
-echo "${MANIFEST}" > ${SCRIPT_ROOT}/deploy/single/all-in-one-postgres.yaml
-
-MANIFEST=$(cd ${SCRIPT_ROOT}/deploy/manifests; cat ${COMMON} ${DBLESS})
-echo "${MANIFEST}" > ${SCRIPT_ROOT}/deploy/single/all-in-one-dbless.yaml
+kustomize build ./deploy/manifests/base > deploy/single/all-in-one-dbless.yaml
+kustomize build ./deploy/manifests/postgres > deploy/single/all-in-one-postgres.yaml
 

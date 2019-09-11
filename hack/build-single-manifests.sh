9603ed50fb366d2696f3fec4d7b16d7b5627f392
Harry Bagdi <harrybagdi@gmail.com>
2019-09-11 09:50:24 -0700

    feat(deploy) add kustomize based and a single YAML for Kong Enterprise

--- hack/build-single-manifests.sh
+++ hack/build-single-manifests.sh
@@ -7,5 +7,8 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 kustomize build ./deploy/manifests/base > deploy/single/all-in-one-dbless.yaml
-kustomize build ./deploy/manifests/postgres > deploy/single/all-in-one-postgres.yaml
+kustomize build ./deploy/manifests/postgres \
+  > deploy/single/all-in-one-postgres.yaml
+kustomize build ./deploy/manifests/enterprise \
+  > deploy/single/all-in-one-postgres-enterprise.yaml
 

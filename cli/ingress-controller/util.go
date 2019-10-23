6fce2892f04efee2016a55984cd014827164da01
Harry <harrybagdi@gmail.com>
2019-10-23 12:45:21 -0700

    feat: create Kong Enterprise workspace when not present
    If Kong Enterprise workspace doesn't exist, the controller simply errors
    out.
    With this change, the controller tries to create a workspace before
    erroring out.
    If the creation is successful, then it proceeds as usual.
    
    From #429

--- cli/ingress-controller/util.go
+++ cli/ingress-controller/util.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 
 	"github.com/blang/semver"
+	"github.com/hbagdi/go-kong/kong"
+	"github.com/pkg/errors"
 )
 
 func getSemVerVer(v string) (semver.Version, error) {
@@ -25,4 +27,32 @@ func getSemVerVer(v string) (semver.Version, error) {
 	}
 	v = fmt.Sprintf("%s.%s%s", m[1], m[2], m[3])
 	return semver.Make(v)
+}
+
+func ensureWorkspace(client *kong.Client, workspace string) error {
+	req, err := client.NewRequest("GET", "/workspaces/"+workspace, nil, nil)
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(nil, req, nil)
+	if err != nil {
+		if kong.IsNotFoundErr(err) {
+			if err := createWorkspace(client, workspace); err != nil {
+				return errors.Wrapf(err, "creating workspace '%v'", workspace)
+			}
+			return nil
+		}
+		return errors.Wrapf(err, "looking up workspace '%v'", workspace)
+	}
+	return nil
+}
+
+func createWorkspace(client *kong.Client, workspace string) error {
+	body := map[string]string{"name": workspace}
+	req, err := client.NewRequest("POST", "/workspaces", nil, body)
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(nil, req, nil)
+	return err
 }

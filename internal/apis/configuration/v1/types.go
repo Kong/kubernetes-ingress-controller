ecf377bbd3e2b236129fbafc4711919aa1eee903
Harry Bagdi <harrybagdi@gmail.com>
2020-01-27 11:27:39 -0800

    feat: introduce KongClusterPlugin as a new type in Go

--- internal/apis/configuration/v1/types.go
+++ internal/apis/configuration/v1/types.go
@@ -37,6 +37,49 @@ type KongIngressList struct {
 }
 
 // +genclient
+// +genclient:nonNamespaced
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KongClusterPlugin is a top-level type. A client is created for it.
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type KongClusterPlugin struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// ConsumerRef is a reference to a particular consumer
+	ConsumerRef string `json:"consumerRef,omitempty"`
+
+	// Disabled set if the plugin is disabled or not
+	Disabled bool `json:"disabled,omitempty"`
+
+	// Config contains the plugin configuration.
+	Config Configuration `json:"config,omitempty"`
+
+	// PluginName is the name of the plugin to which to apply the config
+	PluginName string `json:"plugin,omitempty"`
+
+	// RunOn configures the plugin to run on the first or the second or both
+	// nodes in case of a service mesh deployment.
+	RunOn string `json:"run_on,omitempty"`
+
+	// Protocols configures plugin to run on requests received on specific
+	// protocols.
+	Protocols []string `json:"protocols,omitempty"`
+}
+
+// KongClusterPluginList is a top-level list type. The client methods for lists are automatically created.
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type KongClusterPluginList struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	// +optional
+	Items []KongClusterPlugin `json:"items"`
+}
+
+// +genclient
 // +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -86,25 +129,25 @@ func init() {
 	gob.Register(map[string]interface{}{})
 }
 
+// DeepCopy deepcopy function, copying the receiver, writing into out. in must be non-nil.
+// TODO: change this to be able to use the k8s code generator
+func (in *Configuration) DeepCopy() *Configuration {
+	var result Configuration
+	in.DeepCopyInto(&result)
+	return &result
+}
+
 // DeepCopyInto deepcopy function, copying the receiver, writing into out. in must be non-nil.
 // TODO: change this to be able to use the k8s code generator
-func (in *KongPlugin) DeepCopyInto(out *KongPlugin) {
-	*out = *in
-	out.TypeMeta = in.TypeMeta
-	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
-	if in.Config != nil {
-		var buf bytes.Buffer
-		enc := gob.NewEncoder(&buf)
-		dec := gob.NewDecoder(&buf)
-		err := enc.Encode(in.Config)
-		if err != nil {
-			glog.Errorf("unexpected error copying configuration: %v", err)
-		}
-		err = dec.Decode(&out.Config)
-		if err != nil {
-			glog.Errorf("unexpected error copying configuration: %v", err)
-		}
+func (in *Configuration) DeepCopyInto(out *Configuration) {
+	if out == nil {
+		out = new(Configuration)
 	}
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	dec := gob.NewDecoder(&buf)
+	enc.Encode(in)  // ignoring error XXX
+	dec.Decode(out) // ignoring error XXX
 	return
 }
 

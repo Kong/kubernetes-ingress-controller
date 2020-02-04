103601ba4c217f15b8cd5d45cf489cd00c8d81fd
Harry Bagdi <harrybagdi@gmail.com>
2020-02-04 14:28:25 -0800

    feat: introduce TCPIngress as a new Go type

---
+++ internal/apis/configuration/v1beta1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+
+// Package v1beta1 is the v1beta1 version of the configuration.konghq.com API.
+// +groupName=configuration.konghq.com
+package v1beta1
---
+++ internal/apis/configuration/v1beta1/register.go
@@ -0,0 +1,35 @@
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	// TODO: move SchemeBuilder with zz_generated.deepcopy.go to k8s.io/api.
+	// localSchemeBuilder and AddToScheme will stay in k8s.io/kubernetes.
+
+	// SchemeBuilder is a schemeBuilder with all CRDs
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	// AddToScheme points to SchemeBuilder's AddToScheme
+	AddToScheme = SchemeBuilder.AddToScheme
+	// SchemeGroupVersion is API Group and Version of
+	// Kong Ingress Controller's API.
+	SchemeGroupVersion = schema.GroupVersion{Group: "configuration.konghq.com", Version: "v1beta1"}
+)
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// Adds the list of known types to the given scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&TCPIngress{},
+		&TCPIngressList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}

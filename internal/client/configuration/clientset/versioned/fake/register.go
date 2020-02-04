5b97cf3dff90579507a9a0a00990cf2f9f07deb5
Harry Bagdi <harrybagdi@gmail.com>
2020-02-04 14:31:22 -0800

    chore: run code-gen for TCPIngress

--- internal/client/configuration/clientset/versioned/fake/register.go
+++ internal/client/configuration/clientset/versioned/fake/register.go
@@ -20,6 +20,7 @@ package fake
 
 import (
 	configurationv1 "github.com/kong/kubernetes-ingress-controller/internal/apis/configuration/v1"
+	configurationv1beta1 "github.com/kong/kubernetes-ingress-controller/internal/apis/configuration/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
@@ -32,6 +33,7 @@ var codecs = serializer.NewCodecFactory(scheme)
 var parameterCodec = runtime.NewParameterCodec(scheme)
 var localSchemeBuilder = runtime.SchemeBuilder{
 	configurationv1.AddToScheme,
+	configurationv1beta1.AddToScheme,
 }
 
 // AddToScheme adds all types of this clientset into the given scheme. This allows composition

8c402745709378406be6407e3ba4e8aaf08baeb7
Harry Bagdi <harrybagdi@gmail.com>
2020-02-18 11:13:56 -0800

    feat: new annotations group konghq.com
    This commit moves annotations from `configuration.konghq.com` to
    `konghq.com`. Thre previous annotations are now deprecated and will be
    removed in a future release.
    
    The annotations are mostly the same with the exception of:
    - `configuration.konghq.com` is changed to `konghq.com/override`
    - `plugins.konghq.com` is changed to `konghq.com/plugins`

--- internal/ingress/annotations/annotations_test.go
+++ internal/ingress/annotations/annotations_test.go
@@ -25,78 +25,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestExtractKongPluginsFromAnnotations(t *testing.T) {
-	data := map[string]string{
-		"plugins.konghq.com": "kp-rl, kp-cors",
-	}
-
-	ka := ExtractKongPluginsFromAnnotations(data)
-	if len(ka) != 2 {
-		t.Errorf("expected two keys but %v returned", len(ka))
-	}
-	if ka[0] != "kp-rl" {
-		t.Errorf("expected first element to be 'kp-rl'")
-	}
-}
-
-func TestExtractConfigurationName(t *testing.T) {
-	data := map[string]string{
-		"configuration.konghq.com": "demo",
-	}
-
-	cn := ExtractConfigurationName(data)
-	if cn != "demo" {
-		t.Errorf("expected demo as configuration name but got %v", cn)
-	}
-}
-
-func TestExtractProtocolName(t *testing.T) {
-	data := map[string]string{
-		"configuration.konghq.com/protocol": "grpc",
-	}
-
-	pn := ExtractProtocolName(data)
-	if pn != "grpc" {
-		t.Errorf("expected grpc as configuration name but got %v", pn)
-	}
-}
-
-func TestExtractProtocolNames(t *testing.T) {
-	data := map[string]string{
-		"configuration.konghq.com/protocols": "grpc,grpcs",
-	}
-
-	s := []string{"grpc", "grpcs"}
-
-	pns := ExtractProtocolNames(data)
-	if !reflect.DeepEqual(pns, s) {
-		t.Errorf("expected grpc,grpcs as configuration name but got %v", pns)
-	}
-}
-
-func TestExtractClientCert(t *testing.T) {
-	data := map[string]string{
-		"configuration.konghq.com/client-cert": "secret1",
-	}
-
-	secret := ExtractClientCertificate(data)
-	if secret != "secret1" {
-		t.Errorf("expected secret as secret1 but got %v", secret)
-	}
-}
-
-func TestExtractStripPath(t *testing.T) {
-	data := map[string]string{
-		"configuration.konghq.com/strip-path": "true",
-	}
-
-	secret := ExtractStripPath(data)
-	if secret != "true" {
-		t.Errorf("expected strip-path as true but got %v", secret)
-	}
-}
-
-func TestIngrssClassValidatorFunc(t *testing.T) {
+func TestIngressClassValidatorFunc(t *testing.T) {
 	tests := []struct {
 		ingress    string
 		controller string
@@ -151,11 +80,409 @@ func TestExtractPath(t *testing.T) {
 			},
 			want: "/foo",
 		},
+		{
+			name: "non-empty new group",
+			args: args{
+				anns: map[string]string{
+					"konghq.com/path": "/foo",
+				},
+			},
+			want: "/foo",
+		},
+		{
+			name: "group preference",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/path": "/foo",
+					"konghq.com/path":               "/bar",
+				},
+			},
+			want: "/bar",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ExtractPath(tt.args.anns); got != tt.want {
 				t.Errorf("ExtractPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_valueFromAnnotation(t *testing.T) {
+	type args struct {
+		key  string
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "empty",
+			args: args{},
+			want: "",
+		},
+		{
+			name: "legacy group lookup",
+			args: args{
+				key: "/protocol",
+				anns: map[string]string{
+					"configuration.konghq.com/protocol": "https",
+				},
+			},
+			want: "https",
+		},
+		{
+			name: "new group lookup",
+			args: args{
+				key: "/protocol",
+				anns: map[string]string{
+					"konghq.com/protocol": "https",
+				},
+			},
+			want: "https",
+		},
+		{
+			name: "new annotation takes precedence over deprecated one",
+			args: args{
+				key: "/protocol",
+				anns: map[string]string{
+					"konghq.com/protocol":               "https",
+					"configuration.konghq.com/protocol": "grpc",
+				},
+			},
+			want: "https",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := valueFromAnnotation(tt.args.key, tt.args.anns); got != tt.want {
+				t.Errorf("valueFromAnnotation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractKongPluginsFromAnnotations(t *testing.T) {
+	type args struct {
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "legacy annotation",
+			args: args{
+				anns: map[string]string{
+					"plugins.konghq.com": "kp-rl, kp-cors",
+				},
+			},
+			want: []string{"kp-rl", "kp-cors"},
+		},
+		{
+			name: "new annotation",
+			args: args{
+				anns: map[string]string{
+					"konghq.com/plugins": "kp-rl, kp-cors",
+				},
+			},
+			want: []string{"kp-rl", "kp-cors"},
+		},
+		{
+			name: "annotation prioriy",
+			args: args{
+				anns: map[string]string{
+					"plugins.konghq.com": "a,b",
+					"konghq.com/plugins": "kp-rl, kp-cors",
+				},
+			},
+			want: []string{"kp-rl", "kp-cors"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExtractKongPluginsFromAnnotations(tt.args.anns); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ExtractKongPluginsFromAnnotations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractConfigurationName(t *testing.T) {
+	type args struct {
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "legacy annotation",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com": "foo",
+				},
+			},
+			want: "foo",
+		},
+		{
+			name: "new annotation",
+			args: args{
+				anns: map[string]string{
+					"konghq.com/override": "foo",
+				},
+			},
+			want: "foo",
+		},
+		{
+			name: "annotation prioriy",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com": "bar",
+					"konghq.com/override":      "foo",
+				},
+			},
+			want: "foo",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExtractConfigurationName(tt.args.anns); got != tt.want {
+				t.Errorf("ExtractConfigurationName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractProtocolName(t *testing.T) {
+	type args struct {
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "legacy annotation",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/protocol": "foo",
+				},
+			},
+			want: "foo",
+		},
+		{
+			name: "new annotation",
+			args: args{
+				anns: map[string]string{
+					"konghq.com/protocol": "foo",
+				},
+			},
+			want: "foo",
+		},
+		{
+			name: "annotation prioriy",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/protocol": "bar",
+					"konghq.com/protocol":               "foo",
+				},
+			},
+			want: "foo",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExtractProtocolName(tt.args.anns); got != tt.want {
+				t.Errorf("ExtractProtocolName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractProtocolNames(t *testing.T) {
+	type args struct {
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "legacy annotation",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/protocols": "foo,bar",
+				},
+			},
+			want: []string{"foo", "bar"},
+		},
+		{
+			name: "new annotation",
+			args: args{
+				anns: map[string]string{
+					"konghq.com/protocols": "foo,bar",
+				},
+			},
+			want: []string{"foo", "bar"},
+		},
+		{
+			name: "annotation prioriy",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/protocols": "bar,foo",
+					"konghq.com/protocols":               "foo,baz",
+				},
+			},
+			want: []string{"foo", "baz"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExtractProtocolNames(tt.args.anns); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ExtractProtocolNames() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractClientCertificate(t *testing.T) {
+	type args struct {
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "legacy annotation",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/client-cert": "foo",
+				},
+			},
+			want: "foo",
+		},
+		{
+			name: "new annotation",
+			args: args{
+				anns: map[string]string{
+					"konghq.com/client-cert": "foo",
+				},
+			},
+			want: "foo",
+		},
+		{
+			name: "annotation prioriy",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/client-cert": "bar",
+					"konghq.com/client-cert":               "foo",
+				},
+			},
+			want: "foo",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExtractClientCertificate(tt.args.anns); got != tt.want {
+				t.Errorf("ExtractClientCertificate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHasServiceUpstreamAnnotation(t *testing.T) {
+	type args struct {
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "basic sanity",
+			args: args{
+				anns: map[string]string{
+					"ingress.kubernetes.io/service-upstream": "true",
+				},
+			},
+			want: true,
+		},
+		{
+			name: "garbage value",
+			args: args{
+				anns: map[string]string{
+					"ingress.kubernetes.io/service-upstream": "42",
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HasServiceUpstreamAnnotation(tt.args.anns); got != tt.want {
+				t.Errorf("HasServiceUpstreamAnnotation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExtractStripPath(t *testing.T) {
+	type args struct {
+		anns map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "empty",
+			want: "",
+		},
+		{
+			name: "non-empty",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/strip-path": "false",
+				},
+			},
+			want: "false",
+		},
+		{
+			name: "non-empty new group",
+			args: args{
+				anns: map[string]string{
+					"konghq.com/strip-path": "true",
+				},
+			},
+			want: "true",
+		},
+		{
+			name: "group preference",
+			args: args{
+				anns: map[string]string{
+					"configuration.konghq.com/strip-path": "false",
+					"konghq.com/strip-path":               "true",
+				},
+			},
+			want: "true",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExtractStripPath(tt.args.anns); got != tt.want {
+				t.Errorf("ExtractStripPath() = %v, want %v", got, tt.want)
 			}
 		})
 	}

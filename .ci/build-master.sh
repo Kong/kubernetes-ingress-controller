8bebfa8addd66112683822cf5c74294dfc4e9c81
Harry Bagdi <harrybagdi@gmail.com>
2020-05-11 16:24:44 -0700

    chore: set missing REPO env var in CI script

--- .ci/build-master.sh
+++ .ci/build-master.sh
@@ -7,6 +7,7 @@ export TAG=`git rev-parse --short HEAD`
 make container
 
 # push
+REPO="kong-docker-kubernetes-ingress-controller.bintray.io"
 echo "${BINTRAY_KEY}" | docker login -u "${BINTRAY_USER}" ${REPO} --password-stdin
 docker push ${REPO}/${IMGNAME}:${TAG}
 

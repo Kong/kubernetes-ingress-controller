8ac9fba35d9ff8c983d5cf360b60c96947e40577
Harry Bagdi <harrybagdi@gmail.com>
2020-05-11 14:56:00 -0700

    chore(ci) perform docker builds via github actions
    Other changes:
    - simplification of build script
    - required secrets have already been setup in Github
    - previously, these builds were being performed only on `master`, now
      we support `next` as well

--- .ci/build-master.sh
+++ .ci/build-master.sh
@@ -1,17 +1,12 @@
 #!/bin/bash -e
 
-if [[ "$TRAVIS_BRANCH" != "master" ]];
-then
-  exit 0
-fi
-
-REPO="kong-docker-kubernetes-ingress-controller.bintray.io"
+# build
 export IMGNAME="master"
 export TAG=`git rev-parse --short HEAD`
 
 make container
-docker tag ${REPO}/${IMGNAME}:${TAG} ${REPO}/${IMGNAME}:latest
-echo "${BINTRAY_KEY}" | docker login -u "${BINTRAY_USER}" ${REPO} --password-stdin
 
+# push
+echo "${BINTRAY_KEY}" | docker login -u "${BINTRAY_USER}" ${REPO} --password-stdin
 docker push ${REPO}/${IMGNAME}:${TAG}
-docker push ${REPO}/${IMGNAME}:latest
+

9e0ae2f84c1832b2f518f1eb0b861dfd5e333e8b
Travis Raines <traines@konghq.com>
2020-08-10 10:58:10 -0700

    pr: apply review suggestions
    Co-authored-by: Michał Flendrich <michal@flendrich.pro>

--- hack/verify-manifests.sh
+++ hack/verify-manifests.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")/..
+SCRIPT_ROOT="$(dirname "${BASH_SOURCE}")/.."
 
 DIFFROOT="${SCRIPT_ROOT}/deploy/single/"
 
@@ -13,17 +13,18 @@ cleanup() {
 }
 trap "cleanup" EXIT SIGINT
 
-cleanup
+if ! git status --porcelain --untracked-files=no "$DIFFROOT" ; then
+    echo "error: please run this script on a clean working copy"
+    exit 1
+fi
+
 
 "${SCRIPT_ROOT}/hack/build-single-manifests.sh"
 echo "diffing ${DIFFROOT} against freshly generated single manifests"
-ret=0
-git diff --quiet "${DIFFROOT}" || ret=$?
-if [[ $ret -eq 0 ]]
+if git diff --quiet "${DIFFROOT}"
 then
   echo "${DIFFROOT} up to date."
 else
   echo "${DIFFROOT} is out of date. Please run hack/build-single-manifests.sh"
-  git checkout "${DIFFROOT}"
   exit 1
 fi

c2a8c1b27832d148ffc5d09ad300425f24fd5616
Michał Flendrich <michal@flendrich.pro>
2020-10-26 18:30:05 +0100

    test(integration): patch instead of sed, disable anonymous reports

--- test/integration/util/install-sut.sh
+++ test/integration/util/install-sut.sh
@@ -8,10 +8,30 @@ docker tag "$KIC_IMAGE" "$REMOTE_IMAGE"
 docker push "$REMOTE_IMAGE"
 
 REPO_ROOT="$(dirname "$BASH_SOURCE")/../../.."
-MANIFEST="$(
-	sed 's!^\( *image: \)kong-docker-kubernetes-ingress-controller.*$!\1test-local-registry:5000/kic:local!' \
-		"$REPO_ROOT/deploy/single/all-in-one-dbless.yaml"
-)"
+IMAGE="test-local-registry:5000/kic:local"
+kubectl apply -f "$REPO_ROOT/deploy/single/all-in-one-dbless.yaml"
+kubectl patch -n kong deploy/ingress-kong --patch "
+{
+	\"spec\": {
+		\"template\": {
+			\"spec\": {
+				\"containers\": [
+					{
+						\"name\": \"ingress-controller\",
+						\"image\": \"$IMAGE\",
+						\"env\": [
+							{
+								\"name\": \"CONTROLLER_ANONYMOUS_REPORTS\",
+								\"value\": \"false\"
+							}
+						]
+					}
+				]
+			}
+		}
+	}
+}"
 
-kubectl apply -f <(echo "$MANIFEST")
+
+kubectl rollout status --watch --namespace=kong deploy/ingress-kong --timeout=0
 kubectl wait --for=condition=Available --namespace=kong deploy/ingress-kong --timeout=120s

8c99d12abfe5cb2ba9cdb18f87a6f6737c898c8b
Michał Flendrich <michal@flendrich.pro>
2020-10-29 15:47:04 +0100

    test(e2e): replace kubectl patch with kustomization

--- test/integration/util/install-sut.sh
+++ test/integration/util/install-sut.sh
@@ -7,31 +7,7 @@ REMOTE_IMAGE="localhost:5000/kic:local"
 docker tag "$KIC_IMAGE" "$REMOTE_IMAGE"
 docker push "$REMOTE_IMAGE"
 
-REPO_ROOT="$(dirname "$BASH_SOURCE")/../../.."
-IMAGE="test-local-registry:5000/kic:local"
-kubectl apply -f "$REPO_ROOT/deploy/single/all-in-one-dbless.yaml"
-kubectl patch -n kong deploy/ingress-kong --patch "
-{
-	\"spec\": {
-		\"template\": {
-			\"spec\": {
-				\"containers\": [
-					{
-						\"name\": \"ingress-controller\",
-						\"image\": \"$IMAGE\",
-						\"env\": [
-							{
-								\"name\": \"CONTROLLER_ANONYMOUS_REPORTS\",
-								\"value\": \"false\"
-							}
-						]
-					}
-				]
-			}
-		}
-	}
-}"
+SUT_ROOT="$(dirname "$BASH_SOURCE")/../sut"
+kustomize build --load_restrictor none "$SUT_ROOT" | kubectl apply -f -
 
-
-kubectl rollout status --watch --namespace=kong deploy/ingress-kong --timeout=0
 kubectl wait --for=condition=Available --namespace=kong deploy/ingress-kong --timeout=120s

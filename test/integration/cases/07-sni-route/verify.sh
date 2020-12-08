04c9d24a2d05c609aa30ff9c5255d74fd3035832
Travis Raines <traines@konghq.com>
2020-12-08 13:39:01 -0800

    pr: add 404 test

--- test/integration/cases/07-sni-route/verify.sh
+++ test/integration/cases/07-sni-route/verify.sh
@@ -5,3 +5,5 @@ IFS=: read sut_hostname sut_port <<< "$SUT_HTTPS_HOST"
 [ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.com:27443/foo --resolve example.com:$sut_port:$sut_hostname)" == 200 ]
 
 [ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.net:27443/bar --resolve example.net:$sut_port:$sut_hostname)" == 200 ]
+
+[ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.net:27443/foo --resolve example.net:$sut_port:$sut_hostname)" == 404 ]

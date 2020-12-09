1dc03fd9c8837ccfff73bc337c46317027156fa6
Travis Raines <traines@konghq.com>
2020-12-09 07:42:11 -0800

    pr: fix magic number

--- test/integration/cases/07-sni-route/verify.sh
+++ test/integration/cases/07-sni-route/verify.sh
@@ -2,8 +2,8 @@
 set -ex
 IFS=: read sut_hostname sut_port <<< "$SUT_HTTPS_HOST"
 
-[ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.com:27443/foo --resolve example.com:$sut_port:$sut_hostname)" == 200 ]
+[ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.com:$sut_port/foo --resolve example.com:$sut_port:$sut_hostname)" == 200 ]
 
-[ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.net:27443/bar --resolve example.net:$sut_port:$sut_hostname)" == 200 ]
+[ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.net:$sut_port/bar --resolve example.net:$sut_port:$sut_hostname)" == 200 ]
 
-[ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.net:27443/foo --resolve example.net:$sut_port:$sut_hostname)" == 404 ]
+[ "$(curl -k -sw '%{http_code}' -o /dev/null https://example.net:$sut_port/foo --resolve example.net:$sut_port:$sut_hostname)" == 404 ]

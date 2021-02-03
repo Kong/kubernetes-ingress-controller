728f822e4effe831f0a0a641b035ec55b0daa9e3
Michał Flendrich <michal@flendrich.pro>
2021-02-03 11:19:44 +0100

    kubebuilder create api --controller --group core --version v1 --kind Secret

--- railgun/main.go
+++ railgun/main.go
@@ -127,6 +127,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "V1Beta1Ingress")
 		os.Exit(1)
 	}
+	if err = (&controllers.SecretReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Secret"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Secret")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {

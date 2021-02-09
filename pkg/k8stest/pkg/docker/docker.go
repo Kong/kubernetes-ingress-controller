9cd88d4b8d5093b8c258e78f19ae17bcd93c2582
Shane Utt <shaneutt@linux.com>
2021-02-09 11:18:49 -0500

    feat: first draft of testing utilities

---
+++ pkg/k8stest/pkg/docker/docker.go
@@ -0,0 +1,42 @@
+package docker
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+// -----------------------------------------------------------------------------
+// Public Functions
+// -----------------------------------------------------------------------------
+
+// InspectDockerContainer provides the full container spec for a container by name.
+func InspectDockerContainer(containerID string) (*types.ContainerJSON, error) {
+	// TODO: just grabbing the local ENV for now, later we should be selective & dynamic
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return nil, err
+	}
+	containerJSON, err := cli.ContainerInspect(context.Background(), containerID)
+	return &containerJSON, err
+}
+
+// GetDockerContainerIPNetwork supports retreiving the *net.IP4Net of a container specified
+// by name (and a specified network name for the case of multiple networks).
+func GetDockerContainerIPNetwork(containerID, networkName string) (*net.IPNet, error) {
+	container, err := InspectDockerContainer(containerID)
+	if err != nil {
+		return nil, err
+	}
+
+	dockerNetwork := container.NetworkSettings.Networks[networkName]
+	_, network, err := net.ParseCIDR(fmt.Sprintf("%s/%d", dockerNetwork.Gateway, dockerNetwork.IPPrefixLen))
+	if err != nil {
+		return nil, err
+	}
+
+	return network, nil
+}

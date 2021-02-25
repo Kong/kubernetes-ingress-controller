f37ce7eb65020a84b356b9c9c53013f2dd5b956a
Travis Raines <traines@konghq.com>
2021-02-25 09:58:18 -0800

    chore(ci) update kustomize flag
    As of kustomize 4.0.1, the --load_restrictor flag has changed to
    --load-restrictor and uses "LoadRestrictionsNone" instead of "none"

--- test/integration/util/install-sut.sh
+++ test/integration/util/install-sut.sh
@@ -8,6 +8,6 @@ docker tag "$KIC_IMAGE" "$REMOTE_IMAGE"
 docker push "$REMOTE_IMAGE"
 
 SUT_ROOT="$(dirname "$BASH_SOURCE")/../sut"
-kustomize build --load_restrictor none "$SUT_ROOT" | kubectl apply -f -
+kustomize build --load-restrictor LoadRestrictionsNone "$SUT_ROOT" | kubectl apply -f -
 
 kubectl wait --for=condition=Available --namespace=kong deploy/ingress-kong --timeout=300s

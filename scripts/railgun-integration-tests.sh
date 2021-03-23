3367520849dccab0584571f53e034a2d40ab770b
Shane Utt <shaneutt@linux.com>
2021-03-23 12:53:54 -0400

    feat: add legacy kic integration test workflow

--- scripts/railgun-integration-tests.sh
+++ scripts/railgun-integration-tests.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-set -euox pipefail
-
 cd railgun/
-make test.integration
+
+if [ -z "$1" ]
+then
+    make test.integration
+elif [ "$1" == "LEGACY" ]
+then
+    make test.integration.legacy
+else
+    echo "$1 isn't a valid option (valid options: LEGACY)"
+    exit 1
+fi

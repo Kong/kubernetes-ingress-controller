022db9ce7c3756069b5ceceaeed8140d7fa0138a
Shane Utt <shaneutt@linux.com>
2021-03-26 16:56:23 -0400

    feat: add clientgen typed client generators

--- railgun/apis/configuration/v1alpha1/udpingress_types.go
+++ railgun/apis/configuration/v1alpha1/udpingress_types.go
@@ -20,6 +20,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+//+genclient
+//+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 

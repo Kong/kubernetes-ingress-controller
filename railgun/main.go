b782ee52894394d82c5323a0303b514387e89bb6
Shane Utt <shaneutt@linux.com>
2021-04-22 17:16:00 -0400

    chore: move ctrl generators to hack dir

--- railgun/main.go
+++ railgun/main.go
@@ -5,6 +5,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
+//go:generate go run github.com/kong/kubernetes-ingress-controller/railgun/hack/generators/controllers/networking
+
 func main() {
 	rootcmd.Execute(ctrl.SetupSignalHandler())
 }

43350273cb17bc3fe49c703d29bb233b0d7ba912
Shane Utt <shaneutt@linux.com>
2021-04-26 17:38:10 -0400

    chore: cleanup and logging for existing tests

--- railgun/test/integration/ingress_test.go
+++ railgun/test/integration/ingress_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -23,23 +24,28 @@ import (
 func TestMinimalIngress(t *testing.T) {
 	ctx := context.Background()
 
-	// deploy a minimal deployment to test Ingress routes to
+	t.Log("deploying a minimal HTTP container deployment to test Ingress routes")
 	container := k8sgen.NewContainer("nginx", "nginx", 80)
 	deployment := k8sgen.NewDeploymentForContainer(container)
 	deployment, err := cluster.Client().AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
 	assert.NoError(t, err)
 
-	// ensure cleanup of the deployment
 	defer func() {
+		t.Logf("cleaning up the deployment %s", deployment.Name)
 		assert.NoError(t, cluster.Client().AppsV1().Deployments("default").Delete(ctx, deployment.Name, metav1.DeleteOptions{}))
 	}()
 
-	// expose the deployment via service
+	t.Logf("exposing deployment %s via service", deployment.Name)
 	service := k8sgen.NewServiceForDeployment(deployment, corev1.ServiceTypeLoadBalancer)
 	_, err = cluster.Client().CoreV1().Services("default").Create(ctx, service, metav1.CreateOptions{})
 	assert.NoError(t, err)
 
-	// route to the service via Kong Ingress
+	defer func() {
+		t.Logf("cleaning up the service %s", service.Name)
+		assert.NoError(t, cluster.Client().CoreV1().Services("default").Delete(ctx, service.Name, metav1.DeleteOptions{}))
+	}()
+
+	t.Logf("routing to service %s via Ingress", service.Name)
 	ingress := k8sgen.NewIngressForService("/nginx", map[string]string{
 		"kubernetes.io/ingress.class": "kong",
 		"konghq.com/strip-path":       "true",
@@ -47,8 +53,8 @@ func TestMinimalIngress(t *testing.T) {
 	ingress, err = cluster.Client().NetworkingV1().Ingresses("default").Create(ctx, ingress, metav1.CreateOptions{})
 	assert.NoError(t, err)
 
-	// ensure cleanup of the ingress
 	defer func() {
+		t.Logf("ensuring that Ingress %s is cleaned up", ingress.Name)
 		if err := cluster.Client().NetworkingV1().Ingresses("default").Delete(ctx, ingress.Name, metav1.DeleteOptions{}); err != nil {
 			if !errors.IsNotFound(err) {
 				require.NoError(t, err)
@@ -56,10 +62,10 @@ func TestMinimalIngress(t *testing.T) {
 		}
 	}()
 
-	// wait for the ingress backend to be routable
+	t.Logf("waiting for routes from Ingress %s to be operational", ingress.Name)
 	p := proxyReady()
 	assert.Eventually(t, func() bool {
-		resp, err := http.Get(fmt.Sprintf("%s/nginx", p.ProxyURL.String()))
+		resp, err := httpc.Get(fmt.Sprintf("%s/nginx", p.ProxyURL.String()))
 		if err != nil {
 			t.Logf("WARNING: error while waiting for %s: %v", p.ProxyURL.String(), err)
 			return false
@@ -75,10 +81,10 @@ func TestMinimalIngress(t *testing.T) {
 		return false
 	}, ingressWait, waitTick)
 
-	// ensure that a deleted ingress results in the route being torn down
+	t.Logf("deleting Ingress %s and waiting for routes to be torn down", ingress.Name)
 	assert.NoError(t, cluster.Client().NetworkingV1().Ingresses("default").Delete(ctx, ingress.Name, metav1.DeleteOptions{}))
 	assert.Eventually(t, func() bool {
-		resp, err := http.Get(fmt.Sprintf("%s/nginx", p.ProxyURL.String()))
+		resp, err := httpc.Get(fmt.Sprintf("%s/nginx", p.ProxyURL.String()))
 		if err != nil {
 			t.Logf("WARNING: error while waiting for %s: %v", p.ProxyURL.String(), err)
 			return false
@@ -106,25 +112,28 @@ func TestHTTPSRedirect(t *testing.T) {
 	ctx := context.Background()
 	opts := metav1.CreateOptions{}
 
+	t.Log("creating an HTTP container via deployment to test redirect functionality")
 	container := k8sgen.NewContainer("alsonginx", "nginx", 80)
 	deployment := k8sgen.NewDeploymentForContainer(container)
 	_, err := cluster.Client().AppsV1().Deployments("default").Create(ctx, deployment, opts)
 	assert.NoError(t, err)
 
-	// ensure cleanup of the deployment
 	defer func() {
+		t.Logf("cleaning up deployment %s", deployment.Name)
 		assert.NoError(t, cluster.Client().AppsV1().Deployments("default").Delete(ctx, deployment.Name, metav1.DeleteOptions{}))
 	}()
 
+	t.Logf("exposing deployment %s via Service", deployment.Name)
 	service := k8sgen.NewServiceForDeployment(deployment, corev1.ServiceTypeClusterIP)
 	service, err = cluster.Client().CoreV1().Services("default").Create(ctx, service, opts)
 	assert.NoError(t, err)
 
-	// ensure cleanup of the service
 	defer func() {
+		t.Logf("cleaning up Service %s", service.Name)
 		assert.NoError(t, cluster.Client().CoreV1().Services("default").Delete(ctx, service.Name, metav1.DeleteOptions{}))
 	}()
 
+	t.Logf("exposing Service %s via Ingress", service.Name)
 	ingress := k8sgen.NewIngressForService("/example", map[string]string{
 		"kubernetes.io/ingress.class":           "kong",
 		"konghq.com/protocols":                  "https",
@@ -133,17 +142,18 @@ func TestHTTPSRedirect(t *testing.T) {
 	ingress, err = cluster.Client().NetworkingV1().Ingresses("default").Create(ctx, ingress, opts)
 	assert.NoError(t, err)
 
-	// ensure cleanup of the ingress
 	defer func() {
+		t.Logf("cleaning up Ingress %s", ingress.Name)
 		assert.NoError(t, cluster.Client().NetworkingV1().Ingresses("default").Delete(ctx, ingress.Name, metav1.DeleteOptions{}))
 	}()
 
+	t.Logf("waiting for Ingress %s to be operational and properly redirect", ingress.Name)
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
+		Timeout: time.Second * 3,
 	}
-
 	assert.Eventually(t, func() bool {
 		p := proxyReady()
 		resp, err := client.Get(fmt.Sprintf("%s/example", p.ProxyURL.String()))

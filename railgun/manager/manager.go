a47b52e23fb79fb621599597942c537d70eeaf93
Travis Raines <traines@konghq.com>
2021-05-05 15:27:56 -0700

    chore(mgr) use same health listen as 1.x
    Change the health service default address to use the same port as 1.x
    did.

--- railgun/manager/manager.go
+++ railgun/manager/manager.go
@@ -92,7 +92,7 @@ func MakeFlagSetFor(c *Config) *pflag.FlagSet {
 	flagSet := flagSet{*pflag.NewFlagSet("", pflag.ExitOnError)}
 
 	flagSet.StringVar(&c.MetricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flagSet.StringVar(&c.ProbeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flagSet.StringVar(&c.ProbeAddr, "health-probe-bind-address", ":10254", "The address the probe endpoint binds to.")
 	flagSet.BoolVar(&c.EnableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")

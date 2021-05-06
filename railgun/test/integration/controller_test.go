0bb98af4d512b504d56b48f66a72f61e91e8aed5
Travis Raines <traines@konghq.com>
2021-05-06 11:45:19 -0700

    pr(mgr) use constants for ports

--- railgun/test/integration/controller_test.go
+++ railgun/test/integration/controller_test.go
@@ -4,41 +4,37 @@ package integration
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 	"strings"
 	"testing"
 
+	"github.com/kong/kubernetes-ingress-controller/pkg/manager"
 	"github.com/stretchr/testify/assert"
 )
 
-// TestHealthEndpoint intentionally includes a magic value. localhost:10254 is the default
-// controller health listen. This test should fail, and must be updated, if that default is changed.
 func TestHealthEndpoint(t *testing.T) {
 	_ = proxyReady()
 	assert.Eventually(t, func() bool {
-		resp, err := httpc.Get("http://localhost:10254/healthz")
+		resp, err := httpc.Get(fmt.Sprintf("http://localhost:%v/healthz", manager.HealthzPort))
 		if err != nil {
-			t.Logf("WARNING: error while waiting for http://localhost:10254/healthz: %v", err)
+			t.Logf("WARNING: error while waiting for http://localhost:%v/healthz: %v", manager.HealthzPort, err)
 			return false
 		}
 		defer resp.Body.Close()
 		if resp.StatusCode == http.StatusOK {
-			b := new(bytes.Buffer)
-			b.ReadFrom(resp.Body)
-			return strings.Contains(b.String(), "ok")
+			return true
 		}
 		return false
 	}, ingressWait, waitTick)
 }
 
-// TestMetricsEndpoint intentionally includes a magic value. localhost:8080 is the default
-// controller metrics listen. This test should fail, and must be updated, if that default is changed.
 func TestMetricsEndpoint(t *testing.T) {
 	_ = proxyReady()
 	assert.Eventually(t, func() bool {
-		resp, err := httpc.Get("http://localhost:8080/metrics")
+		resp, err := httpc.Get(fmt.Sprintf("http://localhost:%v/metrics", manager.MetricsPort))
 		if err != nil {
-			t.Logf("WARNING: error while waiting for http://localhost:8080/metrics: %v", err)
+			t.Logf("WARNING: error while waiting for http://localhost:%v/metrics: %v", manager.MetricsPort, err)
 			return false
 		}
 		defer resp.Body.Close()

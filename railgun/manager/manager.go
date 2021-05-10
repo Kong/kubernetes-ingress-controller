83f29fefe8b99e61cc8f8d73ba4d8db359138b93
Travis Raines <traines@konghq.com>
2021-05-10 09:37:38 -0700

    chore(mgr) use same health listen as 1.x (#1283)
    - Change the health service default address to use the same port as 1.x
      did.
    - Change the default metrics service port.
    
    Co-authored-by: Michał Flendrich <michal@flendrich.pro>

--- railgun/manager/manager.go
+++ railgun/manager/manager.go
@@ -49,6 +49,19 @@ var (
 	Commit = "UNKNOWN"
 )
 
+// HealthzPort is the default port the manager's health service listens on.
+// Changing this will result in a breaking change. Existing deployments may use the literal
+// port number in their liveness and readiness probes, and upgrading to a controller version
+// with a changed HealthzPort will result in crash loops until users update their probe config.
+// Note that there are several stock manifests in this repo that also use the literal port number. If you
+// update this value, search for the old port number and update the stock manifests also.
+const HealthzPort = 10254
+
+// MetricsPort is the default port the manager's metrics service listens on.
+// Similar to HealthzPort, it may be used in existing user deployment configurations, and its
+// literal value is used in several stock manifests, which must be updated along with this value.
+const MetricsPort = 10255
+
 // Config collects all configuration that the controller manager takes from the environment.
 // BUG: the above is not 100% accurate today - controllers read some settings from environment variables directly
 type Config struct {
@@ -91,8 +104,10 @@ type Config struct {
 func MakeFlagSetFor(c *Config) *pflag.FlagSet {
 	flagSet := flagSet{*pflag.NewFlagSet("", pflag.ExitOnError)}
 
-	flagSet.StringVar(&c.MetricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flagSet.StringVar(&c.ProbeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flagSet.StringVar(&c.MetricsAddr, "metrics-bind-address", fmt.Sprintf(":%v", MetricsPort),
+		"The address the metric endpoint binds to.")
+	flagSet.StringVar(&c.ProbeAddr, "health-probe-bind-address", fmt.Sprintf(":%v", HealthzPort),
+		"The address the probe endpoint binds to.")
 	flagSet.BoolVar(&c.EnableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")

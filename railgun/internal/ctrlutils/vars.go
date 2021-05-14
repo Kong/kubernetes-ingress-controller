9d1e581a199c597cecae095a01222b247e22eabf
Michał Flendrich <michal@flendrich.pro>
2021-05-14 16:54:30 +0200

    chore: move DefaultNamespace into test

--- railgun/internal/ctrlutils/vars.go
+++ railgun/internal/ctrlutils/vars.go
@@ -5,10 +5,6 @@ package ctrlutils
 // -----------------------------------------------------------------------------
 
 var (
-	// DefaultNamespace indicates the namespace that will be used by default
-	// when no other is provided for the deployment or management of resources.
-	DefaultNamespace = "kong-system"
-
 	// KongIngressFinalizer is the finalizer used to ensure Kong configuration cleanup for deleted resources.
 	KongIngressFinalizer = "configuration.konghq.com/ingress"
 )

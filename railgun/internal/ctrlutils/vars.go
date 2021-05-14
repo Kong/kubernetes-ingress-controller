46bf3a92513ba73fcde9e2f6263fc03be1e6f7ce
Michał Flendrich <michal@flendrich.pro>
2021-05-14 16:49:14 +0200

    chore: rm ControllerNamespaceEnv, ExternalCtrlEnv

--- railgun/internal/ctrlutils/vars.go
+++ railgun/internal/ctrlutils/vars.go
@@ -1,21 +1,6 @@
 package ctrlutils
 
 // -----------------------------------------------------------------------------
-// Environment Variables
-// -----------------------------------------------------------------------------
-
-var (
-	// CtrlNamespaceEnv provides the name of the environment variable where controllers which
-	// manage the Kong configuration should use to find (or create) the configuration secret.
-	CtrlNamespaceEnv = "KONG_CONFIGURATION_NAMESPACE"
-
-	// ExternalCtrlEnv is an environment variable used to indicate whether the controller is running
-	// outside the cluster where the proxy instances are running. If unset it is assumed that the proxy
-	// instances can be reached via their Pod IP address. Only accepts "true" to enable.
-	ExternalCtrlEnv = "KONG_EXTERNAL_CONTROLLER"
-)
-
-// -----------------------------------------------------------------------------
 // General Controller Variables
 // -----------------------------------------------------------------------------
 

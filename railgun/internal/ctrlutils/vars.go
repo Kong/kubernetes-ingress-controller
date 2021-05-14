9e8f9cd735d3ac8137deca6f4d236d0f6f48dd9c
Michał Flendrich <michal@flendrich.pro>
2021-05-14 16:52:20 +0200

    chore: rm dead code and unused vars

--- railgun/internal/ctrlutils/vars.go
+++ railgun/internal/ctrlutils/vars.go
@@ -9,14 +9,6 @@ var (
 	// when no other is provided for the deployment or management of resources.
 	DefaultNamespace = "kong-system"
 
-	// ConfigSecretName indicates the name of the Secret object where Ingress controllers will upload
-	// ingress objects for eventual parsing and configuration in the Kong Proxy APIs.
-	ConfigSecretName = "kong-config"
-
-	// ProxyInstanceLabel is a label used for controllers (such as the secret configuration
-	// controller) to identify which pods are running the Kong proxy which needs to be configured.
-	ProxyInstanceLabel = "konghq.com/proxy-instance"
-
 	// KongIngressFinalizer is the finalizer used to ensure Kong configuration cleanup for deleted resources.
 	KongIngressFinalizer = "configuration.konghq.com/ingress"
 )

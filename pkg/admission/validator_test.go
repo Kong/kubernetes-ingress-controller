bbab9afc8e29d0ccaecd1820dffc257f80125821
Michał Flendrich <michal@flendrich.pro>
2021-05-17 18:17:31 +0200

    chore: gofmt -w

--- pkg/admission/validator_test.go
+++ pkg/admission/validator_test.go
@@ -359,8 +359,8 @@ func TestKongHTTPValidator_ValidatePlugin(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			validator := KongHTTPValidator{
-				SecretGetter:     store,
-				PluginSvc: tt.PluginSvc,
+				SecretGetter: store,
+				PluginSvc:    tt.PluginSvc,
 			}
 			got, got1, err := validator.ValidatePlugin(context.Background(), tt.args.plugin)
 			if (err != nil) != tt.wantErr {

d3cffd6f292de9346d03b6eff16a75e885f313fb
Travis Raines <traines@konghq.com>
2021-05-18 15:48:52 -0700

    fix(mgr) align concurrency flag with 1.x
    Use the same name and docstring for the Railgun concurrency flag as we
    used in 1.x.

--- railgun/manager/config.go
+++ railgun/manager/config.go
@@ -103,7 +103,7 @@ func (c *Config) FlagSet() *pflag.FlagSet {
 	flagSet.BoolVar(&c.EnableLeaderElection, "leader-elect", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flagSet.StringVar(&c.LeaderElectionID, "election-id", "5b374a9e.konghq.com", `Election id to use for status update.`)
 	flagSet.StringSliceVar(&c.FilterTags, "kong-admin-filter-tag", []string{"managed-by-ingress-controller"}, "The tag used to manage and filter entities in Kong. This flag can be specified multiple times to specify multiple tags")
-	flagSet.IntVar(&c.Concurrency, "kong-concurrency", 10, "TODO")
+	flagSet.IntVar(&c.Concurrency, "kong-admin-concurrency", 10, "Max number of concurrent requests sent to Kong's Admin API.")
 	flagSet.StringVar(&c.WatchNamespace, "watch-namespace", corev1.NamespaceAll,
 		`Namespace(s) to watch for Kubernetes resources. Defaults to all namespaces. To watch multiple namespaces, use
 		a comma-separated list of namespaces.`)

74609311f5788b2935f2dd3fb8f1fb129d23b256
Shane Utt <shaneutt@linux.com>
2021-06-30 10:00:55 -0400

    fix: cleanup imports

--- railgun/manager/run.go
+++ railgun/manager/run.go
@@ -17,7 +17,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"github.com/kong/kubernetes-ingress-controller/pkg/sendconfig"
 	"github.com/kong/kubernetes-ingress-controller/pkg/util"

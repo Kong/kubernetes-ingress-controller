0e7761efb44045c41f2dc3485a6b8f79defb49db
Michał Flendrich <michal@flendrich.pro>
2021-07-07 01:01:12 +0200

    pr: apply review suggestion, rename --log-reduce-redundancy to --debug-log-reduce-redundancy

--- railgun/pkg/config/config.go
+++ railgun/pkg/config/config.go
@@ -95,8 +95,8 @@ func (c *Config) FlagSet() *pflag.FlagSet {
 	// Logging configurations
 	flagSet.StringVar(&c.LogLevel, "log-level", "info", `Level of logging for the controller. Allowed values are trace, debug, info, warn, error, fatal and panic.`)
 	flagSet.StringVar(&c.LogFormat, "log-format", "text", `Format of logs of the controller. Allowed values are text and json.`)
-	flagSet.BoolVar(&c.LogReduceRedundancy, "log-reduce-redundancy", false, `If enabled, repetitive log entries are suppressed. Built for testing environments - production use not recommended.`)
-	flagSet.MarkHidden("log-reduce-redundancy")
+	flagSet.BoolVar(&c.LogReduceRedundancy, "debug-log-reduce-redundancy", false, `If enabled, repetitive log entries are suppressed. Built for testing environments - production use not recommended.`)
+	flagSet.MarkHidden("debug-log-reduce-redundancy")
 
 	// Kong high-level controller manager configurations
 	flagSet.BoolVar(&c.KongAdminAPIConfig.TLSSkipVerify, "kong-admin-tls-skip-verify", false, "Disable verification of TLS certificate of Kong's Admin endpoint.")

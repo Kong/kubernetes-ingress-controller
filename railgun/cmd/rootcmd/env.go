8a246741b2749718eb7acd39b37c0a97e0fec46b
Shane Utt <shaneutt@linux.com>
2021-07-07 15:14:22 -0400

    fix: ensure flagset set validation is called
    Previously the wrong Set() method was called when updating
    values from ENV vars, this patch ensures that each flag runs
    it's local Set() method to ensure any validation there is run.

--- railgun/cmd/rootcmd/env.go
+++ railgun/cmd/rootcmd/env.go
@@ -9,20 +9,30 @@ import (
 	"github.com/spf13/pflag"
 )
 
-const envKeyPrefix = "CONTROLLER_"
+const (
+	// ExitCodeBadFlagValue is the program exit code that will be produced when an environment
+	// variable has a value that failed validation when an attempt was made to use it to Set()
+	// the relevant flagset variable.
+	ExitCodeBadFlagValue = 25
+
+	envKeyPrefix = "CONTROLLER_"
+)
 
 // bindEnvVars, for each flag defined on `cmd` (local or parent persistent), looks up the corresponding environment
 // variable and (if the flag is unset) takes that environment variable value as the flag value.
 func bindEnvVars(cmd *cobra.Command, _ []string) {
-	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+	cmd.LocalFlags().VisitAll(func(f *pflag.Flag) {
 		envKey := fmt.Sprintf("%s%s", envKeyPrefix, strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_")))
 
 		if f.Changed {
-			return // flags take precedence over environment variables
+			//return // flags take precedence over environment variables
 		}
 
 		if envValue, envSet := os.LookupEnv(envKey); envSet {
-			cmd.Flags().Set(f.Name, envValue)
+			if err := f.Value.Set(envValue); err != nil {
+				fmt.Fprintf(os.Stderr, "ERROR: could not set %s value from environment to %s: %s\n", envKey, envValue, err)
+				os.Exit(ExitCodeBadFlagValue)
+			}
 		}
 	})
 }

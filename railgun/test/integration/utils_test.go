5669d55f9cc69a26fe6a07f59fb9402a17414102
Shane Utt <shaneutt@linux.com>
2021-07-08 14:26:54 -0400

    chore: remove proxy readiness check in test suite
    KTF now provides a means to wait for cluster and addon
    readiness, as well as providing URLs for the Kong addon
    so this functionality can now be dropped here.

--- railgun/test/integration/utils_test.go
+++ railgun/test/integration/utils_test.go
@@ -6,32 +6,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
-	"sync"
 	"testing"
-
-	ktfkind "github.com/kong/kubernetes-testing-framework/pkg/kind"
 )
-
-var (
-	l            = sync.RWMutex{}
-	proxyReadyCh = make(chan ktfkind.ProxyReadinessEvent)
-
-	readinessEvent *ktfkind.ProxyReadinessEvent
-)
-
-// proxyReady is a threadsafe way to wait for the proxy to be ready
-// and then receive the URLs where it can be reached.
-func proxyReady() ktfkind.ProxyReadinessEvent {
-	l.Lock()
-	defer l.Unlock()
-
-	if readinessEvent == nil {
-		event := <-proxyReadyCh
-		readinessEvent = &event
-	}
-
-	return *readinessEvent
-}
 
 // expect404WithNoRoute is used to check whether a given http response is (specifically) a Kong 404.
 func expect404WithNoRoute(t *testing.T, proxyURL string, resp *http.Response) bool {

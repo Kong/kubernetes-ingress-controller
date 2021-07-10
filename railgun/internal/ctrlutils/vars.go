1553b0fad3866dc9ba12b03ec109077c4c306f02
Shane Utt <shaneutt@linux.com>
2021-07-09 23:13:23 -0400

    fix: remove finalizer handling
    Originally we added finalizers with the intention of
    holding back object deletion until we were certain that
    we had removed the relevant configuration from the Kong
    Admin API. Ultimately at the time of writing it's not
    feasible to get this done, so this removes a half of
    the implementation which was causing us complexity without
    any added value.

--- railgun/internal/ctrlutils/vars.go
+++
@@ -1,8 +0,0 @@
-package ctrlutils
-
-// -----------------------------------------------------------------------------
-// General Controller Variables
-// -----------------------------------------------------------------------------
-
-// KongIngressFinalizer is the finalizer used to ensure Kong configuration cleanup for deleted resources.
-const KongIngressFinalizer = "configuration.konghq.com/ingress"

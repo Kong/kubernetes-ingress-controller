6a3e8ac4561915bcc76bf6c3ff94b33f75574fa5
ccfishk <jimin.susan.hu@gmail.com>
2021-07-16 12:09:45 -0700

    add redis installation scripts to build test environment

--- railgun/scripts/redis.sh
+++ railgun/scripts/redis.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
+
 # install redis as a daemon on a linux environment
-echo "install gcc"
-sudo yum -y install gcc make
-echo "cd /usr/local/src"
+echo "install gcc\n"
+sudo yum install --assumeyes gcc make
+
+echo "cd /usr/local/src\n"
 sudo cd /usr/local/src
-echo "wget redis"
+
+echo "wget redis\n"
 sudo wget http://download.redis.io/redis-stable.tar.gz
-echo "untar redis"
+
+echo "untar redis\n"
 sudo tar xvzf redis-stable.tar.gz
-echo "remove downloaded redis"
+
+echo "remove downloaded redis\n"
 sudo rm -f redis-stable.tar.gzcd redis-stable
-echo "groupinstall development tools"
-sudo yum groupinstall "Development Tools"
-echo "make distclean"
+
+echo "groupinstall development tools\n"
+sudo yum groupinstall --assumeyes "Development Tools"
+
+echo "make distclean\n"
 sudo make distclean
+
 echo "make"
 sudo make
-echo "install tcl"
+
+echo "sudo install tcl \n"
 sudo yum install -y tcl
+
 echo "start redis as daemon on port 6379"
 redis-server --daemonize yes
-echo "ensure redis server is up"
-$redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
 
-echo $redis->ping();
-$redis->disconnect()
+echo "ensure redis server is up"
+redis-cli ping
+
+if [ $? -eq 0 ]
+then
+  echo "Redis server has been up successfully."
+else
+  echo "Failed to checking redis status." >&2
+fi

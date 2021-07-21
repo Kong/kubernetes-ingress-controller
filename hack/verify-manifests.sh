8818bbd6df7d275eb72e0954035300825fffef4e
Travis Raines <traines@konghq.com>
2021-07-21 11:27:08 -0700

    feat(build) move manifests and verify v2 manifests
    - Move deploy/single to deploy/single-v1
    - Move railgun/config/single to deploy/single-v2
    - Update manifest generator scripts with new locations
    - Add v2 manifests to manifest verifier
    - Link deploy/single to deploy/single-v1

--- hack/verify-manifests.sh
+++ hack/verify-manifests.sh
@@ -6,7 +6,8 @@ set -o pipefail
 
 SCRIPT_ROOT="$(dirname "${BASH_SOURCE}")/.."
 
-DIFFROOT="${SCRIPT_ROOT}/deploy/single/"
+DIFFROOT="${SCRIPT_ROOT}/deploy/single-v1/"
+RGDIFFROOT="${SCRIPT_ROOT}/deploy/single-v2/"
 
 cleanup() {
   git checkout "${DIFFROOT}"
@@ -20,6 +21,7 @@ fi
 
 
 "${SCRIPT_ROOT}/hack/build-single-manifests.sh"
+"${SCRIPT_ROOT}/railgun/hack/deploy/build-single-manifests.sh"
 echo "diffing ${DIFFROOT} against freshly generated single manifests"
 if git diff --quiet "${DIFFROOT}"
 then
@@ -28,5 +30,15 @@ else
   echo "${DIFFROOT} is out of date. Please run hack/build-single-manifests.sh"
   echo "Diff output:"
   git --no-pager diff "${DIFFROOT}"
+  exit 1
+fi
+
+if git diff --quiet "${RGDIFFROOT}"
+then
+  echo "${RGDIFFROOT} up to date."
+else
+  echo "${RGDIFFROOT} is out of date. Please run railgun/hack/build-single-manifests.sh"
+  echo "Diff output:"
+  git --no-pager diff "${RGDIFFROOT}"
   exit 1
 fi

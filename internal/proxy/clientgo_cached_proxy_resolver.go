af1b078711d778948167a51c9fc1624f763279fa
Shane Utt <shaneutt@linux.com>
2021-07-30 14:05:21 -0400

    fix: proxy req timeout inherit

--- internal/proxy/clientgo_cached_proxy_resolver.go
+++ internal/proxy/clientgo_cached_proxy_resolver.go
@@ -31,13 +31,13 @@ func NewCacheBasedProxy(ctx context.Context,
 	enableReverseSync bool,
 	kongUpdater KongUpdater,
 	diagnostic util.ConfigDumpDiagnostic,
-	timeout time.Duration,
+	proxyRequestTimeout time.Duration,
 ) (Proxy, error) {
 	stagger, err := time.ParseDuration(fmt.Sprintf("%gs", DefaultSyncSeconds))
 	if err != nil {
 		return nil, err
 	}
-	return NewCacheBasedProxyWithStagger(ctx, logger, k8s, kongConfig, ingressClassName, enableReverseSync, stagger, timeout, diagnostic, kongUpdater)
+	return NewCacheBasedProxyWithStagger(ctx, logger, k8s, kongConfig, ingressClassName, enableReverseSync, stagger, proxyRequestTimeout, diagnostic, kongUpdater)
 }
 
 // NewCacheBasedProxy will provide a new Proxy object. Note that this starts some background goroutines and the caller
@@ -50,7 +50,7 @@ func NewCacheBasedProxyWithStagger(ctx context.Context,
 	ingressClassName string,
 	enableReverseSync bool,
 	stagger time.Duration,
-	timeout time.Duration,
+	proxyRequestTimeout time.Duration,
 	diagnostic util.ConfigDumpDiagnostic,
 	kongUpdater KongUpdater,
 ) (Proxy, error) {
@@ -70,10 +70,10 @@ func NewCacheBasedProxyWithStagger(ctx context.Context,
 		ingressClassName: ingressClassName,
 		stopCh:           make(chan struct{}),
 
-		ctx:        ctx,
-		stagger:    stagger,
-		timeout:    timeout,
-		syncTicker: time.NewTicker(stagger),
+		ctx:                 ctx,
+		stagger:             stagger,
+		proxyRequestTimeout: proxyRequestTimeout,
+		syncTicker:          time.NewTicker(stagger),
 	}
 
 	// initialize the proxy which validates connectivity with the Admin API and
@@ -122,13 +122,13 @@ type clientgoCachedProxyResolver struct {
 	kongUpdater KongUpdater
 	diagnostic  util.ConfigDumpDiagnostic
 
-	// cache server configuration, flow control, channels and utility attributes
-	ingressClassName string
-	ctx              context.Context
-	stagger          time.Duration
-	timeout          time.Duration
-	syncTicker       *time.Ticker
-	stopCh           chan struct{}
+	// server configuration, flow control, channels and utility attributes
+	ingressClassName    string
+	ctx                 context.Context
+	stagger             time.Duration
+	proxyRequestTimeout time.Duration
+	syncTicker          *time.Ticker
+	stopCh              chan struct{}
 
 	// New code should log using "logger". "deprecatedLogger" is here for compatibility with legacy code that relies
 	// on the logrus API.
@@ -171,7 +171,7 @@ func (p *clientgoCachedProxyResolver) startProxyUpdateServer() {
 			return
 		case <-p.syncTicker.C:
 			updateConfigSHA, err := p.kongUpdater(p.ctx, p.lastConfigSHA, p.cache,
-				p.ingressClassName, p.deprecatedLogger, p.kongConfig, p.enableReverseSync, p.diagnostic)
+				p.ingressClassName, p.deprecatedLogger, p.kongConfig, p.enableReverseSync, p.diagnostic, p.proxyRequestTimeout)
 			if err != nil {
 				p.logger.Error(err, "could not update kong admin")
 				break
@@ -235,7 +235,7 @@ func (p *clientgoCachedProxyResolver) initialize() error {
 // kongRootWithTimeout provides the root configuration from Kong, but uses a configurable timeout to avoid long waits if the Admin API
 // is not yet ready to respond. If a timeout error occurs, the caller is responsible for providing a retry mechanism.
 func (p *clientgoCachedProxyResolver) kongRootWithTimeout() (map[string]interface{}, error) {
-	ctx, cancel := context.WithTimeout(p.ctx, p.timeout)
+	ctx, cancel := context.WithTimeout(p.ctx, p.proxyRequestTimeout)
 	defer cancel()
 	return p.kongConfig.Client.Root(ctx)
 }

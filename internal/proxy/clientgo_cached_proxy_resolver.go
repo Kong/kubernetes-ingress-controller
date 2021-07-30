592f5a8ba792d396311cc568f2732a30cdd53c3c
ccfishk <jimin.susan.hu@gmail.com>
2021-07-30 12:34:34 -0700

    merge from next

--- internal/proxy/clientgo_cached_proxy_resolver.go
+++ internal/proxy/clientgo_cached_proxy_resolver.go
@@ -9,11 +9,11 @@ import (
 	"github.com/blang/semver/v4"
 	"github.com/bombsimon/logrusr"
 	"github.com/go-logr/logr"
-	"github.com/kong/go-kong/kong"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/kong/go-kong/kong"
 	"github.com/kong/kubernetes-ingress-controller/internal/sendconfig"
 	"github.com/kong/kubernetes-ingress-controller/internal/store"
 	"github.com/kong/kubernetes-ingress-controller/internal/util"
@@ -33,13 +33,13 @@ func NewCacheBasedProxy(ctx context.Context,
 	enableReverseSync bool,
 	kongUpdater KongUpdater,
 	diagnostic util.ConfigDumpDiagnostic,
-	proxyRequestTimeout time.Duration,
+	timeout time.Duration,
 ) (Proxy, error) {
 	stagger, err := time.ParseDuration(fmt.Sprintf("%gs", DefaultSyncSeconds))
 	if err != nil {
 		return nil, err
 	}
-	return NewCacheBasedProxyWithStagger(ctx, logger, k8s, kongConfig, ingressClassName, enableReverseSync, stagger, proxyRequestTimeout, diagnostic, kongUpdater)
+	return NewCacheBasedProxyWithStagger(ctx, logger, k8s, kongConfig, ingressClassName, enableReverseSync, stagger, timeout, diagnostic, kongUpdater)
 }
 
 // NewCacheBasedProxy will provide a new Proxy object. Note that this starts some background goroutines and the caller
@@ -52,7 +52,7 @@ func NewCacheBasedProxyWithStagger(ctx context.Context,
 	ingressClassName string,
 	enableReverseSync bool,
 	stagger time.Duration,
-	proxyRequestTimeout time.Duration,
+	timeout time.Duration,
 	diagnostic util.ConfigDumpDiagnostic,
 	kongUpdater KongUpdater,
 ) (Proxy, error) {
@@ -72,10 +72,14 @@ func NewCacheBasedProxyWithStagger(ctx context.Context,
 		ingressClassName: ingressClassName,
 		stopCh:           make(chan struct{}),
 
-		ctx:                 ctx,
-		stagger:             stagger,
-		proxyRequestTimeout: proxyRequestTimeout,
-		syncTicker:          time.NewTicker(stagger),
+		ctx:        ctx,
+		update:     make(chan *cachedObject, DefaultObjectBufferSize),
+		del:        make(chan *cachedObject, DefaultObjectBufferSize),
+		stagger:    stagger,
+		timeout:    timeout,
+		syncTicker: time.NewTicker(stagger),
+
+		internalCacheLock: &sync.RWMutex{},
 	}
 
 	// initialize the proxy which validates connectivity with the Admin API and
@@ -84,8 +88,8 @@ func NewCacheBasedProxyWithStagger(ctx context.Context,
 		return nil, err
 	}
 
-	// start the proxy update server in the background
-	go proxy.startProxyUpdateServer()
+	// start the proxy cache server in the background
+	go proxy.startCacheServer()
 
 	return proxy, nil
 }
@@ -124,13 +128,13 @@ type clientgoCachedProxyResolver struct {
 	kongUpdater KongUpdater
 	diagnostic  util.ConfigDumpDiagnostic
 
-	// server configuration, flow control, channels and utility attributes
-	ingressClassName    string
-	ctx                 context.Context
-	stagger             time.Duration
-	proxyRequestTimeout time.Duration
-	syncTicker          *time.Ticker
-	stopCh              chan struct{}
+	// cache server configuration, flow control, channels and utility attributes
+	ingressClassName string
+	ctx              context.Context
+	stagger          time.Duration
+	timeout          time.Duration
+	syncTicker       *time.Ticker
+	stopCh           chan struct{}
 
 	// New code should log using "logger". "deprecatedLogger" is here for compatibility with legacy code that relies
 	// on the logrus API.
@@ -140,9 +144,6 @@ type clientgoCachedProxyResolver struct {
 	// channels
 	update chan *cachedObject
 	del    chan *cachedObject
-
-	// kong ingress controller function metrics
-	promMetrics *util.ControllerFunctionalPrometheusMetrics
 
 	// locks
 	internalCacheLock *sync.RWMutex
@@ -173,48 +174,87 @@ type cachedObject struct {
 // -----------------------------------------------------------------------------
 
 func (p *clientgoCachedProxyResolver) UpdateObject(obj client.Object) error {
-	return p.cache.Add(obj)
+	cobj := &cachedObject{action: updated, key: p.clientObjectKey(obj), runtimeObj: obj.DeepCopyObject()}
+	select {
+	case p.update <- cobj:
+		return nil
+	default:
+		return fmt.Errorf("the proxy is too busy to accept requests at this time, try again later")
+	}
 }
 
 func (p *clientgoCachedProxyResolver) DeleteObject(obj client.Object) error {
-	return p.cache.Delete(obj)
+	cobj := &cachedObject{action: deleted, key: p.clientObjectKey(obj), runtimeObj: obj.DeepCopyObject()}
+	select {
+	case p.del <- cobj:
+		return nil
+	default:
+		return fmt.Errorf("the proxy is too busy to accept requests at this time, try again later")
+	}
 }
 
 func (p *clientgoCachedProxyResolver) ObjectExists(obj client.Object) (bool, error) {
+	p.internalCacheLock.RLock()
+	defer p.internalCacheLock.RUnlock()
 	_, exists, err := p.cache.Get(obj)
 	return exists, err
 }
 
 // -----------------------------------------------------------------------------
-// Client Go Cached Proxy Resolver - Private Methods - Servers
+// Client Go Cached Proxy Resolver - Private Methods - Cache Server
 // -----------------------------------------------------------------------------
 
-// startProxyUpdateServer runs a server in a background goroutine that is responsible for
-// updating the kong proxy backend at regular intervals.
-func (p *clientgoCachedProxyResolver) startProxyUpdateServer() {
+// startCacheServer runs a server in a background goroutine that is responsible for:
+//
+//   1. processing kubernetes object updates (add, replace)
+//   2. processing kubernetes object deletes
+//   3. regularly synchronizing configuration to the Kong Admin API (staggered)
+//
+// While processing objects the cacheServer will (synchronously) convert the objects to kong DSL and
+// submit POST updates to the Kong Admin API with the new configuration.
+func (p *clientgoCachedProxyResolver) startCacheServer() {
+	backendNeedsSync := false
+	p.logger.Info("the proxy cache server has been started")
 	for {
 		select {
-		case <-p.ctx.Done():
-			p.logger.Info("context done: shutting down the proxy update server")
-			if err := p.ctx.Err(); err != nil {
-				p.logger.Error(err, "context completed with error")
+		case cobj := <-p.update:
+			if err := p.cacheUpdate(cobj); err != nil {
+				p.logger.Error(err, "object could not be updated in the cache and will be discarded")
+				break
 			}
-			p.syncTicker.Stop()
-			return
+			backendNeedsSync = true
+		case cobj := <-p.del:
+			if err := p.cacheDelete(cobj); err != nil {
+				p.logger.Error(err, "object could not be deleted from the cache and will be discarded")
+				break
+			}
+			backendNeedsSync = true
 		case <-p.syncTicker.C:
+			if !p.enableReverseSync && !backendNeedsSync {
+				break
+			}
+
 			updateConfigSHA, err := p.kongUpdater(p.ctx, p.lastConfigSHA, p.cache,
-				p.ingressClassName, p.deprecatedLogger, p.kongConfig, p.enableReverseSync, p.diagnostic, p.proxyRequestTimeout, p.promMetrics)
+				p.ingressClassName, p.deprecatedLogger, p.kongConfig, p.enableReverseSync, p.diagnostic)
 			if err != nil {
 				p.logger.Error(err, "could not update kong admin")
 				break
 			}
 			p.lastConfigSHA = updateConfigSHA
+			backendNeedsSync = false
+		case <-p.ctx.Done():
+			p.logger.Info("the proxy cache server's context is done, shutting down")
+			if err := p.ctx.Err(); err != nil {
+				p.logger.Error(err, "context completed with error")
+			}
+			p.syncTicker.Stop()
+			return
 		}
 	}
 }
 
 // -----------------------------------------------------------------------------
-// Client Go Cached Proxy Resolver - Private Methods - Server Utils
+// Client Go Cached Proxy Resolver - Private Methods - Cache Server Utils
 // -----------------------------------------------------------------------------
 
 // initialize validates connectivity with the Kong proxy and some of the configuration options thereof
@@ -261,39 +301,49 @@ func (p *clientgoCachedProxyResolver) initialize() error {
 	p.dbmode = dbmode
 	p.version = proxySemver
 
-	// register controller metrics
-	p.promMetrics = util.ControllerMetricsInit()
-
 	return nil
+}
+
+// objectKey provides a key unique to the cacheServer for tracking objects that are being resolved.
+func (p *clientgoCachedProxyResolver) clientObjectKey(obj client.Object) string {
+	return fmt.Sprintf("%s/%s/%s", obj.GetObjectKind().GroupVersionKind().String(), obj.GetNamespace(), obj.GetName())
+}
+
+// cacheUpdate caches the provided object to the proxy cache and the provided object tracker and reports any errors.
+func (p *clientgoCachedProxyResolver) cacheUpdate(cobj *cachedObject) error {
+	cobj.err = p.cache.Add(cobj.runtimeObj)
+	return cobj.err
+}
+
+// cacheDelete removes the cache entry the provided object from the proxy cache and the provided object tracker and reports any errors.
+func (p *clientgoCachedProxyResolver) cacheDelete(cobj *cachedObject) error {
+	cobj.err = p.cache.Delete(cobj.runtimeObj)
+	return cobj.err
 }
 
 // kongRootWithTimeout provides the root configuration from Kong, but uses a configurable timeout to avoid long waits if the Admin API
 // is not yet ready to respond. If a timeout error occurs, the caller is responsible for providing a retry mechanism.
 func (p *clientgoCachedProxyResolver) kongRootWithTimeout() (map[string]interface{}, error) {
-	ctx, cancel := context.WithTimeout(p.ctx, p.proxyRequestTimeout)
+	ctx, cancel := context.WithTimeout(p.ctx, p.timeout)
 	defer cancel()
 	return p.kongConfig.Client.Root(ctx)
 }
 
-// -----------------------------------------------------------------------------
-// Private Helper Functions
-// -----------------------------------------------------------------------------
-
 // fetchCustomEntities returns the value of the "config" key from a Secret (identified by a "namespace/secretName"
 // string in the store.
-func fetchCustomEntities(secretName string, store store.Storer) ([]byte, error) {
-	ns, name, err := util.ParseNameNS(secretName)
+func fetchCustomEntities(secret string, store store.Storer) ([]byte, error) {
+	ns, name, err := util.ParseNameNS(secret)
 	if err != nil {
 		return nil, fmt.Errorf("parsing kong custom entities secret: %w", err)
 	}
-	secret, err := store.GetSecret(ns, name)
+	kSecret, err := store.GetSecret(ns, name)
 	if err != nil {
 		return nil, fmt.Errorf("fetching secret: %w", err)
 	}
-	config, ok := secret.Data["config"]
+	config, ok := kSecret.Data["config"]
 	if !ok {
 		return nil, fmt.Errorf("'config' key not found in "+
-			"custom entities secret '%v'", secretName)
+			"custom entities secret '%v'", secret)
 	}
 	return config, nil
 }

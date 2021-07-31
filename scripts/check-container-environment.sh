c276d2508141152bf07650381b5ee99b3e66f746
Dustin Scott <sdustin@vmware.com>
2021-07-30 20:52:19 -0500

    feat: added additional pre-checks
    Pre-check script would pass and fail in the middle of the
    integration test due to missing helm and kubectl binaries.
    Added checkes for both and re-organized the print statement
    so that it does not run too far to the right in the
    user terminal.
    
    Signed-off-by: Dustin Scott <sdustin@vmware.com>

--- scripts/check-container-environment.sh
+++ scripts/check-container-environment.sh
@@ -26,7 +26,27 @@ then
     go get -v sigs.k8s.io/kind@${KIND_VERSION}
 fi
 
+# ensure kubectl command is accessible
+if ! command -v kubectl &> /dev/null
+then
+    echo "ERROR: kubectl command not found"
+    exit 10
+fi
+
+# ensure helm command is accessible
+if ! command -v helm &> /dev/null
+then
+    echo "ERROR: helm command not found"
+    exit 10
+fi
+
 DOCKER_VERSION="$(docker -v)"
 KIND_VERSION="$(kind version)"
+KUBECTL_VERSION="$(kubectl version --short --client)"
+HELM_VERSION="$(helm version --short)"
 
-echo "INFO: container environment ready DOCKER=(${DOCKER_VERSION}) KIND=(${KIND_VERSION})"
+echo "INFO: container environment ready
+INFO: DOCKER=(${DOCKER_VERSION})
+INFO: KIND=(${KIND_VERSION})
+INFO: KUBECTL=(${KUBECTL_VERSION})
+INFO: HELM=(${HELM_VERSION})"

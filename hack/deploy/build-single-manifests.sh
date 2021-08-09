99578cb83ec978d365161043ef3a30670e008899
Travis Raines <traines@konghq.com>
2021-08-09 14:48:06 -0700

    refactor(deploy) simplify single manifest script
    Remove staging directory for building single manifests.
    
    Rework kustomizations to allow building single manifests directly from
    the sources under config.
    
    Disable auth proxy in RBAC kustomization.yaml. We aren't using it yet;
    previously we had to copy only RBAC configuration we use into the
    staging directory.

--- hack/deploy/build-single-manifests.sh
+++ hack/deploy/build-single-manifests.sh
@@ -5,22 +5,11 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/../..
-STAGE_DIR=$(mktemp -d)
-
-mkdir $STAGE_DIR/base
+CONFIG_ROOT=config/
 
 cd $SCRIPT_ROOT
 
-# TODO auth proxy service stuff?
-
-cp -Lr ./config/base $STAGE_DIR
-cp -Lr ./config/rbac $STAGE_DIR/base/
-cp -Lr ./config/crd $STAGE_DIR/base/
-cp -Lr ./config/variants $STAGE_DIR
-
-kustomize build $STAGE_DIR/base > deploy/single-v2/all-in-one-dbless.yaml
-kustomize build $STAGE_DIR/variants/postgres > deploy/single-v2/all-in-one-postgres.yaml
-kustomize build $STAGE_DIR/variants/enterprise > deploy/single-v2/all-in-one-enterprise-dbless.yaml
-kustomize build $STAGE_DIR/variants/enterprise-postgres > deploy/single-v2/all-in-one-enterprise-postgres.yaml
-
-rm -rf $STAGE_DIR
+kustomize build $CONFIG_ROOT/base > deploy/single-v2/all-in-one-dbless.yaml
+kustomize build $CONFIG_ROOT/variants/postgres > deploy/single-v2/all-in-one-postgres.yaml
+kustomize build $CONFIG_ROOT/variants/enterprise > deploy/single-v2/all-in-one-enterprise-dbless.yaml
+kustomize build $CONFIG_ROOT/variants/enterprise-postgres > deploy/single-v2/all-in-one-enterprise-postgres.yaml

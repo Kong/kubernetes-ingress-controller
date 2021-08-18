c4be27d906d569950fcfb23b86f3bd3d1b898ef5
Travis Raines <traines@konghq.com>
2021-08-18 07:05:00 -0700

    feat(hack) restore webhook script (#1721)
    Restore the script to generate a self-signed certificate, create a
    ValidatingWebhookConfiguration using it, and patch the KIC deployment to
    use that configuration.
    
    Changed the API version to "admissionregistration.k8s.io/v1" from
    "admissionregistration.k8s.io/v1beta1" in the original. This breaks
    compatibility with Kubernetes versions older than 1.16 to ensure
    compatibility with versions 1.22 and newer.
    
    Remove the static example configuration under deploy/manifests. This
    example was never viable because it lacked a certificate bundle. The
    example will now only exist in the script (which adds the certificate
    bundle automatically) and in documentation (which provides an example
    and instructs you to add the certificate bundle).

---
+++ hack/deploy-admission-controller.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+BASE64_OPTIONS=""
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  BASE64_OPTIONS="-w 0"
+fi
+
+# create a self-signed certificate
+openssl req -x509 -newkey rsa:2048 -keyout tls.key -out tls.crt -days 365  \
+  -nodes -subj "/CN=kong-validation-webhook.kong.svc"
+# create a secret out of this self-signed cert-key pair
+kubectl create secret tls kong-validation-webhook -n kong \
+      --key tls.key --cert tls.crt
+# enable the Admission Webhook Server server
+kubectl patch deploy -n kong ingress-kong \
+  -p '{"spec":{"template":{"spec":{"containers":[{"name":"ingress-controller","env":[{"name":"CONTROLLER_ADMISSION_WEBHOOK_LISTEN","value":":8080"}],"volumeMounts":[{"name":"validation-webhook","mountPath":"/admission-webhook"}]}],"volumes":[{"secret":{"secretName":"kong-validation-webhook"},"name":"validation-webhook"}]}}}}'
+# configure k8s apiserver to send validations to the webhook
+echo "apiVersion: admissionregistration.k8s.io/v1
+kind: ValidatingWebhookConfiguration
+metadata:
+  name: kong-validations
+webhooks:
+- name: validations.kong.konghq.com
+  failurePolicy: Fail
+  sideEffects: None
+  admissionReviewVersions: [\"v1\", \"v1beta1\"]
+  rules:
+  - apiGroups:
+    - configuration.konghq.com
+    apiVersions:
+    - '*'
+    operations:
+    - CREATE
+    - UPDATE
+    resources:
+    - kongconsumers
+    - kongplugins
+  - apiGroups:
+    - ''
+    apiVersions:
+    - 'v1'
+    operations:
+    - CREATE
+    - UPDATE
+    resources:
+    - secrets
+  clientConfig:
+    service:
+      namespace: kong
+      name: kong-validation-webhook
+    caBundle: $(cat tls.crt  | base64 ${BASE64_OPTIONS}) " | kubectl apply -f -
+

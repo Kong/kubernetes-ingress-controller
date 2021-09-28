a73f13a818d0bf319f242202041d22504cce39eb
Shane Utt <shaneutt@linux.com>
2021-09-28 17:03:30 -0400

    feat: add some e2e testing utilities

--- test/e2e/all_in_one_test.go
+++ test/e2e/all_in_one_test.go
@@ -53,12 +53,6 @@ const (
 )
 
 var (
-	// clusterVersionStr indicates the Kubernetes cluster version to use when
-	// generating a testing environment and allows the caller to provide a specific
-	// version. If no version is provided the default version for the cluster
-	// provisioner in the testing framework will be used.
-	clusterVersionStr = os.Getenv("KONG_CLUSTER_VERSION")
-
 	// enterpriseLicenseSecretYAMLVar is the name of the ENV var used to pass an
 	// enterprise license to the tests.
 	enterpriseLicenseSecretYAMLVar = "KONG_ENTERPRISE_LICENSE_SECRET"

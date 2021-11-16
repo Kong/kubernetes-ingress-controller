12099e8cc86c704bfa89abd89c2413e35e1686b2
Shane Utt <shaneutt@linux.com>
2021-11-16 10:25:09 -0500

    feat: add credentials validation for secrets and consumer changes

--- internal/admission/errors.go
+++ internal/admission/errors.go
@@ -1,13 +1,16 @@
 package admission
 
 const (
-	ErrTextConsumerUsernameEmpty           = "username cannot be empty"
-	ErrTextConsumerUnretrievable           = "failed to fetch consumer from kong"
-	ErrTextConsumerExists                  = "consumer already exists"
-	ErrTextPluginNameEmpty                 = "plugin name cannot be empty"
-	ErrTextPluginConfigInvalid             = "could not parse plugin configuration"
-	ErrTextPluginUsesBothConfigTypes       = "plugin cannot use both Config and ConfigFrom"
-	ErrTextPluginConfigViolatesSchema      = "plugin failed schema validation: %s"
-	ErrTextPluginConfigValidationFailed    = "unable to validate plugin schema"
-	ErrTextPluginSecretConfigUnretrievable = "could not load secret plugin configuration"
+	ErrTextConsumerUsernameEmpty              = "username cannot be empty"
+	ErrTextConsumerUnretrievable              = "failed to fetch consumer from kong"
+	ErrTextConsumerExists                     = "consumer already exists"
+	ErrTextPluginNameEmpty                    = "plugin name cannot be empty"
+	ErrTextPluginConfigInvalid                = "could not parse plugin configuration"
+	ErrTextPluginUsesBothConfigTypes          = "plugin cannot use both Config and ConfigFrom"
+	ErrTextPluginConfigViolatesSchema         = "plugin failed schema validation: %s"
+	ErrTextPluginConfigValidationFailed       = "unable to validate plugin schema"
+	ErrTextPluginSecretConfigUnretrievable    = "could not load secret plugin configuration"
+	ErrTextConsumerCredentialSecretNotFound   = "consumer referenced non-existent credentials secret"
+	ErrTextConsumerCredentialValidationFailed = "consumer credential failed validation"
+	ErrTextFailedToRetrieveSecret             = "could not retrieve secrets from the kubernets API" //nolint:gosec
 )
--- internal/admission/validator.go
+++ internal/admission/validator.go
@@ -3,31 +3,61 @@ package admission
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/kong/go-kong/kong"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	credsvalidation "github.com/kong/kubernetes-ingress-controller/v2/internal/adminapi/validators/consumer/credentials"
+	"github.com/kong/kubernetes-ingress-controller/v2/internal/annotations"
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/kongstate"
-	configurationv1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1"
+	kongv1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1"
 )
 
 // KongValidator validates Kong entities.
 type KongValidator interface {
-	ValidateConsumer(ctx context.Context, consumer configurationv1.KongConsumer) (bool, string, error)
-	ValidatePlugin(ctx context.Context, plugin configurationv1.KongPlugin) (bool, string, error)
-	ValidateClusterPlugin(ctx context.Context, plugin configurationv1.KongClusterPlugin) (bool, string, error)
-	ValidateCredential(secret corev1.Secret) (bool, string, error)
+	ValidateConsumer(ctx context.Context, consumer kongv1.KongConsumer) (bool, string, error)
+	ValidatePlugin(ctx context.Context, plugin kongv1.KongPlugin) (bool, string, error)
+	ValidateClusterPlugin(ctx context.Context, plugin kongv1.KongClusterPlugin) (bool, string, error)
+	ValidateCredential(ctx context.Context, secret corev1.Secret) (bool, string, error)
 }
 
 // KongHTTPValidator implements KongValidator interface to validate Kong
 // entities using the Admin API of Kong.
 type KongHTTPValidator struct {
-	ConsumerSvc  kong.AbstractConsumerService
-	PluginSvc    kong.AbstractPluginService
-	Logger       logrus.FieldLogger
-	SecretGetter kongstate.SecretGetter
+	ConsumerSvc   kong.AbstractConsumerService
+	PluginSvc     kong.AbstractPluginService
+	Logger        logrus.FieldLogger
+	SecretGetter  kongstate.SecretGetter
+	ManagerClient client.Client
+
+	ingressClassMatcher func(*metav1.ObjectMeta, annotations.ClassMatching) bool
+}
+
+// NewKongHTTPValidator provides a new KongHTTPValidator object provided a
+// controller-runtime client which will be used to retrieve reference objects
+// such as consumer credentials secrets. If you do not pass a cached client
+// here, the performance of this validator can get very poor at high scales.
+func NewKongHTTPValidator(
+	consumerSvc kong.AbstractConsumerService,
+	pluginSvc kong.AbstractPluginService,
+	logger logrus.FieldLogger,
+	managerClient client.Client,
+	ingressClass string,
+) KongHTTPValidator {
+	matcher := annotations.IngressClassValidatorFuncFromObjectMeta(ingressClass)
+	return KongHTTPValidator{
+		ConsumerSvc:   consumerSvc,
+		PluginSvc:     pluginSvc,
+		Logger:        logger,
+		SecretGetter:  &managerClientSecretGetter{managerClient: managerClient},
+		ManagerClient: managerClient,
+
+		ingressClassMatcher: matcher,
+	}
 }
 
 // ValidateConsumer checks if consumer has a Username and a consumer with
@@ -35,22 +65,130 @@ type KongHTTPValidator struct {
 // If an error occurs during validation, it is returned as the last argument.
 // The first boolean communicates if the consumer is valid or not and string
 // holds a message if the entity is not valid.
-func (validator KongHTTPValidator) ValidateConsumer(ctx context.Context,
-	consumer configurationv1.KongConsumer) (bool, string, error) {
+func (validator KongHTTPValidator) ValidateConsumer(
+	ctx context.Context,
+	consumer kongv1.KongConsumer,
+) (bool, string, error) {
+	// ignore consumers that are being managed by another controller
+	if !validator.ingressClassMatcher(&consumer.ObjectMeta, annotations.ExactClassMatch) {
+		return true, "", nil
+	}
+
+	// a consumer without a username is not valid
 	if consumer.Username == "" {
 		return false, ErrTextConsumerUsernameEmpty, nil
 	}
+
+	// verify that the consumer is not already otherwise present in the data-plane
 	c, err := validator.ConsumerSvc.Get(ctx, &consumer.Username)
 	if err != nil {
-		if kong.IsNotFoundErr(err) {
-			return true, "", nil
+		if !kong.IsNotFoundErr(err) {
+			validator.Logger.Errorf("failed to fetch consumer from kong: %v", err)
+			return false, ErrTextConsumerUnretrievable, err
 		}
-		validator.Logger.Errorf("failed to fetch consumer from kong: %v", err)
-		return false, ErrTextConsumerUnretrievable, err
 	}
 	if c != nil {
 		return false, ErrTextConsumerExists, nil
 	}
+
+	// if there are no credentials for this consumer, there's no need to move on
+	// to credentials validation.
+	if len(consumer.Credentials) == 0 {
+		return true, "", nil
+	}
+
+	// pull all the managed consumers in order to build a validation index of
+	// credentials so that the consumers credentials references can be validated.
+	managedConsumers, err := validator.listManagedConsumers(ctx)
+	if err != nil {
+		return false, ErrTextConsumerUnretrievable, err
+	}
+
+	// retrieve the consumer's credentials secrets to validate them with the index
+	credentials := make([]*corev1.Secret, 0, len(consumer.Credentials))
+	for _, secretName := range consumer.Credentials {
+		// retrieve the credentials secret
+		secret, err := validator.SecretGetter.GetSecret(consumer.Namespace, secretName)
+		if err != nil {
+			if errors.IsNotFound(err) {
+				return false, ErrTextConsumerCredentialSecretNotFound, err
+			}
+			return false, ErrTextFailedToRetrieveSecret, err
+		}
+
+		// do the basic credentials validation
+		if err := credsvalidation.ValidateCredentials(consumer.Name, secret); err != nil {
+			return false, ErrTextConsumerCredentialValidationFailed, err
+		}
+
+		// if valid, store it so we can index it for upcoming constraints validation
+		credentials = append(credentials, secret)
+	}
+
+	// unique constraints on consumer credentials are global to all consumers
+	// and credentials, so we must build an index based on all existing credentials.
+	credentialsIndex, err := globalValidationIndexForCredentials(ctx, validator.ManagerClient, managedConsumers)
+	if err != nil {
+		return false, ErrTextConsumerCredentialValidationFailed, err
+	}
+
+	// validate the consumer's credentials against the index of all managed
+	// credentials to ensure they're not in violation of any unique constraints.
+	for _, secret := range credentials {
+		// do the unique constraints validation of the credentials using the credentials index
+		if err := credentialsIndex.ValidateCredentialsForUniqueKeyConstraints(consumer.Name, secret); err != nil {
+			return false, ErrTextConsumerCredentialValidationFailed, err
+		}
+	}
+
+	return true, "", nil
+}
+
+// ValidateCredential checks if the secret contains a credential meant to
+// be installed in Kong. If so, then it verifies if all the required fields
+// are present in it or not. If valid, it returns true with an empty string,
+// else it returns false with the error messsage. If an error happens during
+// validation, error is returned.
+func (validator KongHTTPValidator) ValidateCredential(
+	ctx context.Context,
+	secret corev1.Secret,
+) (bool, string, error) {
+	// if the secret doesn't contain a type key it's not a credentials secret
+	_, ok := secret.Data[credsvalidation.TypeKey]
+	if !ok {
+		return true, "", nil
+	}
+
+	// credentials are only validated if they are referenced by a managed consumer
+	// in the namespace, as such we pull a list of all consumers from the cached
+	// client to determine if the credentials are referenced.
+	managedConsumers, err := validator.listManagedConsumers(ctx)
+	if err != nil {
+		return false, ErrTextConsumerUnretrievable, err
+	}
+
+	// find the list of managed consumers which actually reference this secret
+	// as a credential.
+	for _, consumerWhichReferencesThisSecret := range listManagedConsumersReferencingCredentialsSecret(secret, managedConsumers) {
+		// perform basic credentials validation first
+		if err := credsvalidation.ValidateCredentials(consumerWhichReferencesThisSecret.Name, &secret); err != nil {
+			return false, ErrTextConsumerCredentialValidationFailed, err
+		}
+
+		// unique constraints on consumer credentials are global to all consumers
+		// and credentials, so we must build an index based on all existing credentials.
+		credentialsIndex, err := globalValidationIndexForCredentials(ctx, validator.ManagerClient, managedConsumers)
+		if err != nil {
+			return false, ErrTextConsumerCredentialValidationFailed, err
+		}
+
+		// validate this new credential against an index of existing credentials
+		secretCopy := secret
+		if err := credentialsIndex.ValidateCredentialsForUniqueKeyConstraints(consumerWhichReferencesThisSecret.Name, &secretCopy); err != nil {
+			return false, ErrTextConsumerCredentialValidationFailed, err
+		}
+	}
+
 	return true, "", nil
 }
 
@@ -59,8 +197,10 @@ func (validator KongHTTPValidator) ValidateConsumer(ctx context.Context,
 // If an error occurs during validation, it is returned as the last argument.
 // The first boolean communicates if k8sPluign is valid or not and string
 // holds a message if the entity is not valid.
-func (validator KongHTTPValidator) ValidatePlugin(ctx context.Context,
-	k8sPlugin configurationv1.KongPlugin) (bool, string, error) {
+func (validator KongHTTPValidator) ValidatePlugin(
+	ctx context.Context,
+	k8sPlugin kongv1.KongPlugin,
+) (bool, string, error) {
 	if k8sPlugin.PluginName == "" {
 		return false, ErrTextPluginNameEmpty, nil
 	}
@@ -75,8 +215,7 @@ func (validator KongHTTPValidator) ValidatePlugin(ctx context.Context,
 		if len(plugin.Config) > 0 {
 			return false, ErrTextPluginUsesBothConfigTypes, nil
 		}
-		config, err := kongstate.SecretToConfiguration(validator.SecretGetter,
-			(*k8sPlugin.ConfigFrom).SecretValue, k8sPlugin.Namespace)
+		config, err := kongstate.SecretToConfiguration(validator.SecretGetter, (*k8sPlugin.ConfigFrom).SecretValue, k8sPlugin.Namespace)
 		if err != nil {
 			return false, ErrTextPluginSecretConfigUnretrievable, err
 		}
@@ -100,9 +239,11 @@ func (validator KongHTTPValidator) ValidatePlugin(ctx context.Context,
 
 // ValidateClusterPlugin transfers relevant fields from a KongClusterPlugin into a KongPlugin and then returns
 // the result of ValidatePlugin for the derived KongPlugin
-func (validator KongHTTPValidator) ValidateClusterPlugin(ctx context.Context,
-	k8sPlugin configurationv1.KongClusterPlugin) (bool, string, error) {
-	derived := configurationv1.KongPlugin{
+func (validator KongHTTPValidator) ValidateClusterPlugin(
+	ctx context.Context,
+	k8sPlugin kongv1.KongClusterPlugin,
+) (bool, string, error) {
+	derived := kongv1.KongPlugin{
 		TypeMeta:    k8sPlugin.TypeMeta,
 		ObjectMeta:  k8sPlugin.ObjectMeta,
 		ConsumerRef: k8sPlugin.ConsumerRef,
@@ -113,8 +254,8 @@ func (validator KongHTTPValidator) ValidateClusterPlugin(ctx context.Context,
 		Protocols:   k8sPlugin.Protocols,
 	}
 	if k8sPlugin.ConfigFrom != nil {
-		ref := configurationv1.ConfigSource{
-			SecretValue: configurationv1.SecretValueFromSource{
+		ref := kongv1.ConfigSource{
+			SecretValue: kongv1.SecretValueFromSource{
 				Secret: k8sPlugin.ConfigFrom.SecretValue.Secret,
 				Key:    k8sPlugin.ConfigFrom.SecretValue.Key,
 			},
@@ -127,65 +268,45 @@ func (validator KongHTTPValidator) ValidateClusterPlugin(ctx context.Context,
 	return validator.ValidatePlugin(ctx, derived)
 }
 
-var (
-	keyAuthFields   = []string{"key"}
-	basicAuthFields = []string{"username", "password"}
-	hmacAuthFields  = []string{"username", "secret"}
-	jwtAuthFields   = []string{"algorithm", "rsa_public_key", "key", "secret"}
-	mtlsAuthFields  = []string{"subject_name"}
+// -----------------------------------------------------------------------------
+// KongHTTPValidator - Private Methods
+// -----------------------------------------------------------------------------
 
-	// TODO dynamically fetch these from Kong
-	credTypeToFields = map[string][]string{
-		"key-auth":             keyAuthFields,
-		"keyauth_credential":   keyAuthFields,
-		"basic-auth":           basicAuthFields,
-		"basicauth_credential": basicAuthFields,
-		"hmac-auth":            hmacAuthFields,
-		"hmacauth_credential":  hmacAuthFields,
-		"jwt":                  jwtAuthFields,
-		"jwt_secret":           jwtAuthFields,
-		"oauth2":               {"name", "client_id", "client_secret", "redirect_uris"},
-		"acl":                  {"group"},
-		"mtls-auth":            mtlsAuthFields,
-	}
-)
-
-// ValidateCredential checks if the secret contains a credential meant to
-// be installed in Kong. If so, then it verifies if all the required fields
-// are present in it or not. If valid, it returns true with an empty string,
-// else it returns false with the error messsage. If an error happens during
-// validation, error is returned.
-func (validator KongHTTPValidator) ValidateCredential(
-	secret corev1.Secret) (bool, string, error) {
-
-	credTypeBytes, ok := secret.Data["kongCredType"]
-	if !ok {
-		// doesn't look like a credential resource
-		return true, "", nil
-	}
-	credType := string(credTypeBytes)
-
-	fields, ok := credTypeToFields[credType]
-	if !ok {
-		return false, "invalid credential type: " + credType, nil
+func (validator KongHTTPValidator) listManagedConsumers(ctx context.Context) ([]*kongv1.KongConsumer, error) {
+	// gather a list of all consumers from the cached client
+	consumers := &kongv1.KongConsumerList{}
+	if err := validator.ManagerClient.List(ctx, consumers, &client.ListOptions{
+		Namespace: corev1.NamespaceAll,
+	}); err != nil {
+		return nil, err
 	}
 
-	var missingFields []string
-	for _, field := range fields {
-		if _, ok := secret.Data[field]; !ok {
-			missingFields = append(missingFields, field)
+	// reduce the consumer set to consumers managed by this controller
+	managedConsumers := make([]*kongv1.KongConsumer, 0)
+	for _, consumer := range consumers.Items {
+		if !validator.ingressClassMatcher(&consumer.ObjectMeta, annotations.ExactClassMatch) {
+			// ignore consumers (and subsequently secrets) that are managed by other controllers
+			continue
 		}
-	}
-	if len(missingFields) != 0 {
-		return false, "missing required field(s): " +
-			strings.Join(missingFields, ", "), nil
+		consumerCopy := consumer
+		managedConsumers = append(managedConsumers, &consumerCopy)
 	}
 
-	// TODO add unique key violation detection
-	// For each credential, there is a unique column, like key for key-auth,
-	// username for basic-auth; make an API call to Kong's Admin API
-	// and verify if there will be a violation, similar to how it's done
-	// for KongConsumer; return error if the resource is already present in
-	// Kong.
-	return true, "", nil
+	return managedConsumers, nil
+}
+
+// -----------------------------------------------------------------------------
+// Private - Manager Client Secret Getter
+// -----------------------------------------------------------------------------
+
+type managerClientSecretGetter struct {
+	managerClient client.Client
+}
+
+func (m *managerClientSecretGetter) GetSecret(namespace, name string) (*corev1.Secret, error) {
+	secret := &corev1.Secret{}
+	return secret, m.managerClient.Get(context.Background(), client.ObjectKey{
+		Namespace: namespace,
+		Name:      name,
+	}, secret)
 }

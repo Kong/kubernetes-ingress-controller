4589242ecae536335cc7a0b1f7d2accc0ea3f39d
Shane Utt <shaneutt@linux.com>
2022-01-19 13:56:59 -0500

    fix: in-place constrained value updates for creds

--- internal/admission/validator.go
+++ internal/admission/validator.go
@@ -112,6 +112,7 @@ func (validator KongHTTPValidator) ValidateConsumer(
 
 	// retrieve the consumer's credentials secrets to validate them with the index
 	credentials := make([]*corev1.Secret, 0, len(consumer.Credentials))
+	ignoredSecrets := make(map[string]map[string]struct{})
 	for _, secretName := range consumer.Credentials {
 		// retrieve the credentials secret
 		secret, err := validator.SecretGetter.GetSecret(consumer.Namespace, secretName)
@@ -129,11 +130,21 @@ func (validator KongHTTPValidator) ValidateConsumer(
 
 		// if valid, store it so we can index it for upcoming constraints validation
 		credentials = append(credentials, secret)
+
+		// later we'll build a global index of all credentials which is needed to
+		// validate unique key constraints. That index should omit the secrets that
+		// are referenced by this consumer to avoid duplication.
+		if _, ok := ignoredSecrets[consumer.Namespace]; !ok {
+			ignoredSecrets[consumer.Namespace] = make(map[string]struct{})
+		}
+		ignoredSecrets[consumer.Namespace][secretName] = struct{}{}
 	}
 
 	// unique constraints on consumer credentials are global to all consumers
 	// and credentials, so we must build an index based on all existing credentials.
-	credentialsIndex, err := globalValidationIndexForCredentials(ctx, validator.ManagerClient, managedConsumers)
+	// we ignore the secrets referenced by this consumer so that the index is not
+	// testing them against themselves.
+	credentialsIndex, err := globalValidationIndexForCredentials(ctx, validator.ManagerClient, managedConsumers, ignoredSecrets)
 	if err != nil {
 		return false, ErrTextConsumerCredentialValidationFailed, err
 	}
@@ -142,7 +153,7 @@ func (validator KongHTTPValidator) ValidateConsumer(
 	// credentials to ensure they're not in violation of any unique constraints.
 	for _, secret := range credentials {
 		// do the unique constraints validation of the credentials using the credentials index
-		if err := credentialsIndex.ValidateCredentialsForUniqueKeyConstraints(consumer.Name, secret); err != nil {
+		if err := credentialsIndex.ValidateCredentialsForUniqueKeyConstraints(secret); err != nil {
 			return false, ErrTextConsumerCredentialValidationFailed, err
 		}
 	}
@@ -173,26 +184,34 @@ func (validator KongHTTPValidator) ValidateCredential(
 		return false, ErrTextConsumerUnretrievable, err
 	}
 
-	// find the list of managed consumers which actually reference this secret
-	// as a credential.
-	for _, consumerWhichReferencesThisSecret := range listManagedConsumersReferencingCredentialsSecret(secret, managedConsumers) {
-		// perform basic credentials validation first
-		if err := credsvalidation.ValidateCredentials(consumerWhichReferencesThisSecret.Name, &secret); err != nil {
-			return false, ErrTextConsumerCredentialValidationFailed, err
-		}
+	// verify whether this secret is referenced by any managed consumer
+	managedConsumersWithReferences := listManagedConsumersReferencingCredentialsSecret(secret, managedConsumers)
+	if len(managedConsumersWithReferences) == 0 {
+		// if no managed consumers reference this secret, its considered
+		// unmanaged and we don't validate it unless it becomes referenced
+		// by a managed consumer at a later time.
+		return true, "", nil
+	}
 
-		// unique constraints on consumer credentials are global to all consumers
-		// and credentials, so we must build an index based on all existing credentials.
-		credentialsIndex, err := globalValidationIndexForCredentials(ctx, validator.ManagerClient, managedConsumers)
-		if err != nil {
-			return false, ErrTextConsumerCredentialValidationFailed, err
-		}
+	// now that we know at least one managed consumer is referencing this
+	// secret we perform the base level credentials secret validation.
+	if err := credsvalidation.ValidateCredentials(&secret); err != nil {
+		return false, ErrTextConsumerCredentialValidationFailed, err
+	}
 
-		// validate this new credential against an index of existing credentials
-		secretCopy := secret
-		if err := credentialsIndex.ValidateCredentialsForUniqueKeyConstraints(consumerWhichReferencesThisSecret.Name, &secretCopy); err != nil {
-			return false, ErrTextConsumerCredentialValidationFailed, err
-		}
+	// if base level validation passes we move on to create an index of
+	// all managed credentials so that we can verify that the updates to
+	// this secret are not in violation of any unique key constraints.
+	ignoreSecrets := map[string]map[string]struct{}{secret.Namespace: {secret.Name: {}}}
+	credentialsIndex, err := globalValidationIndexForCredentials(ctx, validator.ManagerClient, managedConsumers, ignoreSecrets)
+	if err != nil {
+		return false, ErrTextConsumerCredentialValidationFailed, err
+	}
+
+	// the index is built, now validate that the newly updated secret
+	// is not in violation of any constraints.
+	if err := credentialsIndex.ValidateCredentialsForUniqueKeyConstraints(&secret); err != nil {
+		return false, ErrTextConsumerCredentialValidationFailed, err
 	}
 
 	return true, "", nil

04472a39833bdbf81ac49f59d4cf84aeb26e0a91
Shane Utt <shaneutt@linux.com>
2022-02-28 14:53:22 -0500

    chore: refactor proxy int into dataplance synchronizer

--- internal/dataplane/synchronizer.go
+++ internal/dataplane/synchronizer.go
@@ -1,4 +1,4 @@
-package proxy
+package dataplane
 
 import (
 	"context"
@@ -12,97 +12,38 @@ import (
 	"github.com/kong/go-kong/kong"
 	"github.com/sirupsen/logrus"
 
-	"github.com/kong/kubernetes-ingress-controller/v2/internal/dataplane"
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/store"
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/util"
 )
 
 // -----------------------------------------------------------------------------
-// Client Go Cached Proxy Resolver - Public Functions
+// Dataplane Synchronizer - Public Vars
 // -----------------------------------------------------------------------------
 
-// NewCacheBasedProxy will provide a new Proxy object. Note that this starts some background goroutines and the
-// caller is resonsible for marking the provided context.Context as "Done()" to shut down the background routines.
-func NewCacheBasedProxy(logger logrus.FieldLogger, dataplaneClient dataplane.Client) (Proxy, error) {
-	stagger, err := time.ParseDuration(fmt.Sprintf("%gs", DefaultSyncSeconds))
-	if err != nil {
-		return nil, err
-	}
-	return NewCacheBasedProxyWithStagger(logger, dataplaneClient, stagger)
-}
-
-// NewCacheBasedProxy will provide a new Proxy object. Note that this starts some background goroutines and the caller
-// is resonsible for marking the provided context.Context as "Done()" to shut down the background routines. A "stagger"
-// time duration is provided to indicate how often the background routines will sync configuration to the Kong Admin API.
-func NewCacheBasedProxyWithStagger(logger logrus.FieldLogger, dataplaneClient dataplane.Client, stagger time.Duration) (Proxy, error) {
-	// configure the cachestores and the proxy instance
-	proxy := &clientgoCachedProxyResolver{
-		deprecatedLogger: logger,
-		logger:           logrusr.New(logger),
-
-		dataplaneClient: dataplaneClient,
-		stagger:         stagger,
-		stopCh:          make(chan struct{}),
-		syncTicker:      time.NewTicker(stagger),
-		configApplied:   false,
-	}
-
-	// initialize the proxy which validates connectivity with the Admin API and
-	// checks several proxy server attributes such as version and dbmode.
-	if err := proxy.initialize(); err != nil {
-		return nil, err
-	}
-
-	return proxy, nil
-}
+const (
+	// DefaultSyncSeconds indicates the time.Duration (minimum) that will occur between
+	// updates to the DataplaneClient.
+	//
+	// This 1s default was based on local testing wherein it appeared sub-second updates
+	// to the Admin API could be problematic (or at least operate differently) based on
+	// which storage backend was in use (i.e. "dbless", "postgres"). This is a workaround
+	// for improvements we still need to investigate upstream.
+	//
+	// See Also: https://github.com/Kong/kubernetes-ingress-controller/issues/1398
+	DefaultSyncSeconds float32 = 3.0
+)
 
 // -----------------------------------------------------------------------------
-// Client Go Cached Proxy Resolver - Public Methods - Interface Implementation
+// Synchronizer - Public Types
 // -----------------------------------------------------------------------------
 
-func (p *clientgoCachedProxyResolver) NeedLeaderElection() bool {
-	return true
-}
-
-func (p *clientgoCachedProxyResolver) Start(ctx context.Context) error {
-	// TODO https://github.com/Kong/kubernetes-ingress-controller/issues/2249
-	// This is a temporary mitigation to allow some time for controllers to populate the proxy cache
-	time.Sleep(time.Second * 5)
-	go p.startProxyUpdateServer(ctx)
-	return nil
-}
-
-func (p *clientgoCachedProxyResolver) IsReady() bool {
-	// If the proxy is has no database, it is only ready after a successful sync
-	// Otherwise, it has no configuration loaded
-	if p.dbmode == "off" {
-		p.configAppliedMutex.RLock()
-		defer p.configAppliedMutex.RUnlock()
-		return p.configApplied
-	}
-	// If the proxy has a database, it is ready immediately
-	// It will load existing configuration from the database
-	return true
-}
-
-// -----------------------------------------------------------------------------
-// Client Go Cached Proxy Resolver - Private Types
-// -----------------------------------------------------------------------------
-
-// clientgoCachedProxyResolver represents the cached objects and Kong DSL configuration.
-//
-// This implements the Proxy interface to provide asynchronous, non-blocking updates to
-// the Kong Admin API for controller-runtime based controller managers.
-//
-// This object's attributes are immutable (private), and it is threadsafe.
-type clientgoCachedProxyResolver struct {
-	// New code should log using "logger". "deprecatedLogger" is here for compatibility with legacy code that relies
-	// on the logrus API.
-	deprecatedLogger logrus.FieldLogger
-	logger           logr.Logger
+// Synchronizer is a threadsafe object which starts a goroutine to updates
+// the data-plane at regular intervals.
+type Synchronizer struct {
+	logger logr.Logger
 
 	// dataplane client to send updates to the Kong Admin API
-	dataplaneClient dataplane.Client
+	dataplaneClient Client
 
 	// kong configuration metadata
 	dbmode  string
@@ -116,13 +57,76 @@ type clientgoCachedProxyResolver struct {
 	configAppliedMutex sync.RWMutex
 }
 
+// NewSynchronizer will provide a new Synchronizer object. Note that this
+// starts some background goroutines and the caller is resonsible for marking
+// the provided context.Context as "Done()" to shut down the background routines
+func NewSynchronizer(logger logrus.FieldLogger, dataplaneClient Client) (*Synchronizer, error) {
+	stagger, err := time.ParseDuration(fmt.Sprintf("%gs", DefaultSyncSeconds))
+	if err != nil {
+		return nil, err
+	}
+	return NewSynchronizerWithStagger(logger, dataplaneClient, stagger)
+}
+
+// NewSynchronizer will provide a new Synchronizer object with a specified
+// stagger time for data-plane updates to occur. Note that this starts some
+// background goroutines and the caller is resonsible for marking the provided
+// context.Context as "Done()" to shut down the background routines
+func NewSynchronizerWithStagger(logger logrus.FieldLogger, dataplaneClient Client, stagger time.Duration) (*Synchronizer, error) {
+	synchronizer := &Synchronizer{
+		logger:          logrusr.New(logger),
+		dataplaneClient: dataplaneClient,
+		stagger:         stagger,
+		stopCh:          make(chan struct{}),
+		syncTicker:      time.NewTicker(stagger),
+		configApplied:   false,
+	}
+
+	// TODO: this initialization needs to move into the dataplane client
+	if err := synchronizer.initialize(); err != nil {
+		return nil, err
+	}
+
+	return synchronizer, nil
+}
+
 // -----------------------------------------------------------------------------
-// Client Go Cached Proxy Resolver - Private Methods - Server Utilities
+// Synchronizer - Public Methods
 // -----------------------------------------------------------------------------
 
-// startProxyUpdateServer runs a server in a background goroutine that is responsible for
+func (p *Synchronizer) Start(ctx context.Context) error {
+	// TODO https://github.com/Kong/kubernetes-ingress-controller/issues/2249
+	// This is a temporary mitigation to allow some time for controllers to
+	// populate their dataplaneClient cache.
+	time.Sleep(time.Second * 5)
+	go p.startUpdateServer(ctx)
+	return nil
+}
+
+func (p *Synchronizer) IsReady() bool {
+	// If the proxy is has no database, it is only ready after a successful sync
+	// Otherwise, it has no configuration loaded
+	if p.dbmode == "off" {
+		p.configAppliedMutex.RLock()
+		defer p.configAppliedMutex.RUnlock()
+		return p.configApplied
+	}
+	// If the proxy has a database, it is ready immediately
+	// It will load existing configuration from the database
+	return true
+}
+
+func (p *Synchronizer) NeedLeaderElection() bool {
+	return true
+}
+
+// -----------------------------------------------------------------------------
+// Synchronizer - Private Methods - Server Utilities
+// -----------------------------------------------------------------------------
+
+// startUpdateServer runs a server in a background goroutine that is responsible for
 // updating the kong proxy backend at regular intervals.
-func (p *clientgoCachedProxyResolver) startProxyUpdateServer(ctx context.Context) {
+func (p *Synchronizer) startUpdateServer(ctx context.Context) {
 	var initialConfig sync.Once
 	for {
 		select {
@@ -143,16 +147,20 @@ func (p *clientgoCachedProxyResolver) startProxyUpdateServer(ctx context.Context
 	}
 }
 
+// -----------------------------------------------------------------------------
+// Synchronizer - Private Methods - Helper
+// -----------------------------------------------------------------------------
+
 // initialize validates connectivity with the Kong proxy and some of the configuration options thereof
 // and populates several local attributes given retrieved configuration data from the proxy root config.
 //
 // Note: this must be run (and must succeed) in order to successfully start the cache server.
-func (p *clientgoCachedProxyResolver) initialize() error {
+func (p *Synchronizer) initialize() error {
 	// FIXME/TODO: technically we don't have any dataplane implementations
 	// EXCEPT Kong so this switch is here temporarily while we move the
 	// relevant functionality into the dataplane client.
 	switch dataplaneClient := p.dataplaneClient.(type) {
-	case *dataplane.KongClient:
+	case *KongClient:
 		// download the kong root configuration (and validate connectivity to the proxy API)
 		root, err := dataplaneClient.RootWithTimeout()
 		if err != nil {
@@ -196,7 +204,7 @@ func (p *clientgoCachedProxyResolver) initialize() error {
 }
 
 // markConfigApplied marks that config has been applied
-func (p *clientgoCachedProxyResolver) markConfigApplied() {
+func (p *Synchronizer) markConfigApplied() {
 	p.configAppliedMutex.Lock()
 	defer p.configAppliedMutex.Unlock()
 	p.configApplied = true

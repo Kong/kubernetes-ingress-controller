c59815e12f2e72d8a31e0d17a7463fcf738c8f7b
Shane Utt <shaneutt@linux.com>
2022-02-28 14:26:18 -0500

    feat: provide high level dataplane client

--- internal/dataplane/doc.go
+++ internal/dataplane/doc.go
@@ -1,3 +1,3 @@
 // Package dataplane provides functionality for communicating with and
-// providing configuration to Kong Gateways.
+// providing configuration to backend dataplane APIs.
 package dataplane

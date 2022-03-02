1c7ffccb6f7cf935d6cbe58526cd7dfed8b3c94d
Travis Raines <571832+rainest@users.noreply.github.com>
2022-03-02 12:50:38 -0800

    feat(util) add Kong version utility
    Add utility functions to set the Kong version once at startup and
    retrieve it from elsewhere without passing the version down through the
    call chain.

--- internal/dataplane/parser/translate_utils_test.go
+++ internal/dataplane/parser/translate_utils_test.go
@@ -4,13 +4,84 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/blang/semver/v4"
 	"github.com/stretchr/testify/assert"
 	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+
+	"github.com/kong/kubernetes-ingress-controller/v2/internal/util"
 )
+
+func Test_convertGatewayMatchHeadersToKongRouteMatchHeadersVersionBehavior(t *testing.T) {
+	regexType := gatewayv1alpha2.HeaderMatchRegularExpression
+
+	type Case struct {
+		msg    string
+		input  []gatewayv1alpha2.HTTPHeaderMatch
+		output map[string][]string
+		err    error
+	}
+
+	// util reports Kong version 0.0.0 when not initialized
+	belowThresholdTests := []Case{
+		{
+			msg: "regex header matches fail on unsupported versions",
+			input: []gatewayv1alpha2.HTTPHeaderMatch{{
+				Type:  &regexType,
+				Name:  "Content-Type",
+				Value: "^audio/*",
+			}},
+			output: nil,
+			err:    fmt.Errorf("Kong version %s does not support HeaderMatchRegularExpression", semver.MustParse("0.0.0")),
+		},
+		{
+			msg: "a single exact header match succeeds on any version",
+			input: []gatewayv1alpha2.HTTPHeaderMatch{{
+				Name:  "Content-Type",
+				Value: "audio/vorbis",
+			}},
+			output: map[string][]string{
+				"Content-Type": {"audio/vorbis"},
+			},
+		},
+	}
+
+	for _, tt := range belowThresholdTests {
+		t.Run(tt.msg, func(t *testing.T) {
+			output, err := convertGatewayMatchHeadersToKongRouteMatchHeaders(tt.input)
+			assert.Equal(t, tt.err, err)
+			assert.Equal(t, tt.output, output)
+		})
+	}
+
+	util.SetKongVersion(semver.MustParse("2.8.0"))
+
+	aboveThresholdTests := []Case{
+		{
+			msg: "regex header matches succeed on supported versions",
+			input: []gatewayv1alpha2.HTTPHeaderMatch{{
+				Type:  &regexType,
+				Name:  "Content-Type",
+				Value: "^audio/*",
+			}},
+			output: map[string][]string{
+				"Content-Type": {kongHeaderRegexPrefix + "^audio/*"},
+			},
+		},
+	}
+
+	for _, tt := range aboveThresholdTests {
+		t.Run(tt.msg, func(t *testing.T) {
+			output, err := convertGatewayMatchHeadersToKongRouteMatchHeaders(tt.input)
+			assert.Equal(t, tt.err, err)
+			assert.Equal(t, tt.output, output)
+		})
+	}
+}
 
 func Test_convertGatewayMatchHeadersToKongRouteMatchHeaders(t *testing.T) {
 	regexType := gatewayv1alpha2.HeaderMatchRegularExpression
 	exactType := gatewayv1alpha2.HeaderMatchExact
+	util.SetKongVersion(semver.MustParse("2.8.0"))
 
 	t.Log("generating several gateway header matches")
 	tests := []struct {
@@ -20,7 +91,7 @@ func Test_convertGatewayMatchHeadersToKongRouteMatchHeaders(t *testing.T) {
 		err    error
 	}{
 		{
-			msg: "regex header matches will fail due to lack of support",
+			msg: "regex header matches convert correctly",
 			input: []gatewayv1alpha2.HTTPHeaderMatch{{
 				Type:  &regexType,
 				Name:  "Content-Type",

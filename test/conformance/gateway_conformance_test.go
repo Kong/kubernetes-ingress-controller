26859fa70b9d96a8b9959227a84a5829cbc7480e
Shane Utt <shaneutt@linux.com>
2022-03-17 16:02:59 -0400

    tests: add gw conf test HTTPRouteCrossNamespace

--- test/conformance/gateway_conformance_test.go
+++ test/conformance/gateway_conformance_test.go
@@ -4,22 +4,27 @@
 package conformance
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+	"sigs.k8s.io/gateway-api/conformance/tests"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
 
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/controllers/gateway"
 )
 
 var (
-	showDebug                     = true
-	shouldCleanup                 = true
-	conformanceTestsBaseManifests = "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/master/conformance/base/manifests.yaml"
+	showDebug     = true
+	shouldCleanup = true
+
+	manifestRepo                  = "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/master/"
+	conformanceTestsBaseManifests = fmt.Sprintf("%s/conformance/base/manifests.yaml", manifestRepo)
 )
 
 func TestGatewayConformance(t *testing.T) {
@@ -50,5 +55,25 @@ func TestGatewayConformance(t *testing.T) {
 		BaseManifests:    conformanceTestsBaseManifests,
 	})
 	cSuite.Setup(t)
-	// TODO: cSuite.Run(t, tests.ConformanceTests)
+
+	t.Log("configuring gateway conformance tests")
+	for i := range tests.ConformanceTests {
+		for j, manifest := range tests.ConformanceTests[i].Manifests {
+			tests.ConformanceTests[i].Manifests[j] = fmt.Sprintf("%s/conformance/%s", manifestRepo, manifest)
+		}
+	}
+
+	t.Log("running gateway conformance tests")
+	for _, tt := range tests.ConformanceTests {
+		if enabledGatewayConformanceTests.Has(tt.ShortName) {
+			t.Run(tt.Description, func(t *testing.T) { tt.Run(t, cSuite) })
+		}
+	}
 }
+
+// Today we run only the subset below of all Gateway conformance tests.
+// TODO: ensure that this module runs all Gateway conformance tests
+// https://github.com/Kong/kubernetes-ingress-controller/issues/2210
+var enabledGatewayConformanceTests = sets.NewString(
+	"HTTPRouteCrossNamespace",
+)

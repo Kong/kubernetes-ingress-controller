6ee0dfc19ee42c46c997369135140e759adb6556
donna-156 <donna@circleci.com>
2022-05-13 16:59:21 +0100

    rootcmd: Custom SignalHandler to support term-delay
    Context: there are cases where due to network load balancer
    configuration that a delay needs to be introduced to the containers,
    this delay is configurable for the Kong Proxy container through the helm
    chart, however it is not possible to configure this delay for the
    ingress-controller. This can result in stale upstreams during NLB
    draining and downtime for services. Making this delay configurable seems
    the easiest approach to addressing this limitation.
    
    This makes multiple changes:
    1. Adjusts where the SignalHandler is called:
    Originally this was in the entry point to the controller manager,
    however in order to pass through the CLI argument and make the
    term-delay configurable, this needed moved down into the rootcmd
    package.
    
    2. Supports adding a delay to the SignalHandler
    This takes the original SetupSignalHandler, and modifies the logic to
    support setting a time delay. It should still support overriding that
    timedelay by sending a second signal.

--- internal/cmd/fips/main.go
+++ internal/cmd/fips/main.go
@@ -6,13 +6,11 @@ package main
 import (
 	_ "crypto/tls/fipsonly"
 
-	ctrl "sigs.k8s.io/controller-runtime"
-
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/cmd/rootcmd"
 )
 
 //go:generate go run github.com/kong/kubernetes-ingress-controller/v2/hack/generators/controllers/networking
 
 func main() {
-	rootcmd.Execute(ctrl.SetupSignalHandler())
+	rootcmd.Execute()
 }

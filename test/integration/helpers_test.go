6500d1f3d6a12a6aff65b2dde572f4a057f0d732
Patryk Małek <patryk.malek@konghq.com>
2022-07-18 12:46:18 +0200

    tests: print error only when not nil in gateway tests

--- test/integration/helpers_test.go
+++ test/integration/helpers_test.go
@@ -137,23 +137,35 @@ func gatewayLinkStatusMatches(t *testing.T, c *gatewayclient.Clientset, verifyLi
 	var routeParents []gatewayv1alpha2.RouteParentStatus
 
 	// gather a fresh copy of the route, given the specific protocol type
-	switch protocolType { //nolint:exhaustive
+	switch protocolType {
 	case gatewayv1alpha2.HTTPProtocolType:
 		route, err := c.GatewayV1alpha2().HTTPRoutes(namespace).Get(ctx, name, metav1.GetOptions{})
-		t.Logf("error getting http route: %v", err)
-		routeParents = route.Status.Parents
+		if err != nil {
+			t.Logf("error getting http route: %v", err)
+		} else {
+			routeParents = route.Status.Parents
+		}
 	case gatewayv1alpha2.TCPProtocolType:
 		route, err := c.GatewayV1alpha2().TCPRoutes(namespace).Get(ctx, name, metav1.GetOptions{})
-		t.Logf("error getting tcp route: %v", err)
-		routeParents = route.Status.Parents
+		if err != nil {
+			t.Logf("error getting tcp route: %v", err)
+		} else {
+			routeParents = route.Status.Parents
+		}
 	case gatewayv1alpha2.UDPProtocolType:
 		route, err := c.GatewayV1alpha2().UDPRoutes(namespace).Get(ctx, name, metav1.GetOptions{})
-		t.Logf("error getting udp route: %v", err)
-		routeParents = route.Status.Parents
+		if err != nil {
+			t.Logf("error getting udp route: %v", err)
+		} else {
+			routeParents = route.Status.Parents
+		}
 	case gatewayv1alpha2.TLSProtocolType:
 		route, err := c.GatewayV1alpha2().TLSRoutes(namespace).Get(ctx, name, metav1.GetOptions{})
-		t.Logf("error getting tls route: %v", err)
-		routeParents = route.Status.Parents
+		if err != nil {
+			t.Logf("error getting tls route: %v", err)
+		} else {
+			routeParents = route.Status.Parents
+		}
 	default:
 		t.Fatalf("protocol %s not supported", string(protocolType))
 	}

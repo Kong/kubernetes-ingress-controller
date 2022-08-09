bce1a53a1414f4b9fcb5e5136ea934f3ca25673c
Jarek Mroz <jarek.mroz@konghq.com>
2022-08-09 15:43:52 +0200

    ci: Red Hat Scan registry release pipeline

---
+++ .github/actions/redhat-opdev-preflight-action/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Fail fast.
+set -euo pipefail
+
+# Login.
+echo "$INPUT_PASSWORD"  | docker login scan.connect.redhat.com -u "${INPUT_USERNAME}" --password-stdin
+
+# Run checks.
+docker run                                                          \
+    -v "/var/run/docker.sock":"/var/run/docker.sock"                \
+    -v "/home/runner/.docker/":"/docker"                            \
+    quay.io/opdev/preflight:stable                                  \
+    check container "${INPUT_IMAGE}"                                \
+    --docker-config=/docker/config.json                             \
+    --submit                                                        \
+    --certification-project-id="${INPUT_PROJECTID}"                 \
+    --pyxis-api-token="${INPUT_APITOKEN}"

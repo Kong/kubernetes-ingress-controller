16c65d98de44e318e9a4c77c38d74d5e7b509eaf
Jarek Mroz <jarek.mroz@konghq.com>
2022-08-11 16:19:03 +0200

    fix: add check for empty env vars in the preflight action

--- .github/actions/redhat-opdev-preflight-action/entrypoint.sh
+++ .github/actions/redhat-opdev-preflight-action/entrypoint.sh
@@ -1,7 +1,27 @@
-#!/bin/bash
+# !/bin/bash
 
 # Fail fast.
 set -euo pipefail
+
+
+# Required environment variables can not be empty.
+require_env() {
+    local env_vars_names=("$@")
+    local missing_vars=0
+
+    for name in "${env_vars_names[@]}"; do
+        if [[ -z "${!name:-}" ]]; then
+            printf "Require non empty environment variable: %s\n" ${name}
+            missing_vars=1
+        fi
+    done
+
+    if [[ ${missing_vars} -gt 0 ]]; then
+        exit 1
+    fi
+}
+require_env INPUT_PASSWORD INPUT_USERNAME INPUT_CERTIFICATIONID INPUT_IMAGE
+
 
 # Login.
 echo "$INPUT_PASSWORD"  | docker login scan.connect.redhat.com -u "${INPUT_USERNAME}" --password-stdin

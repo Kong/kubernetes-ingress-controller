4147b50c91eb0c3cc37b1fb10ef3db7e29153d6e
Jarek Mroz <jarek.mroz@konghq.com>
2022-08-12 13:14:35 +0200

    fix: make submit optional action

--- .github/actions/redhat-opdev-preflight-action/entrypoint.sh
+++ .github/actions/redhat-opdev-preflight-action/entrypoint.sh
@@ -8,6 +8,21 @@ env
 # Login.
 echo "$INPUT_PASSWORD"  | docker login scan.connect.redhat.com -u "${INPUT_USERNAME}" --password-stdin
 
+
+if [[ -z "${INPUT_SUBMIT:-}" ]]; then
+  echo "Skipping submission to scan.connect.redhat.com"
+
+  docker run                                                          \
+    -v "/var/run/docker.sock":"/var/run/docker.sock"                \
+    -v "/home/runner/.docker/":"/docker"                            \
+    quay.io/opdev/preflight:stable                                  \
+    check container "${INPUT_IMAGE}"                                \
+    --docker-config=/docker/config.json                             
+
+  exit 0
+fi
+
+
 # Run checks.
 docker run                                                          \
     -v "/var/run/docker.sock":"/var/run/docker.sock"                \

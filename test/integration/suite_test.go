b92075b02221a8bf0adcc62c628747aeeb994dee
Yi Tao <tao.yi@konghq.com>
2022-09-26 17:08:35 +0800

    restore comments for setting default k8s version

--- test/integration/suite_test.go
+++ test/integration/suite_test.go
@@ -29,7 +29,17 @@ import (
 	testutils "github.com/kong/kubernetes-ingress-controller/v2/internal/util/test"
 )
 
-// set default k8s version for building kind clusters to v1.24.4.
+// For some reason we've ended up using kind v0.15.0 (which by default deploys k8s v1.25)
+// even though that
+//   - our CI runners use ubuntu-latest (which at the time of writing this comment was ubuntu20.04)
+//     which uses kind v0.14 https://github.com/actions/runner-images/blob/main/images/linux/Ubuntu2004-Readme.md
+//   - when used with ktf (which at the time of writing this comment was set to v0.19.0) we
+//     should use kind v0.14 since that what ktf has set as dependency
+//
+// With all that said, we still managed to get kind v0.15 on our CI
+// https://github.com/Kong/kubernetes-ingress-controller/runs/8211490522?check_suite_focus=true#step:5:6
+// which causes issues down the line (metallb manifests using PSP which is not available
+// in k8s v1.25+).
 var defaultKindClusterVesion = semver.Version{
 	Major: 1,
 	Minor: 24,

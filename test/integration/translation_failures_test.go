d2dbf1fb2e085512afa4c1cac6d710b42cbc33dd
Grzegorz Burzyński <czeslavo@gmail.com>
2022-11-03 12:28:54 +0100

    add integration tests

--- test/integration/translation_failures_test.go
+++ test/integration/translation_failures_test.go
@@ -8,36 +8,108 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kong/kubernetes-ingress-controller/v2/internal/dataplane"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kong/kubernetes-ingress-controller/v2/internal/annotations"
+	"github.com/kong/kubernetes-ingress-controller/v2/internal/dataplane"
+	kongv1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1"
+	"github.com/kong/kubernetes-ingress-controller/v2/pkg/clientset"
 )
+
+const InvalidCASecretID = "8214a145-a328-4c56-ab72-2973a56d4eae"
+
+func invalidCASecret(ns string) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "ca-secret-",
+			Namespace:    ns,
+			Labels: map[string]string{
+				"konghq.com/ca-cert": "true",
+			},
+			Annotations: map[string]string{
+				annotations.IngressClassKey: ingressClass,
+			},
+		},
+		Data: map[string][]byte{
+			"id": []byte(InvalidCASecretID),
+			// missing cert key
+		},
+	}
+}
+
+func pluginUsingInvalidCACert(ns string) *kongv1.KongPlugin {
+	return &kongv1.KongPlugin{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "kong-plugin-",
+			Namespace:    ns,
+			Annotations: map[string]string{
+				annotations.IngressClassKey: ingressClass,
+			},
+		},
+		Config:     v1.JSON{Raw: []byte(fmt.Sprintf(`{"ca_certificates": ["%s"]}`, InvalidCASecretID))},
+		PluginName: "mtls-auth",
+	}
+}
 
 // TestTranslationFailures ensures that proper warning Kubernetes events are recorded in case of translation failures
 // encountered.
 func TestTranslationFailures(t *testing.T) {
-	ns, cleaner := setup(t)
-	defer func() { assert.NoError(t, cleaner.Cleanup(ctx)) }()
-
 	testCases := []struct {
-		name                      string
-		translationFailureTrigger func()
-		expectEventsForObjects    []string
-	}{}
+		name string
+		// translationFailureTrigger should create objects that trigger translation failure and return the objects
+		// that we expect translation failure warning events to be created for.
+		translationFailureTrigger func(ns string) []client.Object
+	}{
+		{
+			name: "invalid CA secret",
+			translationFailureTrigger: func(ns string) []client.Object {
+				createdSecret, err := env.Cluster().Client().CoreV1().Secrets(ns).Create(ctx, invalidCASecret(ns), metav1.CreateOptions{})
+				require.NoError(t, err)
+
+				return []client.Object{createdSecret}
+			},
+		},
+		{
+			name: "invalid CA secret referred by a plugin",
+			translationFailureTrigger: func(ns string) []client.Object {
+				createdSecret, err := env.Cluster().Client().CoreV1().Secrets(ns).Create(ctx, invalidCASecret(ns), metav1.CreateOptions{})
+				require.NoError(t, err)
+
+				c, err := clientset.NewForConfig(env.Cluster().Config())
+				require.NoError(t, err)
+				createdPlugin, err := c.ConfigurationV1().KongPlugins(ns).Create(ctx, pluginUsingInvalidCACert(ns), metav1.CreateOptions{})
+
+				// expect events for both: a faulty secret and a plugin referring it
+				return []client.Object{createdSecret, createdPlugin}
+			},
+		},
+	}
 
 	for _, tt := range testCases {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			tt.translationFailureTrigger()
+			ns, cleaner := setup(t)
+			defer func() { assert.NoError(t, cleaner.Cleanup(ctx)) }()
+
+			expectedCausingObjects := tt.translationFailureTrigger(ns.GetName())
 
 			require.Eventually(t, func() bool {
 				eventsForAllObjectsFound := true
-				for _, objectName := range tt.expectEventsForObjects {
+
+				for _, expectedCausingObject := range expectedCausingObjects {
 					events, err := env.Cluster().Client().CoreV1().Events(ns.GetName()).List(ctx, metav1.ListOptions{
-						FieldSelector: fmt.Sprintf("reason=%s", dataplane.KongConfigurationTranslationFailedEventReason),
+						FieldSelector: fmt.Sprintf(
+							"reason=%s,type=%s,involvedObject.name=%s",
+							dataplane.KongConfigurationTranslationFailedEventReason,
+							corev1.EventTypeWarning,
+							expectedCausingObject.GetName(),
+						),
 					})
 					if err != nil {
 						t.Logf("failed to list events: %s", err)
@@ -45,12 +117,13 @@ func TestTranslationFailures(t *testing.T) {
 					}
 
 					if len(events.Items) == 0 {
-						t.Logf("waiting for events related to %s to be created", objectName)
+						t.Logf("waiting for events related to '%s' to be created", expectedCausingObject.GetName())
 						eventsForAllObjectsFound = false
 					}
 				}
+
 				return eventsForAllObjectsFound
-			}, time.Minute, time.Second)
+			}, time.Minute*5, time.Second)
 		})
 	}
 }

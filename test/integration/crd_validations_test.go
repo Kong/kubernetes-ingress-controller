b8b01648ed7600e4ec88c5947fd8fefc426a3fcf
Grzegorz Burzyński <czeslavo@gmail.com>
2022-11-08 16:49:20 +0100

    lint

--- test/integration/crd_validations_test.go
+++ test/integration/crd_validations_test.go
@@ -6,15 +6,13 @@ package integration
 import (
 	"testing"
 
-	"k8s.io/apimachinery/pkg/util/intstr"
-
-	corev1 "k8s.io/api/core/v1"
-
 	"github.com/google/uuid"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/annotations"
 	kongv1beta1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1beta1"

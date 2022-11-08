eed8e6ff420c194e63b94338e6fba6a3772fcc87
Grzegorz Burzyński <czeslavo@gmail.com>
2022-11-08 14:48:09 +0100

    align failures test

--- internal/dataplane/parser/failures_test.go
+++ internal/dataplane/parser/failures_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/dataplane/parser"
@@ -14,10 +15,6 @@ import (
 )
 
 const someValidTranslationFailureReason = "some valid reason"
-
-func someValidTranslationFailureCausingObjects() []client.Object {
-	return []client.Object{&kongv1.KongIngress{}, &kongv1.KongPlugin{}}
-}
 
 func TestTranslationFailure(t *testing.T) {
 	t.Run("is created and returns reason and causing objects", func(t *testing.T) {
@@ -35,16 +32,32 @@ func TestTranslationFailure(t *testing.T) {
 	})
 
 	t.Run("requires at least one causing object", func(t *testing.T) {
-		_, err := parser.NewTranslationFailure(someValidTranslationFailureReason, someValidTranslationFailureCausingObjects()[0])
+		_, err := parser.NewTranslationFailure(someValidTranslationFailureReason, validCausingObject())
 		require.NoError(t, err)
 
 		_, err = parser.NewTranslationFailure(someValidTranslationFailureReason)
 		require.Error(t, err)
 	})
 
-	t.Run("requires non nil objects", func(t *testing.T) {
+	t.Run("requires valid objects", func(t *testing.T) {
 		_, err := parser.NewTranslationFailure(someValidTranslationFailureReason, nil)
-		require.Error(t, err)
+		assert.Error(t, err, "expected a nil object to be rejected")
+
+		emptyGVK := validCausingObject()
+		emptyGVK.APIVersion = ""
+		emptyGVK.Kind = ""
+		_, err = parser.NewTranslationFailure(someValidTranslationFailureReason, emptyGVK)
+		assert.Error(t, err, "expected an empty GVK object to be rejected")
+
+		noName := validCausingObject()
+		noName.Name = ""
+		_, err = parser.NewTranslationFailure(someValidTranslationFailureReason, noName)
+		assert.Error(t, err, "expected an empty name object to be rejected")
+
+		noNamespace := validCausingObject()
+		noNamespace.Namespace = ""
+		_, err = parser.NewTranslationFailure(someValidTranslationFailureReason, noNamespace)
+		assert.Error(t, err, "expected an empty namespace object to be rejected")
 	})
 }
 
@@ -96,5 +109,22 @@ func TestTranslationFailuresCollector(t *testing.T) {
 func assertErrorLogs(t *testing.T, logHook *test.Hook) {
 	for i := range logHook.AllEntries() {
 		assert.Equalf(t, logrus.ErrorLevel, logHook.AllEntries()[i].Level, "%d-nth log entry expected to have ErrorLevel", i)
+	}
+}
+
+func someValidTranslationFailureCausingObjects() []client.Object {
+	return []client.Object{validCausingObject(), validCausingObject()}
+}
+
+func validCausingObject() *kongv1.KongPlugin {
+	return &kongv1.KongPlugin{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "KongPlugin",
+			APIVersion: kongv1.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "plugin-name",
+			Namespace: "default",
+		},
 	}
 }

5dc471280ffd55971863ab20a21a9fea1c3360d6
Grzegorz Burzyński <czeslavo@gmail.com>
2022-11-30 17:08:39 +0100

    chore(docs): add workflow for synchronizing docs with docs.konghq.com (#3197)
    It adds a workflow that synchronize CRD's auto-generated reference with docs.konghq.com repository. It does some post-processing required for docs only (adding a title, turning vale linting off).
    
    Typically the workflow will be triggered manually on every release, but it can also be used when docs synchronization will be required between releases (caution has to be taken in order to not publish API changes that haven't been released yet).
    
    Co-authored-by: Patryk Małek <patryk.malek@konghq.com>

---
+++ scripts/apidocs-gen/post-process-for-konghq.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script adapts auto-generated api-reference.md to the requirements of
+# docs.konghq.com:
+#   - adds a title section
+#   - turns vale linter off for the whole document
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT="$(dirname "${BASH_SOURCE[0]}")/../.."
+CRD_REF_DOC="${SCRIPT_ROOT}/docs/api-reference.md"
+POST_PROCESSED_DOC="${1}"
+
+echo "---
+title: Custom Resource Definitions API Reference
+---
+<!-- vale off -->
+" > "${POST_PROCESSED_DOC}"
+cat "${CRD_REF_DOC}" >> "${POST_PROCESSED_DOC}"
+echo "<!-- vale on -->" >> "${POST_PROCESSED_DOC}"

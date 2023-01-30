f7b25f78ca747774f8b5663cb5b5a94906dc5145
Grzegorz Burzyński <czeslavo@gmail.com>
2023-01-30 13:26:13 +0100

    feat(konnect): add manifests for konnect config variant

--- scripts/build-single-manifests.sh
+++ scripts/build-single-manifests.sh
@@ -12,3 +12,4 @@ ${REPO_ROOT}/bin/kustomize build config/base > deploy/single/all-in-one-dbless.y
 ${REPO_ROOT}/bin/kustomize build config/variants/postgres > deploy/single/all-in-one-postgres.yaml
 ${REPO_ROOT}/bin/kustomize build config/variants/enterprise > deploy/single/all-in-one-dbless-k4k8s-enterprise.yaml
 ${REPO_ROOT}/bin/kustomize build config/variants/enterprise-postgres > deploy/single/all-in-one-postgres-enterprise.yaml
+${REPO_ROOT}/bin/kustomize build config/variants/konnect/base > deploy/single/all-in-one-dbless-konnect.yaml

36b520680df37cb1e04d9504e1fc2f93eceea83b
Grzegorz Burzyński <czeslavo@gmail.com>
2023-02-06 21:20:01 +0100

    refactor: extract hasConfigurationChanged (#3498)

--- internal/dataplane/sendconfig/sendconfig.go
+++ internal/dataplane/sendconfig/sendconfig.go
@@ -28,8 +28,6 @@ import (
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/metrics"
 )
 
-const initialHash = "00000000000000000000000000000000"
-
 // -----------------------------------------------------------------------------
 // Sendconfig - Public Functions
 // -----------------------------------------------------------------------------
@@ -50,30 +48,13 @@ func PerformUpdate(ctx context.Context,
 
 	// disable optimization if reverse sync is enabled
 	if !config.EnableReverseSync {
-		// use the previous SHA to determine whether or not to perform an update
-		if bytes.Equal(oldSHA, newSHA) {
-			if !hasSHAUpdateAlreadyBeenReported(newSHA) {
-				log.Debugf("sha %s has been reported", hex.EncodeToString(newSHA))
-			}
-
-			// we assume ready as not all Kong versions provide their configuration hash,
-			// and their readiness state is always unknown
-			ready := true
-
-			status, err := client.AdminAPIClient().Status(ctx)
-			if err != nil {
-				return nil, err
-			}
-
-			if status.ConfigurationHash == initialHash {
-				ready = false
-			}
-
-			if ready {
-				log.Debug("no configuration change, skipping sync to kong")
-				return oldSHA, nil
-			}
-			log.Debugf("starting to send configuration (hash: %s)", status.ConfigurationHash)
+		configurationChanged, err := hasConfigurationChanged(ctx, oldSHA, newSHA, client, client.AdminAPIClient(), log)
+		if err != nil {
+			return nil, err
+		}
+		if !configurationChanged {
+			log.Debug("no configuration change, skipping sync to Kong")
+			return oldSHA, nil
 		}
 	}
 
@@ -287,4 +268,64 @@ func isConflictErr(err error) bool {
 	}
 
 	return false
+}
+
+type KonnectAwareClient interface {
+	IsKonnect() bool
+}
+
+type StatusClient interface {
+	Status(context.Context) (*kong.Status, error)
+}
+
+// hasConfigurationChanged verifies whether configuration has changed by comparing old and new config's SHAs.
+// In case the SHAs are equal, it still can return true if a client is considered crashed based on its status.
+func hasConfigurationChanged(
+	ctx context.Context,
+	oldSHA, newSHA []byte,
+	client KonnectAwareClient,
+	statusClient StatusClient,
+	log logrus.FieldLogger,
+) (bool, error) {
+	if !bytes.Equal(oldSHA, newSHA) {
+		return true, nil
+	}
+	if !hasSHAUpdateAlreadyBeenReported(newSHA) {
+		log.Debugf("sha %s has been reported", hex.EncodeToString(newSHA))
+	}
+	// In case of Konnect, we skip further steps as it doesn't report its configuration hash.
+	if client.IsKonnect() {
+		return false, nil
+	}
+
+	hasNoConfiguration, err := kongHasNoConfiguration(ctx, statusClient, log)
+	if err != nil {
+		return false, fmt.Errorf("failed to verify kong readiness: %w", err)
+	}
+	// Kong instance has no configuration, we should push despite the oldSHA and newSHA being equal.
+	if hasNoConfiguration {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+const wellKnownInitialHash = "00000000000000000000000000000000"
+
+// kongHasNoConfiguration checks Kong's status endpoint and read its config hash.
+// If the config hash reported by Kong is the known empty hash, it's considered crashed.
+// This allows providing configuration to Kong instances that have unexpectedly crashed and
+// lost their configuration.
+func kongHasNoConfiguration(ctx context.Context, client StatusClient, log logrus.FieldLogger) (bool, error) {
+	status, err := client.Status(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	if hasNoConfig := status.ConfigurationHash == wellKnownInitialHash; hasNoConfig {
+		log.Debugf("starting to send configuration (hash: %s)", status.ConfigurationHash)
+		return true, nil
+	}
+
+	return false, nil
 }
--- internal/dataplane/sendconfig/sendconfig_test.go
+++ internal/dataplane/sendconfig/sendconfig_test.go
@@ -1,6 +1,7 @@
 package sendconfig
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net"
@@ -9,6 +10,7 @@ import (
 
 	deckutils "github.com/kong/deck/utils"
 	"github.com/kong/go-kong/kong"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -102,4 +104,105 @@ func TestPushFailureReason(t *testing.T) {
 			require.Equal(t, tc.expectedReason, reason)
 		})
 	}
+}
+
+type konnectAwareClientMock struct {
+	expected bool
+}
+
+func (c konnectAwareClientMock) IsKonnect() bool {
+	return c.expected
+}
+
+type statusClientMock struct {
+	expectedValue *kong.Status
+	expectedError error
+}
+
+func (c statusClientMock) Status(context.Context) (*kong.Status, error) {
+	return c.expectedValue, c.expectedError
+}
+
+func TestHasConfigurationChanged(t *testing.T) {
+	ctx := context.Background()
+	testSHAs := [][]byte{
+		[]byte("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"),
+		[]byte("82e35a63ceba37e9646434c5dd412ea577147f1e4a41ccde1614253187e3dbf9"),
+	}
+
+	testCases := []struct {
+		name           string
+		oldSHA, newSHA []byte
+		statusSHA      string
+		isKonnect      bool
+		statusError    error
+
+		expectedResult bool
+		expectError    bool
+	}{
+		{
+			name:           "oldSHA != newSHA",
+			oldSHA:         testSHAs[0],
+			newSHA:         testSHAs[1],
+			expectedResult: true,
+		},
+		{
+			name:           "oldSHA == newSHA, but status signals crash",
+			oldSHA:         testSHAs[0],
+			newSHA:         testSHAs[0],
+			statusSHA:      wellKnownInitialHash,
+			expectedResult: true,
+		},
+		{
+			name:           "oldSHA == newSHA and status signals same",
+			oldSHA:         testSHAs[0],
+			newSHA:         testSHAs[0],
+			statusSHA:      string(testSHAs[0]),
+			expectedResult: false,
+		},
+		{
+			name:           "oldSHA == newSHA and status signals other",
+			oldSHA:         testSHAs[0],
+			newSHA:         testSHAs[0],
+			statusSHA:      string(testSHAs[1]),
+			expectedResult: false,
+		},
+		{
+			name:           "oldSHA == newSHA, status would signal crash, but it's konnect",
+			oldSHA:         testSHAs[0],
+			newSHA:         testSHAs[0],
+			statusSHA:      wellKnownInitialHash,
+			isKonnect:      true,
+			expectedResult: false,
+		},
+		{
+			name:        "oldSHA == newSHA, status returns error",
+			oldSHA:      testSHAs[0],
+			newSHA:      testSHAs[0],
+			statusError: errors.New("getting kong status failure"),
+			expectError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			konnectAwareClient := konnectAwareClientMock{expected: tc.isKonnect}
+			statusClient := statusClientMock{
+				expectedValue: &kong.Status{
+					ConfigurationHash: tc.statusSHA,
+				},
+				expectedError: tc.statusError,
+			}
+
+			result, err := hasConfigurationChanged(ctx, tc.oldSHA, tc.newSHA, konnectAwareClient, statusClient, logrus.New())
+			if tc.expectError {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedResult, result)
+		})
+	}
+
 }

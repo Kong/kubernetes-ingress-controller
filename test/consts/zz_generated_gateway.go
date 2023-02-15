eb281dbfffb5214eeb11554b0b7a31abe5a135fe
Mattia Lavacca <lavacca.mattia@gmail.com>
2023-02-15 10:42:05 +0100

    make generate run
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

--- test/consts/zz_generated_gateway.go
+++ test/consts/zz_generated_gateway.go
@@ -3,7 +3,7 @@
 package consts
 
 const (
-	GatewayStandardCRDsKustomizeURL     = "github.com/kubernetes-sigs/gateway-api/config/crd/?ref=v0.6.0"
-	GatewayExperimentalCRDsKustomizeURL = "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.6.0"
-	GatewayRawRepoURL                   = "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v0.6.0"
+	GatewayStandardCRDsKustomizeURL     = "github.com/kubernetes-sigs/gateway-api/config/crd/?ref=v0.6.1"
+	GatewayExperimentalCRDsKustomizeURL = "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.6.1"
+	GatewayRawRepoURL                   = "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v0.6.1"
 )

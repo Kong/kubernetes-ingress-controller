d675f086bdd1d1710dac2a9897343116b35e19ca
Robert Marsal <507871+robertmarsal@users.noreply.github.com>
2023-02-28 21:43:44 +0000

    Update pkg/clientset_test/clientset_test.go
    Co-authored-by: Patryk Małek <malekpatryk+github@gmail.com>

--- pkg/clientset_test/clientset_test.go
+++ pkg/clientset_test/clientset_test.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"testing"
 
-	configurationv1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1"
-	kongfake "github.com/kong/kubernetes-ingress-controller/v2/pkg/clientset/fake"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	
+	configurationv1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1"
+	kongfake "github.com/kong/kubernetes-ingress-controller/v2/pkg/clientset/fake"
 )
 
 func TestClientset(t *testing.T) {

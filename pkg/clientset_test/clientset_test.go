0fe876d9bcd84c9e89ccb85dab9b4d2cc65110a1
Robert Marsal <robert.marsal@adarga.ai>
2023-03-01 09:15:20 +0000

    fix: remove extra whitespace

--- pkg/clientset_test/clientset_test.go
+++ pkg/clientset_test/clientset_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	
+
 	configurationv1 "github.com/kong/kubernetes-ingress-controller/v2/pkg/apis/configuration/v1"
 	kongfake "github.com/kong/kubernetes-ingress-controller/v2/pkg/clientset/fake"
 )

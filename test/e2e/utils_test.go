c3e5c436a8744dfe286be45f94902c8ac4ee6e36
Grzegorz Burzyński <czeslavo@gmail.com>
2023-03-02 19:55:51 +0100

    chore(tests): extend multi-gw e2e test with scaling to 0

--- test/e2e/utils_test.go
+++ test/e2e/utils_test.go
@@ -453,3 +453,19 @@ func isPodReady(pod corev1.Pod) bool {
 	}
 	return false
 }
+
+// ensureNoneOfDeploymentPodsHasCrashed ensures that none of the pods of a deployment has crashed.
+func ensureNoneOfDeploymentPodsHasCrashed(ctx context.Context, t *testing.T, env environments.Environment, deploymentNN types.NamespacedName) {
+	t.Logf("ensuring none of %s deployment pods has crashed", deploymentNN.String())
+
+	pods, err := env.Cluster().Client().CoreV1().Pods(deploymentNN.Namespace).List(ctx, metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app=%s", deploymentNN.Name),
+	})
+	require.NoError(t, err)
+
+	for _, pod := range pods.Items {
+		for _, container := range pod.Spec.Containers {
+			require.Truef(t, containerDidntCrash(pod, container.Name), "controller pod %s/%s crashed", pod.Namespace, pod.Name)
+		}
+	}
+}

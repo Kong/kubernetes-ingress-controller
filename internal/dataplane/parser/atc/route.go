51ad63206f0a19ce8482f9a2b4c3007778e906d2
Travis Raines <571832+rainest@users.noreply.github.com>
2023-03-28 16:35:13 -0700

    wip: add comments to route.go

--- internal/dataplane/parser/atc/route.go
+++ internal/dataplane/parser/atc/route.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kong/go-kong/kong"
 )
 
+// ApplyExpression sets a Matcher as a Kong route's expression and assigns the route the given priority.
 func ApplyExpression(r *kong.Route, m Matcher, priority int) {
 	r.Expression = kong.String(m.Expression())
 	r.Priority = kong.Int(priority)

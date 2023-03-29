4ee6af6407aa7d6cc2635480c5f4772bead21132
Travis Raines <571832+rainest@users.noreply.github.com>
2023-03-28 17:55:35 -0700

    Suggestions for #3780 (#3820)
    * wip: add field comments
    
    * wip: rename FieldHTTPHeader
    
    Rename FieldHTTPHeader to HTTPHeaderField. The other fields use this
    order, so renaming this one for consistency.
    
    * wip: add predicate comments
    
    * wip: add comments to route.go
    
    * wip: add comments to matcher.go
    
    * wip: replace string concat with sprintfs

--- internal/dataplane/parser/atc/predicate.go
+++ internal/dataplane/parser/atc/predicate.go
@@ -1,6 +1,7 @@
 package atc
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -14,12 +15,16 @@ const (
 	FieldTypeIPCIDR
 )
 
+// LHS is the left hand side (the field) of a predicate expression.
 type LHS interface {
+	// FieldType returns the FieldType iota indicating the LHS type.
 	FieldType() FieldType
-	// TODO(naming): use a better name for this method? "String" is too gerneral
+
+	// String returns a string representation of the LHS.
 	String() string
 }
 
+// BinaryOperator is an operator that accepts two arguments within a predicate expression.
 type BinaryOperator string
 
 const (
@@ -46,13 +51,18 @@ const (
 	LiteralTypeIP
 )
 
+// Literal is the right hand side (the value) of a predicate expression.
 type Literal interface {
+	// Type returns the LiteralType iota indicating the Literal type.
 	Type() LiteralType
+
+	// String returns a string representation of the Literal.
 	String() string
 }
 
 var _ Literal = StringLiteral("")
 
+// StringLiteral is a string Literal.
 type StringLiteral string
 
 func (l StringLiteral) Type() LiteralType {
@@ -68,11 +78,12 @@ func (l StringLiteral) String() string {
 	str = strings.ReplaceAll(str, "\r", "\\r")
 	str = strings.ReplaceAll(str, "\t", "\\t")
 
-	return "\"" + str + "\""
+	return fmt.Sprintf("\"%s\"", str)
 }
 
 var _ Literal = IntLiteral(0)
 
+// IntLiteral is an integer Literal.
 type IntLiteral int
 
 func (l IntLiteral) Type() LiteralType {
@@ -83,19 +94,23 @@ func (l IntLiteral) String() string {
 	return strconv.Itoa(int(l))
 }
 
+// Predicate is an expression consisting of two arguments and a comparison operator. Kong's expression router evaluates
+// these to true or false.
 type Predicate struct {
 	field LHS
 	op    BinaryOperator
 	value Literal
 }
 
+// Expression returns a string representation of a Predicate.
 func (p Predicate) Expression() string {
 	lhs := p.field.String()
 	op := string(p.op)
 	rhs := p.value.String()
-	return lhs + " " + op + " " + rhs
+	return fmt.Sprintf("%s %s %s", lhs, op, rhs)
 }
 
+// IsEmpty returns true if a Predicate has no value to compare against.
 func (p Predicate) IsEmpty() bool {
 	return p.value == nil
 }
@@ -144,7 +159,7 @@ func NewPredicateHTTPMethod(op BinaryOperator, value string) Predicate {
 
 func NewPredicateHTTPHeader(key string, op BinaryOperator, value string) Predicate {
 	return Predicate{
-		field: FieldHTTPHeader{
+		field: HTTPHeaderField{
 			HeaderName: key,
 		},
 		op:    op,

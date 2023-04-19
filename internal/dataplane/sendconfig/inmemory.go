623ebc44aa6f7b40f7069294bddb0f847a23ae5d
Yi Tao <tao.yi@konghq.com>
2023-04-19 18:20:56 +0800

    fix: does not remove nulls in plugin configs

--- internal/dataplane/sendconfig/inmemory.go
+++ internal/dataplane/sendconfig/inmemory.go
@@ -10,7 +10,6 @@ import (
 	"github.com/kong/deck/file"
 	"github.com/sirupsen/logrus"
 
-	"github.com/kong/kubernetes-ingress-controller/v2/internal/dataplane/deckgen"
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/metrics"
 	"github.com/kong/kubernetes-ingress-controller/v2/internal/versions"
 )
@@ -48,9 +47,6 @@ func (s UpdateStrategyInMemory) Update(ctx context.Context, targetState *file.Co
 ) {
 	// Kong will error out if this is set
 	targetState.Info = nil
-
-	// Kong errors out if `null`s are present in `config` of plugins
-	deckgen.CleanUpNullsInPluginConfigs(targetState)
 
 	config, err := json.Marshal(targetState)
 	if err != nil {

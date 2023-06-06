4c780d885c7c48a631bc6400bdad55308c283d26
Travis Raines <571832+rainest@users.noreply.github.com>
2023-06-06 16:39:18 -0700

    chore(tests) make generate

--- test/consts/zz_generated_gateway.go
+++ test/consts/zz_generated_gateway.go
@@ -3,8 +3,8 @@
 package consts
 
 const (
-	GatewayAPIVersion                   = "v0.6.1"
-	GatewayStandardCRDsKustomizeURL     = "github.com/kubernetes-sigs/gateway-api/config/crd/?ref=v0.6.1"
-	GatewayExperimentalCRDsKustomizeURL = "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.6.1"
-	GatewayRawRepoURL                   = "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v0.6.1"
+	GatewayAPIVersion                   = "v0.7.1"
+	GatewayStandardCRDsKustomizeURL     = "github.com/kubernetes-sigs/gateway-api/config/crd/?ref=v0.7.1"
+	GatewayExperimentalCRDsKustomizeURL = "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v0.7.1"
+	GatewayRawRepoURL                   = "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v0.7.1"
 )

c027fc0e76bd8abd67f7bc418635c480006ffdb1
github-action-benchmark <github@users.noreply.github.com>
2023-06-28 15:45:34 +0000

    add Go Benchmark (go) benchmark result for dd359c890a281592bb5248fba2cb4f207974d3f1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687967132819,
+  "lastUpdate": 1687967134197,
   "repoUrl": "https://github.com/Kong/kubernetes-ingress-controller",
   "entries": {
     "Go Benchmark": [
@@ -3750,6 +3750,36 @@ window.BENCHMARK_DATA = {
             "value": 71405,
             "unit": "ns/op\t   11096 B/op\t      12 allocs/op",
             "extra": "15645 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dd359c890a281592bb5248fba2cb4f207974d3f1",
+          "message": "chore(deps): bump github.com/GoogleContainerTools/skaffold/v2\n\nBumps [github.com/GoogleContainerTools/skaffold/v2](https://github.com/GoogleContainerTools/skaffold) from 2.5.1 to 2.6.0.\n- [Release notes](https://github.com/GoogleContainerTools/skaffold/releases)\n- [Changelog](https://github.com/GoogleContainerTools/skaffold/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/GoogleContainerTools/skaffold/compare/v2.5.1...v2.6.0)\n\n---\nupdated-dependencies:\n- dependency-name: github.com/GoogleContainerTools/skaffold/v2\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2023-06-28T15:44:32Z",
+          "tree_id": "cef598d1b26f00cb9374ff2b8845b2470eae4517",
+          "url": "https://github.com/Kong/kubernetes-ingress-controller/commit/dd359c890a281592bb5248fba2cb4f207974d3f1"
+        },
+        "date": 1687967128512,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDeckgenGenerateSHA",
+            "value": 70464,
+            "unit": "ns/op\t   11099 B/op\t      12 allocs/op",
+            "extra": "17058 times\n2 procs"
           }
         ]
       },

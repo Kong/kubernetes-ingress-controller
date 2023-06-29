9869fd4e711a58e5610806a77f8a86caa080685c
Grzegorz Burzyński <czeslavo@gmail.com>
2023-06-29 15:40:45 +0200

    chore(ci): make running gke/konnect cleanup separately possible

--- hack/cleanup/main.go
+++ hack/cleanup/main.go
@@ -12,6 +12,12 @@
 // A runtime group is considered orphaned when all conditions are satisfied:
 // 1. It has a label `created_in_tests` with value `true`.
 // 2. It was created more than 1h ago.
+//
+// Usage: `go run ./hack/cleanup [mode]`
+// Where `mode` is one of:
+// - `all` (default): clean up both GKE clusters and Konnect runtime groups
+// - `gke`: clean up only GKE clusters
+// - `konnect`: clean up only Konnect runtime groups
 package main
 
 import (
@@ -25,6 +31,10 @@ import (
 
 const (
 	konnectAccessTokenVar = "TEST_KONG_KONNECT_ACCESS_TOKEN" //nolint:gosec
+
+	cleanupModeAll     = "all"
+	cleanupModeGKE     = "gke"
+	cleanupModeKonnect = "konnect"
 )
 
 var (
@@ -36,29 +46,94 @@ var (
 )
 
 func main() {
-	validateVars()
+	mode, err := getCleanupMode()
+	if err != nil {
+		log.Errorf("error getting cleanup mode: %v\n", err)
+		os.Exit(1)
+	}
 
+	if err := validateVars(mode); err != nil {
+		log.Errorf("error validating vars: %v\n", err)
+		os.Exit(1)
+	}
+
+	cleanupFuncs := resolveCleanupFuncs(mode)
 	ctx := context.Background()
-	if err := cleanupGKEClusters(ctx); err != nil {
-		log.Errorf("error cleaning up GKE clusters: %v\n", err)
-		os.Exit(1)
-	}
-
-	if err := cleanupKonnectRuntimeGroups(ctx); err != nil {
-		log.Errorf("error cleaning up Konnect runtime groups: %v\n", err)
-		os.Exit(1)
+	for _, f := range cleanupFuncs {
+		if err := f(ctx); err != nil {
+			log.Errorf("error running cleanup function: %v\n", err)
+			os.Exit(1)
+		}
 	}
 }
 
-func validateVars() {
-	mustNotBeEmpty(gke.GKECredsVar, gkeCreds)
-	mustNotBeEmpty(gke.GKEProjectVar, gkeProject)
-	mustNotBeEmpty(gke.GKELocationVar, gkeLocation)
-	mustNotBeEmpty(konnectAccessTokenVar, konnectAccessToken)
+func getCleanupMode() (string, error) {
+	if len(os.Args) < 2 {
+		return cleanupModeAll, nil
+	}
+
+	switch os.Args[1] {
+	case cleanupModeGKE:
+	case cleanupModeKonnect:
+	default:
+		return "", fmt.Errorf("invalid cleanup mode: %s", os.Args[1])
+	}
+
+	return os.Args[1], nil
 }
 
-func mustNotBeEmpty(name, value string) {
+func resolveCleanupFuncs(mode string) []func(context.Context) error {
+	switch mode {
+	case cleanupModeGKE:
+		return []func(context.Context) error{
+			cleanupGKEClusters,
+		}
+	case cleanupModeKonnect:
+		return []func(context.Context) error{
+			cleanupKonnectRuntimeGroups,
+		}
+	default:
+		return []func(context.Context) error{
+			cleanupGKEClusters,
+			cleanupKonnectRuntimeGroups,
+		}
+	}
+}
+
+func validateVars(mode string) error {
+	switch mode {
+	case cleanupModeGKE:
+		return validateGKEVars()
+	case cleanupModeKonnect:
+		return validateKonnectVars()
+	default:
+		if err := validateGKEVars(); err != nil {
+			return err
+		}
+		if err := validateKonnectVars(); err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func validateKonnectVars() error {
+	return notEmpty(konnectAccessTokenVar, konnectAccessToken)
+}
+
+func validateGKEVars() error {
+	if err := notEmpty(gke.GKECredsVar, gkeCreds); err != nil {
+		return err
+	}
+	if err := notEmpty(gke.GKEProjectVar, gkeProject); err != nil {
+		return err
+	}
+	return notEmpty(gke.GKELocationVar, gkeLocation)
+}
+
+func notEmpty(name, value string) error {
 	if value == "" {
-		panic(fmt.Sprintf("%s was empty", name))
+		return fmt.Errorf("%s was empty", name)
 	}
+	return nil
 }

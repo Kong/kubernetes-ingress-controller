d85f53a54f4dd734992f8dc6d8540f3495023938
Grzegorz Burzyński <czeslavo@gmail.com>
2023-07-11 15:59:16 +0200

    feat: append stub upstream when config empty in dbless mode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -98,6 +98,11 @@ Adding a new version? You'll need three changes:
   method is adopted to keep the compatibility with traditional router on
   maximum effort.
   [#4240](https://github.com/Kong/kubernetes-ingress-controller/pull/4240)
+- When a translated Kong configuration is empty in DB-less mode, the controller
+  will now send the configuration with a single empty `Upstream`. This is to make
+  Gateways using `/status/ready` as their health check ready after receiving the
+  initial configuration (even if it's empty).
+  [#4316](https://github.com/Kong/kubernetes-ingress-controller/pull/4316)
 
 ### Changed
 
---
+++ internal/dataplane/deckgen/generate_test.go
@@ -0,0 +1,68 @@
+package deckgen_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kong/deck/file"
+	"github.com/kong/go-kong/kong"
+	"github.com/samber/lo"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+
+	"github.com/kong/kubernetes-ingress-controller/v2/internal/dataplane/deckgen"
+	"github.com/kong/kubernetes-ingress-controller/v2/internal/dataplane/kongstate"
+)
+
+func TestToDeckContent(t *testing.T) {
+	defaultTestParams := func() deckgen.GenerateDeckContentParams {
+		return deckgen.GenerateDeckContentParams{
+			FormatVersion: "3.0",
+		}
+	}
+	modifiedDefaultTestParams := func(fn func(p *deckgen.GenerateDeckContentParams)) deckgen.GenerateDeckContentParams {
+		p := defaultTestParams()
+		fn(&p)
+		return p
+	}
+
+	testCases := []struct {
+		name     string
+		params   deckgen.GenerateDeckContentParams
+		input    *kongstate.KongState
+		expected *file.Content
+	}{
+		{
+			name:   "empty",
+			params: defaultTestParams(),
+			input:  &kongstate.KongState{},
+			expected: &file.Content{
+				FormatVersion: "3.0",
+			},
+		},
+		{
+			name: "empty, generate stub entity",
+			params: modifiedDefaultTestParams(func(p *deckgen.GenerateDeckContentParams) {
+				p.AppendStubEntityWhenConfigEmpty = true
+			}),
+			input: &kongstate.KongState{},
+			expected: &file.Content{
+				FormatVersion: "3.0",
+				Upstreams: []file.FUpstream{
+					{
+						Upstream: kong.Upstream{
+							Name: lo.ToPtr(deckgen.StubUpstreamName),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := deckgen.ToDeckContent(context.Background(), logrus.New(), tc.input, tc.params)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}

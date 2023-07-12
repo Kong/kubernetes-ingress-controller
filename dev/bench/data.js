24459ea96c7c48d136ae125d07b8beda29cfc0c0
github-action-benchmark <github@users.noreply.github.com>
2023-07-12 14:48:42 +0000

    add Go Benchmark (go) benchmark result for 429862e153fb237f54dbdaf3a418c3bcd7f69b59

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689173320258,
+  "lastUpdate": 1689173322560,
   "repoUrl": "https://github.com/Kong/kubernetes-ingress-controller",
   "entries": {
     "Go Benchmark": [
@@ -14550,6 +14550,48 @@ window.BENCHMARK_DATA = {
             "value": 12,
             "unit": "allocs/op",
             "extra": "15027 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "czeslavo@gmail.com",
+            "name": "Grzegorz Burzyński",
+            "username": "czeslavo"
+          },
+          "committer": {
+            "email": "czeslavo@gmail.com",
+            "name": "Grzegorz Burzyński",
+            "username": "czeslavo"
+          },
+          "distinct": true,
+          "id": "429862e153fb237f54dbdaf3a418c3bcd7f69b59",
+          "message": "address review comments",
+          "timestamp": "2023-07-12T16:47:00+02:00",
+          "tree_id": "c65c893969b96f5703665138009cfd44f747df6c",
+          "url": "https://github.com/Kong/kubernetes-ingress-controller/commit/429862e153fb237f54dbdaf3a418c3bcd7f69b59"
+        },
+        "date": 1689173314460,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - ns/op",
+            "value": 85025,
+            "unit": "ns/op",
+            "extra": "15309 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - B/op",
+            "value": 11099,
+            "unit": "B/op",
+            "extra": "15309 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - allocs/op",
+            "value": 12,
+            "unit": "allocs/op",
+            "extra": "15309 times\n2 procs"
           }
         ]
       },

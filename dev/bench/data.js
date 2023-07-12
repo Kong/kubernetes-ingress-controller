170935ed52129ebcfb2543d0d955fd85b9da2ed1
github-action-benchmark <github@users.noreply.github.com>
2023-07-12 13:27:23 +0000

    add Go Benchmark (go) benchmark result for fa2cddd0ea32f0fff674a47e19132f1cfd71e7b2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689168441457,
+  "lastUpdate": 1689168442964,
   "repoUrl": "https://github.com/Kong/kubernetes-ingress-controller",
   "entries": {
     "Go Benchmark": [
@@ -14004,6 +14004,48 @@ window.BENCHMARK_DATA = {
             "value": 12,
             "unit": "allocs/op",
             "extra": "15022 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "czeslavo@gmail.com",
+            "name": "Grzegorz Burzyński",
+            "username": "czeslavo"
+          },
+          "committer": {
+            "email": "czeslavo@gmail.com",
+            "name": "Grzegorz Burzyński",
+            "username": "czeslavo"
+          },
+          "distinct": true,
+          "id": "fa2cddd0ea32f0fff674a47e19132f1cfd71e7b2",
+          "message": "fix(konnect): update nodes status only when it actually changes",
+          "timestamp": "2023-07-12T15:26:17+02:00",
+          "tree_id": "1aaa708ec2959cdc333de143409273a16803e41f",
+          "url": "https://github.com/Kong/kubernetes-ingress-controller/commit/fa2cddd0ea32f0fff674a47e19132f1cfd71e7b2"
+        },
+        "date": 1689168436811,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - ns/op",
+            "value": 72736,
+            "unit": "ns/op",
+            "extra": "17305 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - B/op",
+            "value": 11099,
+            "unit": "B/op",
+            "extra": "17305 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - allocs/op",
+            "value": 12,
+            "unit": "allocs/op",
+            "extra": "17305 times\n2 procs"
           }
         ]
       },

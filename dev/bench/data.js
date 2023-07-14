9e17554af9b8ab1dc4dfe091c94538db54d646bf
github-action-benchmark <github@users.noreply.github.com>
2023-07-14 10:22:08 +0000

    add Go Benchmark (go) benchmark result for fa35a09fe0b6a92f165ffea8169ad9290756cff4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689330095140,
+  "lastUpdate": 1689330127972,
   "repoUrl": "https://github.com/Kong/kubernetes-ingress-controller",
   "entries": {
     "Go Benchmark": [
@@ -19380,6 +19380,48 @@ window.BENCHMARK_DATA = {
             "value": 12,
             "unit": "allocs/op",
             "extra": "16903 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jakub.warczarek@konghq.com",
+            "name": "Jakub Warczarek",
+            "username": "programmer04"
+          },
+          "committer": {
+            "email": "jakub.warczarek@konghq.com",
+            "name": "Jakub Warczarek",
+            "username": "programmer04"
+          },
+          "distinct": true,
+          "id": "fa35a09fe0b6a92f165ffea8169ad9290756cff4",
+          "message": "fix(e2e): proper checking if Kong Enterprise has valid license",
+          "timestamp": "2023-07-14T12:18:45+02:00",
+          "tree_id": "8b354e8706cf00c4a87824f7a4459c223ad54f96",
+          "url": "https://github.com/Kong/kubernetes-ingress-controller/commit/fa35a09fe0b6a92f165ffea8169ad9290756cff4"
+        },
+        "date": 1689330123345,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - ns/op",
+            "value": 69414,
+            "unit": "ns/op",
+            "extra": "18354 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - B/op",
+            "value": 11098,
+            "unit": "B/op",
+            "extra": "18354 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - allocs/op",
+            "value": 12,
+            "unit": "allocs/op",
+            "extra": "18354 times\n2 procs"
           }
         ]
       }

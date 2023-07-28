e4c27419aa75a389240bc54bc70f16c1c41a66ef
Grzegorz Burzyński <czeslavo@gmail.com>
2023-07-28 10:37:38 +0200

    feat: add kubebuilder annotations to programmed conditions

--- pkg/apis/configuration/v1/kongclusterplugin_types.go
+++ pkg/apis/configuration/v1/kongclusterplugin_types.go
@@ -100,6 +100,16 @@ type KongClusterPluginList struct {
 
 // KongClusterPluginStatus represents the current status of the KongClusterPlugin resource.
 type KongClusterPluginStatus struct {
+	// Conditions describe the current conditions of the KongClusterPluginStatus.
+	//
+	// Known condition types are:
+	//
+	// * "Programmed"
+	//
+	// +listType=map
+	// +listMapKey=type
+	// +kubebuilder:validation:MaxItems=8
+	// +kubebuilder:default={{type: "Programmed", status: "Unknown", reason:"Pending", message:"Waiting for controller", lastTransitionTime: "1970-01-01T00:00:00Z"}}
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 

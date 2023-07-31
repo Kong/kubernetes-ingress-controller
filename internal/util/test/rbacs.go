0dfd102d904c8d218b5952036182c4dbe840dea9
Park Jeong Ryul <vita400@kakao.com>
2023-08-01 00:54:25 +0900

    tests: add impersonation to integration test (#4376)

---
+++ internal/util/test/rbacs.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
+
+	"github.com/kong/kubernetes-ingress-controller/v2/test/consts"
+)
+
+const (
+	kongRBACsKustomize        = "../../config/rbac/"
+	kongKnativeRBACsKustomize = "../../config/rbac/knative"
+	kongGatewayRBACsKustomize = "../../config/rbac/gateway"
+	kongCRDsRBACsKustomize    = "../../config/rbac/crds"
+)
+
+func DeployRBACsForCluster(ctx context.Context, cluster clusters.Cluster) error {
+	fmt.Printf("INFO: deploying Kong RBACs to cluster")
+	if err := clusters.KustomizeDeployForCluster(ctx, cluster, kongRBACsKustomize, "-n", consts.ControllerNamespace); err != nil {
+		return err
+	}
+
+	fmt.Printf("INFO: deploying Kong knative RBACs to cluster")
+	if err := clusters.KustomizeDeployForCluster(ctx, cluster, kongKnativeRBACsKustomize, "-n", consts.ControllerNamespace); err != nil {
+		return err
+	}
+
+	fmt.Printf("INFO: deploying Kong gateway RBACs to cluster")
+	if err := clusters.KustomizeDeployForCluster(ctx, cluster, kongGatewayRBACsKustomize, "-n", consts.ControllerNamespace); err != nil {
+		return err
+	}
+
+	fmt.Printf("INFO: deploying Kong CRDs RBACs to cluster")
+	return clusters.KustomizeDeployForCluster(ctx, cluster, kongCRDsRBACsKustomize, "-n", consts.ControllerNamespace)
+}

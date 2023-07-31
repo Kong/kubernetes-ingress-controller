04c465e4fe002095d84638ff947b02c4987f3dc2
Grzegorz Burzyński <czeslavo@gmail.com>
2023-07-31 10:01:05 +0200

    feat: propagate translation failures for KongPlugin and KongClusterPlugin (#4428)

--- test/envtest/programmed_condition_envtest_test.go
+++ test/envtest/programmed_condition_envtest_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8stypes "k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -38,6 +39,7 @@ func TestKongCRDs_ProgrammedCondition(t *testing.T) {
 		objects                     []client.Object
 		getExpectedObjectConditions func(ctrlClient client.Client) ([]metav1.Condition, error)
 		expectedProgrammedStatus    metav1.ConditionStatus
+		expectedProgrammedReason    kongv1.ConditionReason
 	}{
 		{
 			name: "valid KongConsumer",
@@ -65,6 +67,7 @@ func TestKongCRDs_ProgrammedCondition(t *testing.T) {
 				return consumer.Status.Conditions, nil
 			},
 			expectedProgrammedStatus: metav1.ConditionTrue,
+			expectedProgrammedReason: kongv1.ReasonProgrammed,
 		},
 		{
 			name: "KongConsumer referencing non-existent secret",
@@ -93,6 +96,7 @@ func TestKongCRDs_ProgrammedCondition(t *testing.T) {
 				return consumer.Status.Conditions, nil
 			},
 			expectedProgrammedStatus: metav1.ConditionFalse,
+			expectedProgrammedReason: kongv1.ReasonInvalid,
 		},
 		{
 			name: "valid KongConsumerGroup",
@@ -119,6 +123,7 @@ func TestKongCRDs_ProgrammedCondition(t *testing.T) {
 				return consumerGroup.Status.Conditions, nil
 			},
 			expectedProgrammedStatus: metav1.ConditionTrue,
+			expectedProgrammedReason: kongv1.ReasonProgrammed,
 		},
 		{
 			name: "valid KongPlugin",
@@ -155,6 +160,52 @@ func TestKongCRDs_ProgrammedCondition(t *testing.T) {
 				return plugin.Status.Conditions, nil
 			},
 			expectedProgrammedStatus: metav1.ConditionTrue,
+			expectedProgrammedReason: kongv1.ReasonProgrammed,
+		},
+		{
+			name: "invalid KongPlugin",
+			objects: []client.Object{
+				&kongv1.KongPlugin{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:        "invalid-kong-plugin",
+						Namespace:   ns.Name,
+						Annotations: map[string]string{annotations.IngressClassKey: annotations.DefaultIngressClass},
+					},
+					PluginName: "plugin",
+					// Specifying both Config and ConfigFrom is invalid.
+					Config: apiextensionsv1.JSON{Raw: []byte(`{"key": "value"}`)},
+					ConfigFrom: &kongv1.ConfigSource{
+						SecretValue: kongv1.SecretValueFromSource{
+							Secret: "secret",
+							Key:    "key",
+						},
+					},
+				},
+				&kongv1.KongConsumer{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "consumer-for-invalid-plugin",
+						Namespace: ns.Name,
+						Annotations: map[string]string{
+							annotations.IngressClassKey:                           annotations.DefaultIngressClass,
+							annotations.AnnotationPrefix + annotations.PluginsKey: "invalid-kong-plugin",
+						},
+					},
+					Username: "foo",
+				},
+			},
+			getExpectedObjectConditions: func(ctrlClient client.Client) ([]metav1.Condition, error) {
+				var plugin kongv1.KongPlugin
+				err := ctrlClient.Get(ctx, k8stypes.NamespacedName{
+					Name:      "invalid-kong-plugin",
+					Namespace: ns.Name,
+				}, &plugin)
+				if err != nil {
+					return nil, err
+				}
+				return plugin.Status.Conditions, nil
+			},
+			expectedProgrammedStatus: metav1.ConditionFalse,
+			expectedProgrammedReason: kongv1.ReasonInvalid,
 		},
 		{
 			name: "valid KongClusterPlugin",
@@ -189,6 +240,52 @@ func TestKongCRDs_ProgrammedCondition(t *testing.T) {
 				return clusterPlugin.Status.Conditions, nil
 			},
 			expectedProgrammedStatus: metav1.ConditionTrue,
+			expectedProgrammedReason: kongv1.ReasonProgrammed,
+		},
+		{
+			name: "invalid KongClusterPlugin",
+			objects: []client.Object{
+				&kongv1.KongPlugin{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:        "invalid-kong-cluster-plugin",
+						Namespace:   ns.Name,
+						Annotations: map[string]string{annotations.IngressClassKey: annotations.DefaultIngressClass},
+					},
+					PluginName: "plugin",
+					// Specifying both Config and ConfigFrom is invalid.
+					Config: apiextensionsv1.JSON{Raw: []byte(`{"key": "value"}`)},
+					ConfigFrom: &kongv1.ConfigSource{
+						SecretValue: kongv1.SecretValueFromSource{
+							Secret: "secret",
+							Key:    "key",
+						},
+					},
+				},
+				&kongv1.KongConsumer{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "consumer-for-invalid-cluster-plugin",
+						Namespace: ns.Name,
+						Annotations: map[string]string{
+							annotations.IngressClassKey:                           annotations.DefaultIngressClass,
+							annotations.AnnotationPrefix + annotations.PluginsKey: "invalid-kong-cluster-plugin",
+						},
+					},
+					Username: "foo",
+				},
+			},
+			getExpectedObjectConditions: func(ctrlClient client.Client) ([]metav1.Condition, error) {
+				var plugin kongv1.KongPlugin
+				err := ctrlClient.Get(ctx, k8stypes.NamespacedName{
+					Name:      "invalid-kong-cluster-plugin",
+					Namespace: ns.Name,
+				}, &plugin)
+				if err != nil {
+					return nil, err
+				}
+				return plugin.Status.Conditions, nil
+			},
+			expectedProgrammedStatus: metav1.ConditionFalse,
+			expectedProgrammedReason: kongv1.ReasonInvalid,
 		},
 	}
 
@@ -208,6 +305,7 @@ func TestKongCRDs_ProgrammedCondition(t *testing.T) {
 				if !conditions.Contain(
 					cs,
 					conditions.WithType(string(kongv1.ConditionProgrammed)),
+					conditions.WithReason(string(tc.expectedProgrammedReason)),
 					conditions.WithStatus(tc.expectedProgrammedStatus),
 				) {
 					t.Logf("Programmed condition not found, actual: %v", cs)

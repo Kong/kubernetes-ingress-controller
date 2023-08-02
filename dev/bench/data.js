7ac71fea9c6b74d23c40baf889d62430accd5a4a
github-action-benchmark <github@users.noreply.github.com>
2023-08-02 14:25:38 +0000

    add Go Benchmark (go) benchmark result for 12a38cbd4d7a3692ebc81af0f0a10ba11a6d2d12

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690985645431,
+  "lastUpdate": 1690986338806,
   "repoUrl": "https://github.com/Kong/kubernetes-ingress-controller",
   "entries": {
     "Go Benchmark": [
@@ -46302,6 +46302,48 @@ window.BENCHMARK_DATA = {
             "value": 12,
             "unit": "allocs/op",
             "extra": "17737 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "12a38cbd4d7a3692ebc81af0f0a10ba11a6d2d12",
+          "message": "chore(deps): bump golang from 1.20.6 to 1.20.7\n\nBumps golang from 1.20.6 to 1.20.7.\n\n---\nupdated-dependencies:\n- dependency-name: golang\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2023-08-02T14:24:29Z",
+          "tree_id": "e8bee17b6ec4faf9966cdd191ee0d500e8d629ac",
+          "url": "https://github.com/Kong/kubernetes-ingress-controller/commit/12a38cbd4d7a3692ebc81af0f0a10ba11a6d2d12"
+        },
+        "date": 1690986330514,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - ns/op",
+            "value": 79004,
+            "unit": "ns/op",
+            "extra": "16441 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - B/op",
+            "value": 11098,
+            "unit": "B/op",
+            "extra": "16441 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - allocs/op",
+            "value": 12,
+            "unit": "allocs/op",
+            "extra": "16441 times\n2 procs"
           }
         ]
       }

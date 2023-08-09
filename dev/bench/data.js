056d1c240f6275cf93e98b7214b2f9487730b1ee
github-action-benchmark <github@users.noreply.github.com>
2023-08-09 14:54:37 +0000

    add Go Benchmark (go) benchmark result for 118da12139292fee3138d67c79fcb315868ebf05

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691592875281,
+  "lastUpdate": 1691592876953,
   "repoUrl": "https://github.com/Kong/kubernetes-ingress-controller",
   "entries": {
     "Go Benchmark": [
@@ -54534,6 +54534,66 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "11887549 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "118da12139292fee3138d67c79fcb315868ebf05",
+          "message": "chore(deps): bump google.golang.org/api from 0.135.0 to 0.136.0\n\nBumps [google.golang.org/api](https://github.com/googleapis/google-api-go-client) from 0.135.0 to 0.136.0.\n- [Release notes](https://github.com/googleapis/google-api-go-client/releases)\n- [Changelog](https://github.com/googleapis/google-api-go-client/blob/main/CHANGES.md)\n- [Commits](https://github.com/googleapis/google-api-go-client/compare/v0.135.0...v0.136.0)\n\n---\nupdated-dependencies:\n- dependency-name: google.golang.org/api\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2023-08-09T14:51:15Z",
+          "tree_id": "c7280f5585480014786a44a29f9da7e2d8e7b12d",
+          "url": "https://github.com/Kong/kubernetes-ingress-controller/commit/118da12139292fee3138d67c79fcb315868ebf05"
+        },
+        "date": 1691592868899,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - ns/op",
+            "value": 67427,
+            "unit": "ns/op",
+            "extra": "18169 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - B/op",
+            "value": 11095,
+            "unit": "B/op",
+            "extra": "18169 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - allocs/op",
+            "value": 12,
+            "unit": "allocs/op",
+            "extra": "18169 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDefaultContentToDBLessConfigConverter_Convert - ns/op",
+            "value": 110.4,
+            "unit": "ns/op",
+            "extra": "12332353 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDefaultContentToDBLessConfigConverter_Convert - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "12332353 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDefaultContentToDBLessConfigConverter_Convert - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "12332353 times\n2 procs"
           }
         ]
       },

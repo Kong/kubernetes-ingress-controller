e95fde067d2ac1903085040df2fed7bc9a6c0618
Jakub Warczarek <jakub.warczarek@konghq.com>
2023-08-17 10:57:25 +0200

    chore(ci): improve logs for test.conformance (#4527)

--- internal/util/test/rbacs.go
+++ internal/util/test/rbacs.go
@@ -17,21 +17,21 @@ const (
 )
 
 func DeployRBACsForCluster(ctx context.Context, cluster clusters.Cluster) error {
-	fmt.Printf("INFO: deploying Kong RBACs to cluster")
+	fmt.Printf("INFO: deploying Kong RBACs to cluster\n")
 	if err := clusters.KustomizeDeployForCluster(ctx, cluster, kongRBACsKustomize, "-n", consts.ControllerNamespace); err != nil {
 		return err
 	}
 
-	fmt.Printf("INFO: deploying Kong knative RBACs to cluster")
+	fmt.Printf("INFO: deploying Kong knative RBACs to cluster\n")
 	if err := clusters.KustomizeDeployForCluster(ctx, cluster, kongKnativeRBACsKustomize, "-n", consts.ControllerNamespace); err != nil {
 		return err
 	}
 
-	fmt.Printf("INFO: deploying Kong gateway RBACs to cluster")
+	fmt.Printf("INFO: deploying Kong gateway RBACs to cluster\n")
 	if err := clusters.KustomizeDeployForCluster(ctx, cluster, kongGatewayRBACsKustomize, "-n", consts.ControllerNamespace); err != nil {
 		return err
 	}
 
-	fmt.Printf("INFO: deploying Kong CRDs RBACs to cluster")
+	fmt.Printf("INFO: deploying Kong CRDs RBACs to cluster\n")
 	return clusters.KustomizeDeployForCluster(ctx, cluster, kongCRDsRBACsKustomize, "-n", consts.ControllerNamespace)
 }

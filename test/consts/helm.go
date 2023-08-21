79a75933bc35e6ed2c2820dcfdfee58287de130c
Tao Yi <tao.yi@konghq.com>
2023-08-21 16:46:34 +0800

    test: update helm chart version to the latest (#4511)
    * test: update helm chart version to 2.26.3
    
    * set effective versions in github workflow files
    
    * update instructions of release issue to make sure effective version of kong updated

--- test/consts/helm.go
+++ test/consts/helm.go
@@ -1,6 +1,4 @@
 package consts
 
 // KongHelmChartVersion is the version of the Kong Helm chart to use in tests.
-// TODO: use chart 2.25.0 as a workaround before charts deal with invalid semver in image tag properly
-// https://github.com/Kong/charts/issues/856
-const KongHelmChartVersion = "2.25.0"
+const KongHelmChartVersion = "2.26.3"

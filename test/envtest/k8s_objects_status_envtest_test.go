6006caba873da26c12c51d900dfc872d25cdd744
Travis Raines <571832+rainest@users.noreply.github.com>
2023-08-22 15:48:38 -0700

    chore(tests) add watch namespace envtest

--- test/envtest/k8s_objects_status_envtest_test.go
+++ test/envtest/k8s_objects_status_envtest_test.go
@@ -107,6 +107,120 @@ func TestHTTPRouteReconciliation_DoesNotBlockSyncLoopWhenStatusQueueBufferIsExce
 	t.Cleanup(func() { _ = ctrlClient.Delete(ctx, &httpRoute) })
 }
 
+func Test_WatchNamespaces(t *testing.T) {
+	scheme := Scheme(t, WithGatewayAPI)
+	envcfg := Setup(t, scheme)
+	ctrlClient := NewControllerClient(t, scheme, envcfg)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	gw := deployGateway(ctx, t, ctrlClient)
+	hidden := CreateNamespace(ctx, t, ctrlClient)
+	RunManager(ctx, t, envcfg, WithPublishService(gw.Namespace), WithGatewayFeatureEnabled, func(cfg *manager.Config) {
+		// Enable status updates and change the queue's buffer size to 0 to
+		// ensure that the status update notifications do not block the
+		// sync loop despite the fact that the status update queue is full.
+		// This is a regression test for https://github.com/Kong/kubernetes-ingress-controller/issues/4260.
+		// The test will timeout if the sync loop blocks (effectively manager.Run does not return after canceling context).
+		cfg.UpdateStatus = true
+		cfg.UpdateStatusQueueBufferSize = 0
+		// hidden is excluded
+		cfg.WatchNamespaces = []string{gw.Namespace}
+	})
+
+	backendService := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: gw.Namespace,
+			Name:      "backend-svc",
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:     "http",
+					Protocol: corev1.ProtocolTCP,
+					Port:     80,
+				},
+			},
+		},
+	}
+
+	hiddenService := backendService
+	hiddenService.Namespace = hidden.Name
+
+	require.NoError(t, ctrlClient.Create(ctx, &backendService))
+	t.Cleanup(func() { _ = ctrlClient.Delete(ctx, &backendService) })
+
+	require.NoError(t, ctrlClient.Create(ctx, &hiddenService))
+	t.Cleanup(func() { _ = ctrlClient.Delete(ctx, &hiddenService) })
+
+	httpRoute := gatewayv1beta1.HTTPRoute{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: gw.Namespace,
+			Name:      uuid.NewString(),
+		},
+		Spec: gatewayv1beta1.HTTPRouteSpec{
+			CommonRouteSpec: gatewayv1beta1.CommonRouteSpec{
+				ParentRefs: []gatewayv1beta1.ParentReference{{
+					Name: gatewayv1beta1.ObjectName(gw.Name),
+				}},
+			},
+			Rules: []gatewayv1beta1.HTTPRouteRule{{
+				Matches: builder.NewHTTPRouteMatch().
+					WithPathPrefix("/path").
+					ToSlice(),
+				BackendRefs: builder.NewHTTPBackendRef(backendService.Name).
+					WithPort(80).
+					ToSlice(),
+			}},
+		},
+	}
+
+	hiddenRoute := httpRoute
+	hiddenRoute.Namespace = hidden.Name
+
+	require.NoError(t, ctrlClient.Create(ctx, &httpRoute))
+	t.Cleanup(func() { _ = ctrlClient.Delete(ctx, &httpRoute) })
+
+	require.NoError(t, ctrlClient.Create(ctx, &hiddenRoute))
+	t.Cleanup(func() { _ = ctrlClient.Delete(ctx, &hiddenRoute) })
+
+	require.Eventually(t, func() bool {
+		nn := k8stypes.NamespacedName{Name: httpRoute.Name, Namespace: httpRoute.Namespace}
+		if err := ctrlClient.Get(ctx, nn, &httpRoute); err != nil {
+			t.Logf("failed to get httpRoute: %v", err)
+			return false
+		}
+		if len(httpRoute.Status.Parents) == 0 {
+			t.Logf("no gateway parent in httpRoute status")
+			return false
+		}
+		programmed, ok := lo.Find(httpRoute.Status.Parents[0].Conditions, func(c metav1.Condition) bool {
+			return c.Type == string(gatewayv1beta1.GatewayConditionProgrammed)
+		})
+		if !ok {
+			t.Logf("no programmed condition in httpRoute status")
+			return false
+		}
+		if programmed.Status != metav1.ConditionTrue {
+			t.Logf("programmed condition is not true")
+			return false
+		}
+		return true
+	}, time.Second*10, time.Millisecond*50)
+
+	require.Never(t, func() bool {
+		nn := k8stypes.NamespacedName{Name: hiddenRoute.Name, Namespace: hiddenRoute.Namespace}
+		if err := ctrlClient.Get(ctx, nn, &hiddenRoute); err != nil {
+			return false
+		}
+		if len(hiddenRoute.Status.Parents) != 0 {
+			t.Logf("gateway parent assigned to ignored namespace HTTPRoute")
+			return true
+		}
+		return false
+	}, time.Second*10, time.Second)
+}
+
 // deployGateway deploys a Gateway, GatewayClass, and publish Service for use in tests.
 func deployGateway(ctx context.Context, t *testing.T, client client.Client) gatewayv1beta1.Gateway {
 	ns := CreateNamespace(ctx, t, client)

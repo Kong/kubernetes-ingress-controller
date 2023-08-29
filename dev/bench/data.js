6afee7d91fff5c8c2518e554d3b9729beeb50c95
github-action-benchmark <github@users.noreply.github.com>
2023-08-29 21:14:29 +0000

    add Go Benchmark (go) benchmark result for 5c768212f21ab5650534f852157c762b8213365f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693343666865,
+  "lastUpdate": 1693343669474,
   "repoUrl": "https://github.com/Kong/kubernetes-ingress-controller",
   "entries": {
     "Go Benchmark": [
@@ -82134,6 +82134,66 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "9525951 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "571832+rainest@users.noreply.github.com",
+            "name": "Travis Raines",
+            "username": "rainest"
+          },
+          "committer": {
+            "email": "571832+rainest@users.noreply.github.com",
+            "name": "Travis Raines",
+            "username": "rainest"
+          },
+          "distinct": true,
+          "id": "5c768212f21ab5650534f852157c762b8213365f",
+          "message": "fix(plugins) add suffix to instance_name\n\nAppend a short hash derived from the attached Kong entity name to plugin\ninstance_name. If the short hash would collide, append the full hash\ninstead.\n\ninstance_name must be unique. Applying the same KongPlugin to multiple\nresources, or resources that generate multiple Kong entities, would\nviolate this rule otherwise.",
+          "timestamp": "2023-08-29T14:13:03-07:00",
+          "tree_id": "4da508407246d30764feb127b0ea935d449d121f",
+          "url": "https://github.com/Kong/kubernetes-ingress-controller/commit/5c768212f21ab5650534f852157c762b8213365f"
+        },
+        "date": 1693343655742,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - ns/op",
+            "value": 67489,
+            "unit": "ns/op",
+            "extra": "17562 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - B/op",
+            "value": 11093,
+            "unit": "B/op",
+            "extra": "17562 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDeckgenGenerateSHA - allocs/op",
+            "value": 12,
+            "unit": "allocs/op",
+            "extra": "17562 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDefaultContentToDBLessConfigConverter_Convert - ns/op",
+            "value": 100.4,
+            "unit": "ns/op",
+            "extra": "13259175 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDefaultContentToDBLessConfigConverter_Convert - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "13259175 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDefaultContentToDBLessConfigConverter_Convert - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "13259175 times\n2 procs"
           }
         ]
       },

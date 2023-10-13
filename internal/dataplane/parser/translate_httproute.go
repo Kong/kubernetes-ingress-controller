3fd65eca7993afc5c42143a5df700bbaa790874c
Yi Tao <tao.yi@konghq.com>
2023-10-13 22:37:38 +0800

    update comments

--- internal/dataplane/parser/translate_httproute.go
+++ internal/dataplane/parser/translate_httproute.go
@@ -202,7 +202,7 @@ func GenerateKongRouteFromTranslation(
 	)
 }
 
-// generateKongRoutesFromHTTPRouteMatches converts an HTTPRouteMatches to a slice of Kong Route objects.
+// generateKongRoutesFromHTTPRouteMatches converts an HTTPRouteMatches to a slice of Kong Route objects with traditional routes.
 // This function assumes that the HTTPRouteMatches share the query params, headers and methods.
 func generateKongRoutesFromHTTPRouteMatches(
 	routeName string,
@@ -232,8 +232,9 @@ func generateKongRoutesFromHTTPRouteMatches(
 		return []kongstate.Route{r}, nil
 	}
 
-	// Kong starts to support query parameter match since 3.4.1 with expression router
-	// so with traditional/traditional compatible router, we do not support query parameter match.
+	// Kong starts to support query parameter match since 3.4.1 with expression router.
+	// This function is only called for Kong with traditional/traditional compatible router,
+	// so we do not support query parameter match here.
 	if len(matches[0].QueryParams) > 0 {
 		return []kongstate.Route{}, translators.ErrRouteValidationQueryParamMatchesUnsupported
 	}

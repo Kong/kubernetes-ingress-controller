a961d7e276b0754365ee9684a332143ebd132d61
Patryk Małek <patryk.malek@konghq.com>
2023-10-18 15:29:22 +0200

    make linter happy

--- internal/util/test/repo.go
+++ internal/util/test/repo.go
@@ -1,13 +1,17 @@
 package test
 
 import (
+	"fmt"
 	"path"
 	"path/filepath"
 	"runtime"
 )
 
 func getRepoRoot() (string, error) {
-	_, b, _, _ := runtime.Caller(0)
+	_, b, _, ok := runtime.Caller(0)
+	if !ok {
+		return "", fmt.Errorf("failed to get repo root: runtime.Caller(0) failed")
+	}
 	d := filepath.Dir(path.Join(path.Dir(b), "../../")) // Number of ../ depends on the path of this file.
 	return filepath.Abs(d)
 }

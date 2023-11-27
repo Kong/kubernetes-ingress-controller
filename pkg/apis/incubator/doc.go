20a39ec60a3b34c0fca45c8c82b0b7cac607101a
Grzegorz Burzyński <czeslavo@gmail.com>
2023-11-27 11:06:58 +0100

    add comment about graduating

--- pkg/apis/incubator/doc.go
+++ pkg/apis/incubator/doc.go
@@ -1,4 +1,8 @@
 // Package incubator contains API Schema definitions for the incubator.konghq.com API group.
 // This group is for APIs that are highly experimental and may not be supported in the future.
 // It's not distributed by default and has to be installed separately.
+//
+// CRDs in this group are not going to graduate to beta or GA in scope of the group.
+// They're unlikely to be promoted to the `configuration.konghq.com` group, but if they do,
+// this will be the way for them to be promoted to beta or GA.
 package incubator

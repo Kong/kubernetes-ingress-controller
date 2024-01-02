e896fa39e8604ae4107e94369e2150e14c039277
Yi Tao <tao.yi@konghq.com>
2024-01-02 11:23:43 +0800

    remove check of namespace in generating translation failures

--- internal/dataplane/failures/failures_test.go
+++ internal/dataplane/failures/failures_test.go
@@ -54,11 +54,6 @@ func TestResourceFailure(t *testing.T) {
 		noName.Name = ""
 		_, err = NewResourceFailure(someValidResourceFailureReason, noName)
 		assert.Error(t, err, "expected an empty name object to be rejected")
-
-		noNamespace := validCausingObject()
-		noNamespace.Namespace = ""
-		_, err = NewResourceFailure(someValidResourceFailureReason, noNamespace)
-		assert.Error(t, err, "expected an empty namespace object to be rejected")
 	})
 }
 

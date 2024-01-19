412f55f77a2deacabeddaf43b84140d910177722
Grzegorz Burzyński <czeslavo@gmail.com>
2024-01-19 20:05:34 +0100

    tests: integrate populating UpstreamPolicy's ancestors status (#5450)

--- test/internal/testlabels/labels.go
+++ test/internal/testlabels/labels.go
@@ -2,12 +2,13 @@ package testlabels
 
 const (
 	// Kind is the label key used to store the primary kind that's being tested.
-	Kind                  = "kind"
-	KindUDPRoute          = "UDPRoute"
-	KindTCPRoute          = "TCPRoute"
-	KindGRPCRoute         = "GRPCRoute"
-	KindIngress           = "Ingress"
-	KindKongServiceFacade = "KongServiceFacade"
+	Kind                   = "kind"
+	KindUDPRoute           = "UDPRoute"
+	KindTCPRoute           = "TCPRoute"
+	KindGRPCRoute          = "GRPCRoute"
+	KindIngress            = "Ingress"
+	KindKongServiceFacade  = "KongServiceFacade"
+	KindKongUpstreamPolicy = "KongUpstreamPolicy"
 )
 
 const (

d24b7e9a9e513a23902cc94dd73cb64688420b25
Yi Tao <tao.yi@konghq.com>
2024-02-02 16:21:58 +0800

    add isolated integration tests

--- test/internal/helpers/kong.go
+++ test/internal/helpers/kong.go
@@ -98,3 +98,13 @@ func GetKongRouterFlavor(ctx context.Context, proxyAdminURL *url.URL, kongTestPa
 	}
 	return routerFlavor, nil
 }
+
+// GetKongLicenses fetches all licenses applied to Kong gateway.
+func GetKongLicenses(ctx context.Context, proxyAdminURL *url.URL, kongTestPassword string) ([]*kong.License, error) {
+	httpClient, err := adminapi.MakeHTTPClient(&adminapi.HTTPClientOpts{}, kongTestPassword)
+	kc, err := kong.NewClient(lo.ToPtr(proxyAdminURL.String()), httpClient)
+	if err != nil {
+		return nil, err
+	}
+	return kc.Licenses.ListAll(ctx)
+}

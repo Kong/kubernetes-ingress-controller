99ea8d01bc579246bb5abc73464305ecf558e73a
Yi Tao <tao.yi@konghq.com>
2024-02-20 15:53:57 +0800

    fix: do not translate licenses if Kong gateway is not enterprise edition

--- internal/manager/run.go
+++ internal/manager/run.go
@@ -172,6 +172,7 @@ func Run(
 		featureGates,
 		routerFlavor,
 		c.UpdateStatus,
+		kongStartUpConfig.Version.IsKongGatewayEnterprise(),
 	)
 
 	referenceIndexers := ctrlref.NewCacheIndexers(setupLog.WithName("reference-indexers"))

dda3db4b894fdad05b2313f72d7988f6c7d8886a
Hoang Quoc Trung <quoctrunghoang1998@gmail.com>
2024-03-13 00:51:23 +0100

    fix(init) remove check for tag support (#5682)
    Remove a check for tag support on the connected Kong proxy instance.
    
    This check could incorrectly delete data if it ran incorrectly.
    
    The current minimum supported Kong version supports tags, so this check is unnecessary.
    The proxy has supported tags since before its 1.0.0 release.
    
    ---------
    
    Signed-off-by: Hoang Quoc Trung <trung.hoang@pricehubble.com>
    Co-authored-by: Travis Raines <571832+rainest@users.noreply.github.com>
    (cherry picked from commit c2b7f8058c248ce038ce090f0ec9178ec8f8ff1f)

--- internal/dataplane/sendconfig/kong.go
+++ internal/dataplane/sendconfig/kong.go
@@ -1,14 +1,7 @@
 package sendconfig
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/blang/semver/v4"
-	"github.com/go-logr/logr"
-	"golang.org/x/sync/errgroup"
-
-	"github.com/kong/kubernetes-ingress-controller/v3/internal/adminapi"
 )
 
 // Config gathers parameters that are needed for sending configuration to Kong Admin APIs.
@@ -40,36 +33,4 @@ type Config struct {
 
 	// SanitizeKonnectConfigDumps indicates whether to sanitize Konnect config dumps.
 	SanitizeKonnectConfigDumps bool
-}
-
-// Init sets up variables that need external calls.
-func (c *Config) Init(
-	ctx context.Context,
-	logger logr.Logger,
-	kongClients []*adminapi.Client,
-) {
-	if err := tagsFilteringEnabled(ctx, kongClients); err != nil {
-		logger.Error(err, "Tag filtering disabled")
-		c.FilterTags = nil
-	} else {
-		logger.Info("Tag filtering enabled", "tags", c.FilterTags)
-	}
-}
-
-func tagsFilteringEnabled(ctx context.Context, kongClients []*adminapi.Client) error {
-	var errg errgroup.Group
-	for _, cl := range kongClients {
-		cl := cl
-		errg.Go(func() error {
-			ok, err := cl.AdminAPIClient().Tags.Exists(ctx)
-			if err != nil {
-				return fmt.Errorf("Kong Admin API (%s) does not support tags: %w", cl.BaseRootURL(), err)
-			}
-			if !ok {
-				return fmt.Errorf("Kong Admin API (%s) does not support tags", cl.BaseRootURL())
-			}
-			return nil
-		})
-	}
-	return errg.Wait()
 }

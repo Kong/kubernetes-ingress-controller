cbda15729b4d1fff0811730787f2ed26983dc605
Patryk Małek <patryk.malek@konghq.com>
2024-05-29 12:20:10 +0200

    tests: fix invalid expectation

--- test/kongintegration/inmemory_update_strategy_test.go
+++ test/kongintegration/inmemory_update_strategy_test.go
@@ -90,7 +90,7 @@ func TestUpdateStrategyInMemory_PropagatesResourcesErrors(t *testing.T) {
 			},
 		},
 	}
-	expectedMessage := "invalid service:test-service: failed conditional validation given value of field 'protocol'"
+	expectedMessage := "invalid path: value must be null"
 	expectedRawErrBody := []byte(`{"code":14,"name":"invalid declarative configuration","fields":{},"message":"declarative config is invalid: {}","flattened_errors":[{"entity_type":"service","entity_name":"test-service","entity_tags":["k8s-name:test-service","k8s-namespace:default","k8s-kind:Service","k8s-uid:a3b8afcc-9f19-42e4-aa8f-5866168c2ad3","k8s-group:","k8s-version:v1"],"errors":[{"type":"field","message":"value must be null","field":"path"},{"type":"entity","message":"failed conditional validation given value of field 'protocol'"}],"entity":{"path":"/test","name":"test-service","protocol":"grpc","tags":["k8s-name:test-service","k8s-namespace:default","k8s-kind:Service","k8s-uid:a3b8afcc-9f19-42e4-aa8f-5866168c2ad3","k8s-group:","k8s-version:v1"],"host":"konghq.com","port":80}}]}`)
 	expectedBody := map[string]interface{}{}
 	require.NoError(t, json.Unmarshal(expectedRawErrBody, &expectedBody))

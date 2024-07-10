44c2f8638878499b8ab32d4310764a8e0f8f6b52
Tao Yi <tao.yi@konghq.com>
2024-07-11 07:02:31 +0800

    fix(plugin): fix plugin reference check (#6295)
    Fix the plugin reference check. This was originally providing reference
    information backwards of how the reference check functions expected it.
    
    Add util function TypeMetaFromGVK.
    
    Add unit tests for plugin reference checks.

--- internal/util/objectmeta.go
+++ internal/util/objectmeta.go
@@ -1,8 +1,7 @@
 package util
 
 import (
-	"maps"
-
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -15,14 +14,27 @@ type K8sObjectInfo struct {
 	GroupVersionKind schema.GroupVersionKind
 }
 
+// FromK8sObject extracts information from a Kubernetes object.
+// It performs a shallow copy of object annotations so any modifications after
+// calling FromK8sObject will have an effect on the original object.
 func FromK8sObject(obj client.Object) K8sObjectInfo {
-	ret := K8sObjectInfo{
-		Name:        obj.GetName(),
-		Namespace:   obj.GetNamespace(),
-		Annotations: maps.Clone(obj.GetAnnotations()),
+	return K8sObjectInfo{
+		Name:             obj.GetName(),
+		Namespace:        obj.GetNamespace(),
+		Annotations:      obj.GetAnnotations(),
+		GroupVersionKind: obj.GetObjectKind().GroupVersionKind(),
 	}
-	if gvk := obj.GetObjectKind().GroupVersionKind(); gvk.String() != "" {
-		ret.GroupVersionKind = gvk
+}
+
+// TypeMetaFromGVK returns typemeta from groupversionkind of a k8s object.
+func TypeMetaFromGVK(gvk schema.GroupVersionKind) metav1.TypeMeta {
+	return metav1.TypeMeta{
+		APIVersion: gvk.GroupVersion().String(),
+		Kind:       gvk.Kind,
 	}
-	return ret
+}
+
+// TypeMetaFromK8sObject returns the typemeta of a client Object by its GVK.
+func TypeMetaFromK8sObject(obj client.Object) metav1.TypeMeta {
+	return TypeMetaFromGVK(obj.GetObjectKind().GroupVersionKind())
 }

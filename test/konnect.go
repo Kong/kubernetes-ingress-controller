815cc921e18e1b537483365b222e97af522684ff
Patryk Małek <patryk.malek@konghq.com>
2024-11-13 20:51:48 +0100

    tests: fix Konnect ControlPlane role cleanup code

--- test/konnect.go
+++ test/konnect.go
@@ -2,6 +2,12 @@ package test
 
 import "os"
 
+const (
+	// KonnectControlPlaneLabelCreatedInTests is the label that is set on all
+	// Konnect control planes created by tests.
+	KonnectControlPlaneLabelCreatedInTests = "created_in_tests"
+)
+
 // KonnectServerURL returns the Konnect server URL to be used for Konnect API
 // requests in tests and CI.
 // It is driven by the TEST_KONG_KONNECT_SERVER_URL environment variable.

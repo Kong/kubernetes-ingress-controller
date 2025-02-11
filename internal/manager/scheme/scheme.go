d9c5ccf74624c377708a83646c225999a51ec09f
Yi Tao <tao.yi@konghq.com>
2025-02-11 16:38:59 +0800

    run AddToScheme only when not done

--- internal/manager/scheme/scheme.go
+++ internal/manager/scheme/scheme.go
@@ -1,6 +1,8 @@
 package scheme
 
 import (
+	"sync/atomic"
+
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -15,46 +17,53 @@ import (
 	incubatorv1alpha1 "github.com/kong/kubernetes-configuration/api/incubator/v1alpha1"
 )
 
+var (
+	kicScheme       = runtime.NewScheme()
+	addToSchemeDone atomic.Bool
+)
+
+// REVIEW: Is it better to move initialization of scheme elsewhere,
+// and change the `Get` to only return the scheme (or directly use the scheme)?
+
 // Get returns a scheme aware of all types the manager can interact with.
 func Get() (*runtime.Scheme, error) {
-	scheme := runtime.NewScheme()
+	// Only add schemes to the runtime scheme when adding is not done.
+	if !addToSchemeDone.Load() {
+		if err := apiextensionsv1.AddToScheme(kicScheme); err != nil {
+			return nil, err
+		}
 
-	if err := apiextensionsv1.AddToScheme(scheme); err != nil {
-		return nil, err
-	}
+		if err := clientgoscheme.AddToScheme(kicScheme); err != nil {
+			return nil, err
+		}
 
-	if err := clientgoscheme.AddToScheme(scheme); err != nil {
-		return nil, err
-	}
+		if err := kongv1.AddToScheme(kicScheme); err != nil {
+			return nil, err
+		}
+		if err := kongv1alpha1.AddToScheme(kicScheme); err != nil {
+			return nil, err
+		}
+		if err := kongv1beta1.AddToScheme(kicScheme); err != nil {
+			return nil, err
+		}
+		if err := incubatorv1alpha1.AddToScheme(kicScheme); err != nil {
+			return nil, err
+		}
 
-	if err := kongv1.AddToScheme(scheme); err != nil {
-		return nil, err
+		if err := gatewayv1alpha2.Install(kicScheme); err != nil {
+			return nil, err
+		}
+		if err := gatewayv1alpha3.Install(kicScheme); err != nil {
+			return nil, err
+		}
+		if err := gatewayv1beta1.Install(kicScheme); err != nil {
+			return nil, err
+		}
+		if err := gatewayv1.Install(kicScheme); err != nil {
+			return nil, err
+		}
 	}
-	if err := kongv1alpha1.AddToScheme(scheme); err != nil {
-		return nil, err
-	}
-	if err := kongv1beta1.AddToScheme(scheme); err != nil {
-		return nil, err
-	}
-	if err := incubatorv1alpha1.AddToScheme(scheme); err != nil {
-		return nil, err
-	}
-
-	if err := gatewayv1alpha2.Install(scheme); err != nil {
-		return nil, err
-	}
-
-	if err := gatewayv1alpha3.Install(scheme); err != nil {
-		return nil, err
-	}
-
-	if err := gatewayv1beta1.Install(scheme); err != nil {
-		return nil, err
-	}
-
-	if err := gatewayv1.Install(scheme); err != nil {
-		return nil, err
-	}
-
-	return scheme, nil
+	addToSchemeDone.Store(true)
+	// REVIEW: is it safe to return the same scheme everywhere it is used?
+	return kicScheme, nil
 }

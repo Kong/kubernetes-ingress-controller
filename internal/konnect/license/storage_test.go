87146081c4dd32a13d340f8236ea671268d3b019
Yi Tao <tao.yi@konghq.com>
2025-06-13 10:45:30 +0800

    add tests and logger

--- internal/konnect/license/storage_test.go
+++ internal/konnect/license/storage_test.go
@@ -2,6 +2,8 @@ package license_test
 
 import (
 	"context"
+	"encoding/base64"
+	"strconv"
 	"testing"
 	"time"
 
@@ -25,7 +27,6 @@ func TestSecretLicenseStore_Store(t *testing.T) {
 		{
 			name: "stored secrets",
 			secret: &corev1.Secret{
-
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "konnect-license-test-cp",
 					Namespace: "default",
@@ -67,5 +68,91 @@ func TestSecretLicenseStore_Store(t *testing.T) {
 			// fake client stores stringData of secret as-is.
 			require.Equal(t, tc.license.Payload, secret.StringData["payload"])
 		})
+	}
+}
+
+func TestSecretLicenseStore_Load(t *testing.T) {
+	timeNowUnix := time.Now().Unix()
+	testCases := []struct {
+		name        string
+		secret      *corev1.Secret
+		license     license.KonnectLicense
+		expectError bool
+	}{
+		{
+			name: "load license successfully",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "konnect-license-test-cp",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"payload":    []byte(base64.StdEncoding.EncodeToString([]byte("some-license-payload"))),
+					"id":         []byte(base64.StdEncoding.EncodeToString([]byte("some-license-id"))),
+					"updated_at": []byte(base64.StdEncoding.EncodeToString([]byte(strconv.FormatInt(timeNowUnix, 10)))),
+				},
+			},
+			license: license.KonnectLicense{
+				Payload:   "some-license-payload",
+				ID:        "some-license-id",
+				UpdatedAt: time.Unix(timeNowUnix, 0),
+			},
+		},
+		{
+			name: "secret not found",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "some-other-secret",
+					Namespace: "default",
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "missing payload",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "konnect-license-test-cp",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"id":         []byte(base64.StdEncoding.EncodeToString([]byte("some-license-id"))),
+					"updated_at": []byte(base64.StdEncoding.EncodeToString([]byte(strconv.FormatInt(timeNowUnix, 10)))),
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "cannot parse update_at",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "konnect-license-test-cp",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"payload":    []byte(base64.StdEncoding.EncodeToString([]byte("some-license-payload"))),
+					"id":         []byte(base64.StdEncoding.EncodeToString([]byte("some-license-id"))),
+					"updated_at": []byte(base64.StdEncoding.EncodeToString([]byte("not-a-timestamp"))),
+				},
+			},
+			expectError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cl := fake.NewClientBuilder().WithObjects(tc.secret).Build()
+			s := konnectlicense.NewSecretLicenseStore(cl, "default", "test-cp")
+
+			l, err := s.Load(context.Background())
+			if tc.expectError {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.license, l)
+		})
+
 	}
 }

23e3e0cd1145908d702ea741a7256073c8c19474
Yi Tao <tao.yi@konghq.com>
2025-06-17 11:07:48 +0800

    address comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -115,7 +115,9 @@ Adding a new version? You'll need three changes:
    [#7466](https://github.com/Kong/kubernetes-ingress-controller/pull/7466)
  - Store the license fetched from Konnect to the `Secret` named `konnect-license-<cpID>`
    in the same namespace where KIC runs. The `cpID` is the ID of the Konnect
-   control plane. This requires the `Secret` to be created before KIC runs.
+   control plane. This requires the user to create the `Secret` with the given
+   name in the namespace before KIC runs and KIC will update and read the
+   existing `Secret`.
    It is enabled by default when `--konnect-licensing-enabled` is `true`. You
    can set `--konnect-license-storage-enabled` to `false` to disable it.
    [#7488](https://github.com/Kong/kubernetes-ingress-controller/pull/7488)
--- internal/konnect/license/storage.go
+++ internal/konnect/license/storage.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/samber/lo"
@@ -12,14 +13,19 @@ import (
 	k8stypes "k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/kong/kubernetes-ingress-controller/v3/internal/labels"
 	"github.com/kong/kubernetes-ingress-controller/v3/internal/license"
 )
 
 const (
 	// licenseResourceNamePrefix is the prefix of the secret name storing the konnect license.
 	licenseResourceNamePrefix = "konnect-license-"
-	// LabelKeyManagedBy is the label key to mark that the secret is managed by KIC.
-	LabelKeyManagedBy = "managed-by"
+	// secretKeyPayload is the key to store the payload of the license in the secret.
+	secretKeyPayload = "payload"
+	// secretKeyID is the key to store the ID of the license.
+	secretKeyID = "id"
+	// secretKeyUpdatedAt is the key to store updated time of the license.
+	secretKeyUpdatedAt = "updated_at"
 )
 
 // Storer is the interface to store license fetched from Konnect and load license from storage if failed to fetch.
@@ -28,6 +34,7 @@ type Storer interface {
 	Load(context.Context) (license.KonnectLicense, error)
 }
 
+// SecretLicenseStore is the storage to store the license in a certain secret in the given namespace.
 type SecretLicenseStore struct {
 	cl             client.Client
 	namespace      string
@@ -62,12 +69,12 @@ func (s *SecretLicenseStore) Store(ctx context.Context, l license.KonnectLicense
 	if secret.Labels == nil {
 		secret.Labels = map[string]string{}
 	}
-	secret.Labels[LabelKeyManagedBy] = "konghq.com/ingress-controller"
+	secret.Labels[labels.ManagedByLabel] = labels.ManagedByLabelValueIngressController
 
 	secret.StringData = map[string]string{
-		"payload":    l.Payload,
-		"updated_at": strconv.FormatInt(l.UpdatedAt.Unix(), 10),
-		"id":         l.ID,
+		secretKeyPayload:   l.Payload,
+		secretKeyUpdatedAt: strconv.FormatInt(l.UpdatedAt.Unix(), 10),
+		secretKeyID:        l.ID,
 	}
 	return s.cl.Update(ctx, secret)
 }
@@ -85,19 +92,26 @@ func (s *SecretLicenseStore) Load(
 		return license.KonnectLicense{}, err
 	}
 
-	if (!lo.HasKey(secret.Data, "payload")) || (!lo.HasKey(secret.Data, "updated_at")) || (!lo.HasKey(secret.Data, "id")) {
-		return license.KonnectLicense{}, fmt.Errorf("missing required key in secret %s", secret.Name)
+	requiredKeys := []string{secretKeyPayload, secretKeyID, secretKeyUpdatedAt}
+	missingKeys := []string{}
+	for _, key := range requiredKeys {
+		if !lo.HasKey(secret.Data, key) {
+			missingKeys = append(missingKeys, key)
+		}
+	}
+	if len(missingKeys) > 0 {
+		return license.KonnectLicense{}, fmt.Errorf("missing required key(s): %s in secret %s", strings.Join(missingKeys, ","), secret.Name)
 	}
 
-	decodedPayload, err := base64.StdEncoding.DecodeString(string(secret.Data["payload"]))
+	decodedPayload, err := base64.StdEncoding.DecodeString(string(secret.Data[secretKeyPayload]))
 	if err != nil {
 		return license.KonnectLicense{}, fmt.Errorf("failed to decode payload of license stored in secret %s: %w", secret.Name, err)
 	}
-	decodedID, err := base64.StdEncoding.DecodeString(string(secret.Data["id"]))
+	decodedID, err := base64.StdEncoding.DecodeString(string(secret.Data[secretKeyID]))
 	if err != nil {
 		return license.KonnectLicense{}, fmt.Errorf("failed to decode id of license stored in secret %s: %w", secret.Name, err)
 	}
-	decodedUpdateAt, err := base64.StdEncoding.DecodeString(string(secret.Data["updated_at"]))
+	decodedUpdateAt, err := base64.StdEncoding.DecodeString(string(secret.Data[secretKeyUpdatedAt]))
 	if err != nil {
 		return license.KonnectLicense{}, fmt.Errorf("failed to decode updated_at of license stored in secret %s: %w", secret.Name, err)
 	}

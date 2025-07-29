7a0292807172181d551fc2df9c2ae524ff338a7b
Yi Tao <tao.yi@konghq.com>
2025-07-29 20:42:36 +0800

    fix label value of secret to store license

--- internal/labels/labels.go
+++ internal/labels/labels.go
@@ -25,7 +25,7 @@ const (
 	// ManagedByLabel is the label key to mark that the object is managed by a specific controller.
 	ManagedByLabel = LabelPrefix + ManagedByKey
 	// ManagedByLabelValueIngressController is the label value that marks the object is managed byu KIC.
-	ManagedByLabelValueIngressController = LabelPrefix + "/ingress-controller"
+	ManagedByLabelValueIngressController = "kong-ingress-controller"
 )
 
 // ValidateType indicates the type of validation applied to a Secret.

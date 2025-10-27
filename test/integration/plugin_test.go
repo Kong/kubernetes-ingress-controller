243cfef2ac69666e2c980032ef36150296cb4078
Yi Tao <tao.yi@konghq.com>
2025-10-27 16:21:12 +0800

    find plugin in multiple plugins

--- test/integration/plugin_test.go
+++ test/integration/plugin_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/kong/go-kong/kong"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
 	"github.com/kong/kubernetes-testing-framework/pkg/utils/kubernetes/generators"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
@@ -800,20 +801,27 @@ func TestPluginNullInConfig(t *testing.T) {
 		require.NoError(t, err, "failed to create Kong client")
 		plugins, err := kc.Plugins.ListAll(ctx)
 		require.NoError(t, err, "failed to list plugins")
-		if len(plugins) != 1 {
-			t.Logf("Expected to get 1 plugin, actual %d", len(plugins))
-			return false
-		}
-		plugin := plugins[0]
-		if plugin.Name == nil || *plugin.Name != "datadog" {
+
+		datadogPlugin, found := lo.Find(plugins, func(p *kong.Plugin) bool {
+			return p.Name != nil && *p.Name == "datadog"
+		})
+		if !found {
+			t.Logf("datadog plugin not found. %d plugins found: %s",
+				len(plugins),
+				strings.Join(
+					lo.Map(plugins, func(p *kong.Plugin, _ int) string {
+						return lo.FromPtrOr(p.Name, "_")
+					}),
+					","),
+			)
 			return false
 		}
 
-		configJSON, err := json.Marshal(plugin.Config)
+		configJSON, err := json.Marshal(datadogPlugin.Config)
 		require.NoError(t, err)
 		t.Logf("Configuration of datadog plugin: %s", string(configJSON))
 
-		configPrefix, ok := plugin.Config["prefix"]
+		configPrefix, ok := datadogPlugin.Config["prefix"]
 		return ok && configPrefix == nil
 	}, ingressWait, waitTick, "failed to find 'datadog' plugin with null in config.prefix in Kong")
 }

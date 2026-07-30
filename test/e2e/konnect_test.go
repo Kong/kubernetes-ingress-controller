5a0c283d593d069708ea3d9bb98227b1e25b1559
Jintao Zhang <zhangjintao9020@gmail.com>
2026-07-30 15:31:04 +0000

    test(e2e): remove unsupported Konnect licensing helper
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- test/e2e/konnect_test.go
+++ test/e2e/konnect_test.go
@@ -11,7 +11,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/blang/semver/v4"
 	environment "github.com/kong/kubernetes-testing-framework/pkg/environments"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
@@ -78,10 +77,10 @@ func TestKonnectLicenseActivation(t *testing.T) {
 	cert, key := testkonnect.CreateClientCertificate(ctx, t, rgID)
 	createKonnectClientSecretAndConfigMap(ctx, t, env, cert, key, rgID)
 
-	testKonnectLicenseActivationWithoutForceLicense(ctx, t, env, rgID)
+	testKonnectLicenseActivationWithoutForceLicense(ctx, t, env)
 }
 
-func testKonnectLicenseActivationWithoutForceLicense(ctx context.Context, t *testing.T, env environment.Environment, rgID string) {
+func testKonnectLicenseActivationWithoutForceLicense(ctx context.Context, t *testing.T, env environment.Environment) {
 	const manifestFile = "manifests/all-in-one-dbless-konnect-enterprise.yaml"
 	ManifestDeploy{Path: manifestFile}.Run(ctx, t, env)
 
@@ -135,37 +134,6 @@ func testKonnectLicenseActivationWithoutForceLicense(ctx context.Context, t *tes
 		}
 		return license.License.Expiration != ""
 	}, adminAPIWait, time.Second)
-	t.Log("done")
-}
-
-func testKonnectLicenseActivationWithForceLicense(ctx context.Context, t *testing.T, env environment.Environment, rgID string) {
-	const manifestFile = "manifests/all-in-one-dbless-konnect-enterprise.yaml"
-	ManifestDeploy{
-		Path: manifestFile,
-		Patches: []ManifestPatch{
-			WithKonnectLicensingPatch(),
-		},
-	}.Run(ctx, t, env)
-	exposeAdminAPI(ctx, t, env, k8stypes.NamespacedName{Namespace: "kong", Name: "proxy-kong"})
-
-	t.Log("confirming that the license is set")
-	assert.Eventually(t, func() bool {
-		license, err := getLicenseFromAdminAPI(ctx, env, "")
-		if err != nil {
-			t.Logf("failed to get license: %v", err)
-			return false
-		}
-		return license.License.Expiration != ""
-	}, adminAPIWait, time.Second)
-
-	skipTestIfControllerVersionBelow(t, semver.MustParse("3.5.1"))
-	t.Log("checking if the license is stored in the local secret")
-	secret, err := env.Cluster().Client().CoreV1().Secrets(namespace).Get(
-		ctx, "konnect-license-"+rgID, metav1.GetOptions{})
-	require.NoError(t, err)
-	require.NotNil(t, secret.Data, "secret to store Konnect license should not be empty")
-	require.NotEmpty(t, secret.Data["id"], "stored license should have a non-empty ID")
-
 	t.Log("done")
 }
 
